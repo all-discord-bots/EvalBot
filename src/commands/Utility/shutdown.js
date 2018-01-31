6c2a9b8f13286fe1e8c94daca76d97c9af5048d1
BannerBomb <BannerBomb55@gmail.com>
2018-01-30 19:26:18 -0500

    Update shutdown.js

--- src/commands/Utility/shutdown.js
+++ src/commands/Utility/shutdown.js
@@ -1,8 +1,9 @@
 exports.run = async (bot, msg) => {
-    if (msg.author.id === "269247101697916939") {
-      await msg.edit(':white_check_mark: Shutting down. Bye!');
-      process.exit(666);
-    }
+    if (msg.author.id !== bot.config.botCreatorID) return;
+    (await msg.channel.send("Shutting down...").then((msg)=>{
+        msg.edit(':white_check_mark: Shutting down. Bye!');
+    }));
+    process.exit(666);
 };
 
 exports.info = {
