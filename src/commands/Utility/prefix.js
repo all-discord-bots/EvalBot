91dc58a905395c6fdc1e3d03c667a7cdd0549c6f
BannerBomb <BannerBomb55@gmail.com>
2018-01-30 19:23:10 -0500

    Update prefix.js

--- src/commands/Utility/prefix.js
+++ src/commands/Utility/prefix.js
@@ -1,15 +1,16 @@
 exports.run = (bot, msg, args) => {
-    if (msg.author.id === "269247101697916939") {
-      if (args.length < 1) {
+    if (msg.author.id !== bot.config.botCreatorID) return;
+    if (args.length < 1) {
         throw 'Please provide a prefix to set!';
-      }
-
-      const prefix = args.join(' ');
-      bot.managers.config.set('prefix', prefix);
-      // No point trying to delete this message, the bot will be
-      // rebooting before we have a chance to.
-      msg.edit('Prefix set, rebooting! :ok_hand:');
     }
+    
+    const prefix = args.join(' ');
+    bot.managers.config.set('prefix', prefix);
+    // No point trying to delete this message, the bot will be
+    // rebooting before we have a chance to.
+    (await msg.channel.send("Setting Prefix...").then((msg)=>{
+        msg.edit('Prefix set, rebooting! :ok_hand:');
+    }));
 };
 
 exports.info = {
