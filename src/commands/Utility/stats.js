4ccbbb383291503f0a2b10378d53c300d17f4812
BannerBomb <BannerBomb55@gmail.com>
2018-02-01 20:55:11 -0500

    Update stats.js

--- src/commands/Utility/stats.js
+++ src/commands/Utility/stats.js
@@ -1,1 +1,22 @@
+const Discord = require('discord.js');
 
+exports.run = async (bot, msg) => {
+  msg.channel.sendMessage(`**Statistics**
+\`\`\`
+STATISTICS
+• Mem Usage    : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Swap Size    : ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB
+• Uptime       : ${bot.uptime}
+• Users        : ${bot.users.size}
+• Servers      : ${bot.guilds.size}
+• Channels     : ${bot.channels.size}
+• Discord.js   : v${Discord.version}
+\`\`\``);
+};
+
+exports.info = {
+  name: 'stats',
+  hidden: true,
+  usage: 'stats',
+  description: 'Provides some information about this bot.',
+};
