ef8c2998ad5de9ef88067119f6f894ae4c7b0989
BannerBomb <BannerBomb55@gmail.com>
2018-02-02 03:15:52 -0500

    Update cleanreactions.js

--- src/commands/Utility/cleanreactions.js
+++ src/commands/Utility/cleanreactions.js
@@ -1,13 +1,13 @@
 exports.run = (bot, msg, args) => {
   if(!parseInt(args[0], 10)) {
-    return msg.edit(`Please provide a number of messages to clean reactions from!`).then(setTimeout(msg.delete.bind(msg), 1000));
+    return msg.channel.send(`Please provide a number of messages to clean reactions from!`);
   }
   if (msg.author.id !== bot.config.botCreatorID) {
     if(!msg.member.hasPermission("MANAGE_MESSAGES")) {
       return msg.channel.send(`You are missing permissions \`Manage Messages\``);
     }
   }
-  msg.channel.messages.fetch({limit: parseInt(args[0], 10)}).then(msglog => {
+  msg.channel.fetchMessages({limit: parseInt(args[0], 10)}).then(msglog => {
     msg.channel.send(`Clearing Reactions from this channel for ${args[0]} messages...`);
     msglog.forEach(message => {
       message.clearReactions();
