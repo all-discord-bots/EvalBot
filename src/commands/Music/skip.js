ad931676c9686fedb8649e3b81111a2d6347df2d
BannerBomb <BannerBomb55@gmail.com>
2018-02-02 00:19:03 -0500

    Update skip.js

--- src/commands/Music/skip.js
+++ src/commands/Music/skip.js
@@ -5,5 +5,5 @@ console.log("song skipped");
 exports.info = {
   name: 'skip',
   usage: 'skip <number>',
-  description: 'Skip song number of songs. Skips 1 song 1 song if a number is not specified.'
+  description: 'Skip song number of songs. Skips 1 song if a number is not specified.'
 };
