03c28a00d11e3ba5df01a0c135dc61dfd3574484
BannerBomb <BannerBomb55@gmail.com>
2018-02-02 00:02:22 -0500

    Create clearqueue.js

---
+++ src/commands/Music/clearqueue.js
@@ -0,0 +1,9 @@
+exports.run = async (bot, msg) => {
+console.log("cleared song queue");
+};
+
+exports.info = {
+  name: 'clearqueue',
+  usage: 'clearqueue',
+  description: 'Clears the song queue.'
+};
