5639c4dd65216dd8266df816a21c96ef320d6d4d
BannerBomb <BannerBomb55@gmail.com>
2018-02-02 03:34:44 -0500

    Update purge.js

--- src/commands/Utility/purge.js
+++ src/commands/Utility/purge.js
@@ -1,10 +1,15 @@
 exports.run = async (bot, msg, args) => {
+  if (msg.author.id !== bot.config.botCreatorID) {
+    if (!msg.member.hasPermission('MANAGE_MESSAGES')) {
+      return msg.channel.send('You must have the permission `Manage Messages`!');
+    }
+  }
   const user = (msg.mentions.users.first() || bot.users.get(args[0]) || null);
   const amount = !!user ? parseInt(msg.content.split(" ")[2], 10) : parseInt(msg.content.split(" ")[1], 10);
-  if (!amount) return msg.edit("Must specify an amount to delete!").then(msg.delete(2000));
-  if (!amount && !user) return msg.edit("Must specify a user and amount, or just an amount, of messages to purge!").then(msg.delete(2000));
+  if (!amount) return msg.channel.send("Must specify an amount to delete!");
+  if (!amount && !user) return msg.channel.send("Must specify a user and amount, or just an amount, of messages to purge!");
   await msg.delete();
-  let messages = await msg.channel.messages.fetch({limit: 100});
+  let messages = await msg.channel.fetchMessages({limit: 100});
   if(user) {
     messages = messages.array().filter(m=>m.author.id === user.id);
     bot.log("log", "Purge Amount", msg.author, "Amount: " + amount);
