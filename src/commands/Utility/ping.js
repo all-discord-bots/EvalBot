b4ae04ae22869dc10f295e87f5af1ec2ce41628f
BannerBomb <BannerBomb55@gmail.com>
2018-02-01 19:59:30 -0500

    Update ping.js

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -1,1 +1,13 @@
+exports.run = async (bot, msg) => {
+  // const m = await msg.channel.send("Pinging...");
+  (await msg.channel.send("Pinging...").then((msg)=>{
+    msg.edit(`Latency: \`${msg.createdTimestamp - msg.createdTimestamp}ms\`\nAPI Latency: \`${Math.round(bot.ping)}ms\``);
+  }));
+};
 
+exports.info = {
+  name: 'ping',
+  hidden: true,
+  usage: 'ping',
+  description: 'Get the API Latency'
+};
