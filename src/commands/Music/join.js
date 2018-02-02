b93c86914ef03a7358a3a2dbfbabd094e2a6ccea
BannerBomb <BannerBomb55@gmail.com>
2018-02-01 22:14:32 -0500

    Update join.js

--- src/commands/Music/join.js
+++ src/commands/Music/join.js
@@ -1,15 +1,15 @@
 exports.run = async (bot, msg) => {
-  const voiceChannel = msg.channel.voiceChannel;
+  const voiceChannel = msg.member.voiceChannel;
   if (!voiceChannel) {
-    return msg.channel.send(':redx: You must be in a voice channel for the bot to join!');
+    return msg.channel.send(`<:redx:318595018396794884> You must be in a voice channel for the bot to join!`);
   }
   if (msg.author.id !== bot.config.botCreatorID) {
     if (!msg.member.hasPermission("MANAGE_GUILD")) {
-      msg.channel.send("You need `Manage Server` permission to use this command!");
+      msg.channel.send(`<:redx:318595018396794884> You need \`Manage Server\` permission to use this command!`);
       return;
     }
   }
-  msg.channel.send("Voice channel successfully joined!");
+  msg.channel.send(`✅ Voice channel successfully joined!`);
   msg.member.voiceChannel.join().then(connection => {
     require('http').get("http://www.internet-radio.com/servers/tools/playlistgenerator/?u=http://www.partyviberradio.com:8016/listen.pls?sid=1&t=.m3u", (res) => {
       connection.playStream(res);
