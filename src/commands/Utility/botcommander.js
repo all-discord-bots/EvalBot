8f607349881fa49c8ad96522cb1e67497d1f8a3b
BannerBomb <BannerBomb55@gmail.com>
2018-02-03 18:59:32 -0500

    Update botcommander.js

--- src/commands/Utility/botcommander.js
+++ src/commands/Utility/botcommander.js
@@ -1,1 +1,22 @@
+exports.run = async (bot, msg, args) => {
+  if (msg.author.id !== bot.config.botCreatorID) {
+    if (!msg.member.hasPermission('ADMINISTRATOR')) return;
+  }
+  
+  if (args[0].toLowerCase() == "enable") {
+    msg.guild.createRole({ name: bot.config.botCommanderRole, permissions: 0 });
+    msg.channel.send(`Successfully created \`${bot.config.botCommanderRole}\` role!`);
+  } else if (args[0].toLowerCase() == "disable") {
+    msg.guild.roles.find("name", bot.config.botCommanderRole).delete();
+    msg.channel.send(`Successfully deleted \`${bot.config.botCommanderRole}\` role!`);
+  } else {
+    return msg.channel.send("Invalid argument provided, please specify `enable` or `disable`!");
+  }
+};
 
+exports.info = {
+  name: 'bot-commander',
+  aliases: ['bot-commander-role','commander-role','botcommander'],
+  usages: 'bot-commander <enable | disable>',
+  description: 'Enable a role to use commands that need require higher permissions, without having those permissions (e.g. Manage Server). Moderation commands are excluded from this.'
+};
