19c5a4a74ae6e9fd506c817063abee0a88e768df
BannerBomb <BannerBomb55@gmail.com>
2018-02-04 02:06:03 -0500

    Update sendemoji.js

--- src/commands/Fun/sendemoji.js
+++ src/commands/Fun/sendemoji.js
@@ -1,6 +1,6 @@
 exports.run = async (bot, msg, args) => {
   if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('EXTERNAL_EMOJIS')) return;
+    if (!msg.member.hasPermission('EXTERNAL_EMOJIS')) return msg.channel.send("You are missing the permission `External Emojis`!");
   }
   if (args.length < 1) {
     return msg.channel.send("Please provide a emoji ID!");
@@ -13,5 +13,5 @@ exports.run = async (bot, msg, args) => {
 exports.info = {
   name: 'sendemoji',
   usage: 'sendemoji <emoji id>',
-  description: 'Make the bot message an emoji from an emoji ID.'
+  description: 'Make the bot message an emoji from an emoji ID only if the bot is in the server.'
 };
