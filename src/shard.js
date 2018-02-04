34ac5a0b81e629c2d2d850b733d1edcef5f3a4c8
BannerBomb <BannerBomb55@gmail.com>
2018-02-03 19:04:51 -0500

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -4,5 +4,6 @@
 */
 
 const Discord = require('discord.js');
+const SpawnShards = require('./data/configs/config.json');
 const Manager = new Discord.ShardingManager('./src/bot.js');
-Manager.spawn(1); // Current set will spawn 1 shard // This example will spawn 2 shards (5,000 guilds);
+Manager.spawn(SpawnShards.shards); // Current set will spawn 1 shard // This example will spawn 2 shards (5,000 guilds);
