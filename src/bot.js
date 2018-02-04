6570ed64393c54bb4243fc25c8f3490fd8a4b248
BannerBomb <BannerBomb55@gmail.com>
2018-02-03 21:26:48 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -121,7 +121,7 @@ bot.on("reconnecting", () => {
 	var months = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
 	var days = ["1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th","21st","22nd","23rd","24th","25th","26th","27th","28th","29th","30th","31st"];
 	const date = new Date();
-	reconnectHook.send(`\`${days[date.getDate() - 2 ]} ${months[date.getMonth()} ${date.getFullYear()}  ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}\`<@${bot.user.id}> Shard \`${bot.shard.id}\` reconnecting`);
+	reconnectHook.send(`\`${days[date.getDate() - 2 ]} ${months[date.getMonth()]} ${date.getFullYear()}  ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}\`<@${bot.user.id}> Shard \`${bot.shard.id}\` reconnecting`);
 	bot.destroy();
 	process.exit(0);
 });
