65572f4233e7eee494d73c4f73d37fd4cbc55d61
BannerBomb <BannerBomb55@gmail.com>
2018-02-04 21:49:41 -0500

    Create roleusers.js

---
+++ src/commands/Utility/roleusers.js
@@ -0,0 +1,12 @@
+exports.run = async (bot, msg, args) => {
+  if (args.length < 1) {
+    return msg.channel.send("Invalid arguments! Please provide a role ID.");
+  }
+  msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find(args[0]).members.map(m => m.user.tag)}\n\`\`\``);
+};
+
+exports.info = {
+  name: 'roleusers',
+  usage: 'roleusers <role id>',
+  description: 'Lists all the usernames who have a given role'
+};
