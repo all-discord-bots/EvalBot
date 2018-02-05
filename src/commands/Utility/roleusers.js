9c47b4b947e73dbdef194459ace9bdd40eef8ed1
BannerBomb <BannerBomb55@gmail.com>
2018-02-04 21:57:10 -0500

    Update roleusers.js

--- src/commands/Utility/roleusers.js
+++ src/commands/Utility/roleusers.js
@@ -2,7 +2,7 @@ exports.run = async (bot, msg, args) => {
   if (args.length < 1) {
     return msg.channel.send("Invalid arguments! Please provide a role ID.");
   }
-  msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find(args[0]).members.map(m => m.user.tag)}\n\`\`\``);
+  msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find(args[0].value).members.map(m => m.user.tag)}\n\`\`\``);
 };
 
 exports.info = {
