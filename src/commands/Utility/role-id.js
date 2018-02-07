e8f3abbf944be54d6c391d4f6e7a37f1fd427610
BannerBomb <BannerBomb55@gmail.com>
2018-02-07 11:24:23 -0500

    Update role-id.js

--- src/commands/Utility/role-id.js
+++ src/commands/Utility/role-id.js
@@ -2,9 +2,11 @@ exports.run = async (bot, msg, args) => {
   if (args.length < 1) {
     return msg.channel.send("Invalid arguments! Please provide a role name.");
   }
-  const rolename = args.join(" ");
-  let getroleid = msg.channel.guild.roles.find('name', `${rolename}`).id;
-  msg.channel.send(`${getroleid}`);
+  //const rolename = args.join(" ");
+  let getroleid = msg.channel.guild.roles.find('name', `${args[0]}`).id;
+  (await msg.channel.send("Loading role id...").then((msg)=>{
+    msg.edit(`${getroleid}`);
+  }));
 };
 
 exports.info = {
