768bcbd96a458dd3efc45641d0ea569af6ccaf1a
BannerBomb <BannerBomb55@gmail.com>
2018-02-07 12:31:12 -0500

    Update role-id.js

--- src/commands/Utility/role-id.js
+++ src/commands/Utility/role-id.js
@@ -4,14 +4,15 @@ exports.run = async (bot, msg, args) => {
   }
   const rolename = args.join(" ");
   let getroleid = msg.guild.roles.find('name', `${rolename}`).id;
-  (await msg.channel.send("Loading role id...").then((msg)=>{
-    msg.edit(`${getroleid}`);
-  }));
+  //(await msg.channel.send("Loading role id...").then((msg)=>{
+  //  msg.edit(`${getroleid}`);
+  //}));
+  msg.channel.send(`${getroleid}`);
 };
 
 exports.info = {
   name: 'role-id',
   aliases: ['roleid'],
   usage: 'role-id <role name>',
-  description: 'Lists all id of the role you specify'
+  description: 'Easy and simple way to get the ID of any role.'
 };
