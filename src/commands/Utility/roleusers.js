085fa0d4e2acd22cff1ebde71307ef8ce9bfcec8
BannerBomb <BannerBomb55@gmail.com>
2018-02-07 11:06:41 -0500

    Update roleusers.js

--- src/commands/Utility/roleusers.js
+++ src/commands/Utility/roleusers.js
@@ -2,18 +2,18 @@ exports.run = async (bot, msg, args) => {
   if (args.length < 1) {
     return msg.channel.send("Invalid arguments! Please provide a role ID.");
   }
-  if (args[1].toLowerCase() == "-users") {
-    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[0]}`).members.map(m => m.user.tag)}\n\`\`\``);
-  } else if (args[1].toLowerCase() == "-count") {
-    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[0]}`).members.size}\n\`\`\``);
+  if (args[0].toLowerCase() == "users") {
+    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[1]}`).members.map(m => m.user.tag)}\n\`\`\``);
+  } else if (args[0].toLowerCase() == "size") {
+    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[1]}`).members.size}\n\`\`\``);
     //msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find(args[0].value).members.map(m => m.user.tag)}\n\`\`\``);
-  } else {
-    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[0]}`).members.size}\n\`\`\``);
+  } else if (args[0].toLowerCase != "size" || args[0].toLowerCase != "users") {
+    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[1]}`).members.size}\n\`\`\``);
   }
 };
 
 exports.info = {
   name: 'roleusers',
-  usage: 'roleusers <role id> [-users|-count]',
+  usage: 'roleusers [users|size] <role id>',
   description: 'Lists all the usernames who have a given role'
 };
