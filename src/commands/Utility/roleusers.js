204d038402efd91ef5ac84075841e38562d505b5
BannerBomb <BannerBomb55@gmail.com>
2018-02-07 10:49:47 -0500

    Update roleusers.js

--- src/commands/Utility/roleusers.js
+++ src/commands/Utility/roleusers.js
@@ -2,11 +2,18 @@ exports.run = async (bot, msg, args) => {
   if (args.length < 1) {
     return msg.channel.send("Invalid arguments! Please provide a role ID.");
   }
-  msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find(args[0].value).members.map(m => m.user.tag)}\n\`\`\``);
+  if (args[1].toLowerCase() == "-users") {
+    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[0]}`).members.map(m => m.user.tag)}\n\`\`\``);
+  } else if (args[1].toLowerCase() == "-count") {
+    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[0]}`).members.size}\n\`\`\``);
+    //msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find(args[0].value).members.map(m => m.user.tag)}\n\`\`\``);
+  } else {
+    msg.channel.send(`\`\`\`\n${msg.channel.guild.roles.find('id', `${args[0]}`).members.size}\n\`\`\``);
+  }
 };
 
 exports.info = {
   name: 'roleusers',
-  usage: 'roleusers <role id>',
+  usage: 'roleusers <role id> [-users|-count]',
   description: 'Lists all the usernames who have a given role'
 };
