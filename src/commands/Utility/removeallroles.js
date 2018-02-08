c7fb50bad67fa6b6b2fcdeb737a22dc0737028de
BannerBomb <BannerBomb55@gmail.com>
2018-02-08 10:47:04 -0500

    Update removeallroles.js

--- src/commands/Utility/removeallroles.js
+++ src/commands/Utility/removeallroles.js
@@ -5,13 +5,13 @@ exports.run = async (bot, msg, args) => {
   if (args.length < 1) {
     return msg.channel.send("Invalid arguments!");
   }
-  if (!args[1]) {
-    return msg.channel.send("Invalid arguments!");
+    if (args[1].toLowerCase() != "name" || args[1].toLowerCase() != "id") {
+      return msg.channel.send("Invalid arguments!");
   }
-  if (args[0] == "name") {
-    msg.channel.guild.roles.find('name', args[1]).members.map(m => m.removeRole('name', args[1]));
-  } else if (args[0] == "id") {
-    msg.channel.guild.roles.find(args[1]).members.map(m => m.removeRole(args[1]));
+  if (args[0].toLowerCase() == "name") {
+    msg.guild.roles.find('name', `${args[1]}`).members.map(m => m.removeRole('name', `${args[1]}`));
+  } else if (args[0].toLowerCase() == "id") {
+    msg.guild.roles.find('id', `{args[1]}`).members.map(m => m.removeRole('id', `${args[1]}`));
   }
 };
   
