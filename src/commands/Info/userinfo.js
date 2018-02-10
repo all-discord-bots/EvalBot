648efddf6ca0cc588d1581b064b4b6f8f9f50c73
BannerBomb <BannerBomb55@gmail.com>
2018-02-09 21:13:40 -0500

    Update and rename user.js to userinfo.js

---
+++ src/commands/Info/userinfo.js
@@ -0,0 +1,66 @@
+const https = require('https')
+exports.run = async (bot, msg) => {
+	var user = msg.mentions.users.first()
+	let statusemoji;
+	if (user.presence.status === "online") {
+		statusemoji = `<:online:411637359398879232>`;
+	} else if (user.presence.status === "idle") {
+		statusemoji = `<:away:411637359214460939>`;
+	} else if (user.presence.status === "dnd") {
+		statusemoji = `<:dnd:411636698993262593>`;
+	} else if (user.presence.status === "offline") {
+		statusemoji = `<:offline:411637359361392650>`;
+	}
+	let gdate = new Date(user.createdTimestamp);
+	let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+	let days = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+	let gmonth = months[gdate.getMonth()];
+	let gday = days[gdate.getDate()+1];
+	let gyear = gdate.getYear()-100;
+	let newyear = new Date();
+	let gnewmonth = months[newyear.getMonth()];
+	let gnewyear = newyear.getYear()-100;
+	let gsdate = new Date(user.joinedTimestamp); // msg.member.joinedTimestamp
+	let gsmonth = months[gsdate.getMonth()];
+	let gsday = days[gsdate.getDate()+1];
+	let gsyear = gsdate.getYear()-100;
+	let ggame;
+	if (user.presence.game != null) {
+		ggame = `\n <:transparent:411703305467854889>${user.presence.game}`;
+	} else {
+		ggame = "";
+	}
+	msg.channel.send({embed: ({
+		color: 3447003,
+		description: `${statusemoji} <@${user.id}>${ggame}`,
+		thumbnail: {
+			url: `${user.avatarURL}`
+		}, author: {
+			name: `${user.tag}`,
+			icon_url: `${user.avatarURL}`
+		}, fields: [{
+			name: 'User ID',
+			value: `${user.id}`
+		}, {
+			name: 'Joined Discord',
+			value: `${gday}-${gmonth}-${gyear} (${gnewyear-gyear} year and ${gnewmonth-gmonth} month ago)`
+		}, {
+			name: 'Joined Server',
+			value: `${gsday}-${gsmonth}-${gsyear} (${gsmonth-gmonth} month ago)`
+		}, {
+			name: 'Highest Role',
+			value: 'N/A',
+			inline: true
+		}, {
+			name: 'Member #',
+			value: 'N/A',
+			inline: true
+		],
+	})});
+}
+exports.info = {
+	name: 'user-info',
+	aliases: ['user','userinfo','member','member-info','profile'],
+	usage: 'user-info <user>',
+	description: 'Shows various information and data on the mentioned user.',
+};
