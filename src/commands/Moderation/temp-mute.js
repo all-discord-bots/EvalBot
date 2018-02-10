47bbc7a8c2df3a27df9a4ca8ba10cc82317abea1
BannerBomb <BannerBomb55@gmail.com>
2018-02-10 15:20:32 -0500

    Update temp-mute.js

--- src/commands/Moderation/temp-mute.js
+++ src/commands/Moderation/temp-mute.js
@@ -5,18 +5,18 @@ exports.run = async (bot, msg, args) => {
 let mutedrole = "Muted"; // name of the 'muted' role
   //!tempmute @user 1s/m/h/d
   if (msg.author.id !== bot.config.botCreatorID) {
-    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("You are missing permissions `Kick Members`!");
+    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Kick Members\`!`);
   }
   let tomute = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
-  if(!tomute) return msg.reply("Couldn't find user.");
-  //if(tomute.hasPermission("MANAGE_MESSAGES")) return msg.reply("Can't mute them!");
+  if(!tomute) return msg.channel.send(`<:redx:411978781226696705> Couldn't find user.`);
+  //if(tomute.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(`<:redx:411978781226696705> Can't mute them!`);
   let muterole = msg.guild.roles.find(`name`, `${mutedrole}`);
   //start of create role
   if(!muterole){
     try{
       muterole = await msg.guild.createRole({
         name: `${mutedrole}`,
-        color: "#000000",
+        color: "#818386",
         permissions:[]
       })
       msg.guild.channels.forEach(async (channel, id) => {
@@ -31,15 +31,18 @@ let mutedrole = "Muted"; // name of the 'muted' role
   }
   //end of create role
   let mutetime = args[1];
-  if(!mutetime) return msg.reply("You didn't specify a time!");
+  if(mutetime) {
+    await(tomute.addRole(muterole.id));
+    msg.channel.send(`<:check:411976443522711552> <@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
 
-  await(tomute.addRole(muterole.id));
-  msg.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
-  setTimeout(function(){
-    tomute.removeRole(muterole.id);
-    msg.channel.send(`<@${tomute.id}> has been unmuted!`);
-  }, ms(mutetime));
+    setTimeout(function(){
+      tomute.removeRole(muterole.id);
+      msg.channel.send(`<@${tomute.id}> has been unmuted!`);
+    }, ms(mutetime));
+  } else if (!mutetime) {
+    msg.channel.send(`<:check:411976443522711552> <@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+    await(tomute.addRole(muterole.id));
+  }
 
 
 //end of module
@@ -47,7 +50,7 @@ let mutedrole = "Muted"; // name of the 'muted' role
 
 exports.info = {
   name: 'tempmute',
-  aliases: ['temp-mute'],
-  usage: 'tempmute <user> <1s/m/h/d>',
-  description: 'Temporarily mute a user.',
+  aliases: ['temp-mute','c-mute','chat-mute','t-mute','text-mute','m'],
+  usage: 'tempmute <user> [1s/m/h/d]',
+  description: 'Mutes a member from the server. This prevents them from sending messages. If no length is given they will be muted until un-muted. Length format examples: `1s`, `1m`, `1h`, `1d`, `1w`, `1mth`, `1y`.',
 }
