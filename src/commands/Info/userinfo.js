64834d220f1b259c41c498409441d3dca05123a2
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 02:32:24 -0500

    Update userinfo.js

--- src/commands/Info/userinfo.js
+++ src/commands/Info/userinfo.js
@@ -21,10 +21,19 @@ exports.run = async (bot, msg, args) => {
 		let rone = r.replace(/>/g, '');
 		//let guserone = msg.mentions.users.first();
 		//let gid = user.user.id;
-		user = msg.guild.members.get(`${rone}`);
+		
+		if (msg.guild.members.get(`${rone}`)) { // in current guild
+			user = msg.guild.members.get(`${rone}`);
+		} else if (!msg.guild.members.get(`${rone}`)) { // not in current guild
+			user = bot.users.find(`id`, `${rone}`);
+		}
 		//tag = msg.mentions.users.first().tag;
 	} else if (bot.users.find(`id`, `${args[0]}`)) {
-		user = msg.guild.members.get(`${args[0]}`);
+		if (msg.guild.members.get(`${args[0]}`)) {
+			user = msg.guild.members.get(`${args[0]}`);
+		} else if (!msg.guild.members.get(`${args[0]}`)) {
+			user = bot.users.find(`id`, `${args[0]}`);
+		}
 	} else if (!args[0]/*!msg.mentions.users.first() && !bot.users.find(`id`, `${gusers}`)*/) {
 		user = msg.guild.members.get(`${msg.member.id}`);
 	} // meed to get the user by plain name eg. BannerBomb
@@ -38,7 +47,12 @@ exports.run = async (bot, msg, args) => {
 	} else if (user.presence.status === "offline") {
 		statusemoji = `<:offline:411637359361392650>`;
 	}
-	let gdate = new Date(user.user.createdTimestamp);
+	let gdate;
+	if (msg.guild.members.get(`${user.id}`)) {
+		gdate = new Date(user.user.createdTimestamp);
+	} else if (!msg.guild.members.get(`${user.id}`)) {
+		gdate = new Date(user.createdTimestamp);
+	}
 	let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
 	let days = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
 	let gmonth = months[gdate.getMonth()];
@@ -57,6 +71,7 @@ exports.run = async (bot, msg, args) => {
 	} else {
 		ggame = "";
 	}
+	if (msg.guild.members.get(`${user.id}`)) {
 	msg.channel.send({embed: ({
 		color: 3447003,
 		description: `${statusemoji} <@${user.id}>${ggame}`,
@@ -88,6 +103,27 @@ exports.run = async (bot, msg, args) => {
 		}
 	],
 })})
+	} else if (!msg.guild.members.get(`${user.id}`)) {
+		msg.channel.send({embed: ({
+		color: 3447003,
+		description: `${statusemoji} <@${user.id}>${ggame}`,
+		thumbnail: {
+			url: `${user.avatarURL}`
+		}, author: {
+			name: `${user.tag}`,
+			icon_url: `${user.avatarURL}`
+		}, fields: [
+		{
+			name: 'User ID',
+			value: `${user.id}`
+		}, {
+			name: 'Joined Discord',
+			//value: `${gday}-${gmonth}-${gyear} (${gnewyear-gyear} year and ${gnewmonth-gmonth} month ago)`
+			value: `${moment.utc(gdate).format(`${gday}-${gmonth}-${gyear}`, "DD-MM-YY")}`
+		}
+	],
+})})
+	}
 }
 exports.info = {
 	name: 'user-info',
