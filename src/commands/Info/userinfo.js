0efa02cd4aa6bfb96ba3894ed574c741c2c8366c
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 01:06:43 -0500

    Update userinfo.js

--- src/commands/Info/userinfo.js
+++ src/commands/Info/userinfo.js
@@ -8,27 +8,16 @@ exports.run = async (bot, msg, args) => {
 		'Listening to',
 		'Watching',
 	];
-	let joined;
-	let created;
-	let avatar;
-	//let tag;
 	let user;
 	if (msg.mentions.users.first()) {
-		user = msg.mentions.users.first();
-		joined = msg.mentions.users.first().joinedTimestamp;
-		created = msg.mentions.users.first().createdTimestamp;
-		avatar = msg.mentions.users.first().avatarURL;
+		let guserone = msg.mentions.users.first();
+		let gid = user.id;
+		user = msg.guild.members.get(`${gid}`);
 		//tag = msg.mentions.users.first().tag;
 	} else if (bot.users.find(`id`, `${args[0]}`)) {
 		user = msg.guild.members.get(`${args[0]}`);
-		joined = msg.guild.members.get(`${args[0]}`).joinedTimestamp;
-		created = msg.guild.members.get(`${args[0]}`).user.createdTimestamp;
-		avatar = msg.guild.members.get(`${args[0]}`).user.avatarURL;
 	} else if (!args[0]/*!msg.mentions.users.first() && !bot.users.find(`id`, `${gusers}`)*/) {
 		user = msg.guild.members.get(`${msg.member.id}`);
-		joined = msg.guild.members.get(`${msg.member.id}`).joinedTimestamp;
-		created = msg.guild.members.get(`${msg.member.id}`).createdTimestamp;
-		avatar = msg.guild.members.get(`${msg.member.id}`).avatarURL;
 	} // meed to get the user by plain name eg. BannerBomb
 	let statusemoji;
 	if (user.presence.status === "online") {
@@ -40,7 +29,7 @@ exports.run = async (bot, msg, args) => {
 	} else if (user.presence.status === "offline") {
 		statusemoji = `<:offline:411637359361392650>`;
 	}
-	let gdate = new Date(created);
+	let gdate = new Date(user.user.createdTimestamp);
 	let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
 	let days = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
 	let gmonth = months[gdate.getMonth()];
@@ -49,7 +38,7 @@ exports.run = async (bot, msg, args) => {
 	let newyear = new Date();
 	let gnewmonth = months[newyear.getMonth()];
 	let gnewyear = newyear.getYear()-100;
-	let jdate = new Date(joined); // msg.member.joinedTimestamp
+	let jdate = new Date(user.joinedTimestamp); // msg.member.joinedTimestamp
 	let jmonth = months[jdate.getMonth()];
 	let jday = days[jdate.getDate()];
 	let jyear = jdate.getYear()-100;
@@ -63,10 +52,10 @@ exports.run = async (bot, msg, args) => {
 		color: 3447003,
 		description: `${statusemoji} <@${user.id}>${ggame}`,
 		thumbnail: {
-			url: `${avatar}`
+			url: `${user.user.avatarURL}`
 		}, author: {
 			name: `${user.user.tag}`,
-			icon_url: `${avatar}`
+			icon_url: `${user.user.avatarURL}`
 		}, fields: [
 		{
 			name: 'User ID',
