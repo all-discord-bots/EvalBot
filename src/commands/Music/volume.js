157a6b015f7deed31f92f447aaa8d84a143b1647
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 19:40:20 -0500

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -1,9 +1,15 @@
 exports.run = async (bot, msg) => {
 console.log("changed volume");
 };
-
+let hide;
+if (!bot.config.usemusic) {
+  hide = true;
+} else {
+  hide = false;
+}
 exports.info = {
   name: 'volume',
+  hidden: `${hide}`,
   usage: 'volume <number>',
   description: 'Adjust the playback volume between 1 and 200'
 };
