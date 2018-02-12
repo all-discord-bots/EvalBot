9cc3ed204d8b7f858a973b68ebf97cc006298a92
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 19:48:28 -0500

    Update clearqueue.js

--- src/commands/Music/clearqueue.js
+++ src/commands/Music/clearqueue.js
@@ -1,6 +1,8 @@
 exports.run = async (bot, msg) => {
-  if (!bot.config.nomusic) return;
+  if (bot.config.nomusic) return;
 console.log("cleared song queue");
+};
+
 exports.info = {
   name: 'clearqueue',
   hidden: `${bot.config.nomusic}`,
