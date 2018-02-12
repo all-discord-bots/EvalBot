984f5466f98cdc9ea1060f07dc53787697d74a01
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 19:55:45 -0500

    Update pause.js

--- src/commands/Music/pause.js
+++ src/commands/Music/pause.js
@@ -1,15 +1,11 @@
 exports.run = async (bot, msg) => {
-console.log("paused song");
+  if (msg.author.id !== bot.config.botCreatorID) return;
+  console.log("paused song");
 };
-let hide;
-if (!bot.config.usemusic) {
-  hide = true;
-} else {
-  hide = false;
-}
+
 exports.info = {
   name: 'pause',
-  hidden: `${hide}`,
+  hidden: true,
   usage: 'pause',
   description: 'Pause music playback.'
 };
