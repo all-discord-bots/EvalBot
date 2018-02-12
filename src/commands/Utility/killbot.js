fa3c57893ea9156274cb963fc9c7858d28272236
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 23:30:59 -0500

    Update killbot.js

--- src/commands/Utility/killbot.js
+++ src/commands/Utility/killbot.js
@@ -1,1 +1,15 @@
+exports.run = async (bot, msg) => {
+    if (msg.author.id !== bot.config.botCreatorID) return;
+    (await msg.channel.send("Killing processes...").then((msg)=>{
+        msg.edit(`<:check:411976443522711552> Killing all processes.`);
+    }));
+    process.exit();
+};
 
+exports.info = {
+    name: 'killbot',
+    aliases: ['kill-bot','powerdown'],
+    hidden: true,
+    usage: 'killbot',
+    description: 'Fully kills the bots processes.'
+};
