00d412af9cf2de938bc0c4968b1e67cf0172aedd
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 19:37:19 -0500

    Update leave.js

--- src/commands/Music/leave.js
+++ src/commands/Music/leave.js
@@ -1,9 +1,15 @@
 exports.run = async (bot, msg) => {
 console.log("stopped playing and left voice channel");
 };
-
+let hide;
+if (!bot.config.usemusic) {
+  hide = true;
+} else {
+  hide = false;
+}
 exports.info = {
   name: 'leave',
+  hidden: `${hide}`,
   usage: 'leave',
   description: 'Clears the song queue and leaves the channel.'
 };
