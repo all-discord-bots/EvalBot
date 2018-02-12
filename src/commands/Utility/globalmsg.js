a4a26f2bd69c6e8c2f1222a9993b3ee3880e22e5
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 23:15:47 -0500

    Update globalmsg.js

--- src/commands/Utility/globalmsg.js
+++ src/commands/Utility/globalmsg.js
@@ -1,18 +1,18 @@
 exports.run = async (bot, msg, args) => {
-  if (args.length < 1) return msg.channel.send("Invalid arguments!");
+  if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments provided!`);
   if (msg.author.id !== bot.config.botCreatorID) return;
   let getMessageToSend = args.join(" ");
   var guildList = bot.guilds.array();
   try {
     guildList.forEach(guild => guild.defaultChannel.send(`${getMessageToSend}`));
   } catch (err) {
-    console.log("Could not send message to " + guild.name);
+    console.log(`✅ Could not send message to ${guild.name}`);
   }
 };
 
 exports.info = {
   name: 'globalmsg',
-  aliases: ['globalmessage'],
+  aliases: ['globalmessage','globalmsg','msgglobal','messageglobal'],
   usage: 'globalmsg <message>',
   description: 'Sends a global message to every guild the bot is in to the guilds default channel'
 };
