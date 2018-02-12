fffd26ed7c2f802ee608f34b698c3d1b1083d5b6
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 19:38:31 -0500

    Update play.js

--- src/commands/Music/play.js
+++ src/commands/Music/play.js
@@ -1,9 +1,15 @@
 exports.run = async (bot, msg) => {
     console.log("music playing!");
 };
-
+let hide;
+if (!bot.config.usemusic) {
+  hide = true;
+} else {
+  hide = false;
+}
 exports.info = {
   name: 'play',
+  hidden: `${hide}`,
   usage: 'play <url>|<search string>',
   description: 'Play a video/music. from (Youtube, Vimeo, YouKo, etc). You can also search using a string.'
 };
