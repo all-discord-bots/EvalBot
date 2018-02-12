9e5914f13c30a4b9da8eff8f42aa6b93d8ed4961
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 21:48:09 -0500

    Update prefix.js

--- src/commands/Utility/prefix.js
+++ src/commands/Utility/prefix.js
@@ -1,1 +1,15 @@
-exports.run = async (bot, msg, args) => {
+exports.run = async (bot, msg) => {
+	if (msg.author.id !== bot.config.botCreatorID) {
+		if (!msg.member.hasPermission('MANAGE_SERVER')) return;
+	}
+	let newPrefix = msg.content.split(' ').slice(1).join(' ');
+	//var globalPrefOld = bot.config.[msg.guild.id].prefix;
+	bot.config.set(msg.guild.id, {prefix: newPrefix})
+	msg.channel.send(`Prefix changed to ${newPrefix}`);
+};
+
+exports.info = {
+	name: 'prefix',
+	usage: 'prefix <prefix>',
+	description: 'Change the prefix of the bot'
+};
