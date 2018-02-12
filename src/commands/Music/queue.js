d42e92faa96172adf5745e51b22658fb482aa359
BannerBomb <BannerBomb55@gmail.com>
2018-02-11 19:39:01 -0500

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,9 +1,15 @@
 exports.run = async (bot, msg) => {
 console.log("view queue");
 };
-
+let hide;
+if (!bot.config.usemusic) {
+  hide = true;
+} else {
+  hide = false;
+}
 exports.info = {
   name: 'queue',
+  hidden: `${hide}`,
   usage: 'queue',
   description: 'Display the current queue.'
 };
