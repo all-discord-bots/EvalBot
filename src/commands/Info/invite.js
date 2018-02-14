991d59b77e50859907c0070893fc2ef317a2233d
BannerBomb <BannerBomb55@gmail.com>
2018-02-14 09:11:09 -0500

    Update invite.js

--- src/commands/Info/invite.js
+++ src/commands/Info/invite.js
@@ -1,18 +1,21 @@
 exports.run = async (bot, msg) => {
-    (await msg.channel.send("Loading bot invitation link...").then((msg)=>{
-        msg.edit({
+        msg.channel.send({
         embed: bot.utils.embed(`${bot.user.username} Invite`, '***Invite***', [
             {
-                name: '**Invite Link**:',
+                name: '**Bot Invite**:',
                 value: `https://discordapp.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot`,
+            }, {
+                name: '**Support Server**:',
+                value: 'https://discord.gg/AY6z42D',
             }
-        ], { inline: false })
+        ], { inline: true })
     });
   }));
 };
 
 exports.info = {
     name: 'invite',
+    aliases: 'support',
     usage: 'invite',
     description: 'Gives you a invite link for the bot'
 };
