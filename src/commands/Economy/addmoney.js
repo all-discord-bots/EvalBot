36479ac1cc5e85e515351b4e3085bae0f52e5826
BannerBomb <BannerBomb55@gmail.com>
2018-02-16 20:39:33 -0500

    Update addmoney.js

--- src/commands/Economy/addmoney.js
+++ src/commands/Economy/addmoney.js
@@ -7,31 +7,31 @@ exports.run = async (bot, msg, args) => {
     if (!msg.member.roles.find("name", botCommanderRole) || !msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Manage Server\` or the ${botCommanderRole} role`);
     //msg.channel.send('**You need the role `' + botCommanderRole + '` to use this command...**');
   }
-  if (!args[0]) {
-    msg.channel.send(`<:redx:411978781226696705> **You need to define an amount. Usage: ${bot.config.prefix}add-money <amount> <user>**`);
-    return;
-  }
-  if (isNaN(args[0])) {
-    msg.channel.send(`<:redx:411978781226696705> **The amount has to be a number. Usage: ${bot.config.prefix}add-money <amount> <user>**`);
-    return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
-  }
   // Check if they defined a user
   let defineduser = '';
-  if (!args[1]) { // If they didn't define anyone, set it to their own.
+  if (!args[0]) { // If they didn't define anyone, set it to their own.
     defineduser = msg.author.id;
   } else { // Run this if they did define someone...
     let firstMentioned = msg.mentions.users.first();
     defineduser = firstMentioned.id;
   }
+  if (!args[1]) {
+    msg.channel.send(`<:redx:411978781226696705> **You need to define an amount. Usage: ${bot.config.prefix}add-money [user] <amount>**`);
+    return;
+  }
+  if (isNaN(args[1])) {
+    msg.channel.send(`<:redx:411978781226696705> **The amount has to be a number. Usage: ${bot.config.prefix}add-money [user] <amount>**`);
+    return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+  }
   // Finally, run this.. REMEMBER IF you are doing the guild-unique method, make sure you add the guild ID to the end,
-  economy.updateBalance(defineduser + msg.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
-    msg.channel.send(`<:check:411976443522711552> **User defined had ${args[0]} added/subtracted from their account.**`);
+  economy.updateBalance(defineduser + msg.guild.id, parseInt(args[1])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
+    msg.channel.send(`<:check:411976443522711552> **User defined had ${args[1]} added/subtracted from their account.**`);
   });
 };
 
 exports.info = {
   name: 'add-money',
   aliases: ['remove-money','addmoney','removemoney'],
-  usage: 'add-money <amount> <user>',
+  usage: 'add-money [user] <amount>',
   description: 'add/subtract money from a users balance'
 };
