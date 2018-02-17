8c8c41305e76c03f5a4a22213e35b3a3cda8ba8c
BannerBomb <BannerBomb55@gmail.com>
2018-02-16 20:35:28 -0500

    Update addmoney.js

--- src/commands/Economy/addmoney.js
+++ src/commands/Economy/addmoney.js
@@ -3,17 +3,16 @@ const economy = require('discord-eco');
 
 exports.run = async (bot, msg, args) => {
   if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.roles.find("name", bot.config.cripsBotModeratorRole)) {
-      msg.channel.send('**You need the role `' + modRole + '` to use this command...**');
-      return;
-    }
+    let botCommanderRole = bot.config.botCommanderRole;
+    if (!msg.member.roles.find("name", botCommanderRole) || !msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Manage Server\` or the ${botCommanderRole} role`);
+    //msg.channel.send('**You need the role `' + botCommanderRole + '` to use this command...**');
   }
   if (!args[0]) {
-    msg.channel.send(`**You need to define an amount. Usage: ${bot.config.prefix}add-money <amount> <user>**`);
+    msg.channel.send(`<:redx:411978781226696705> **You need to define an amount. Usage: ${bot.config.prefix}add-money <amount> <user>**`);
     return;
   }
   if (isNaN(args[0])) {
-    msg.channel.send(`**The amount has to be a number. Usage: ${bot.config.prefix}add-money <amount> <user>**`);
+    msg.channel.send(`<:redx:411978781226696705> **The amount has to be a number. Usage: ${bot.config.prefix}add-money <amount> <user>**`);
     return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
   }
   // Check if they defined a user
@@ -26,7 +25,7 @@ exports.run = async (bot, msg, args) => {
   }
   // Finally, run this.. REMEMBER IF you are doing the guild-unique method, make sure you add the guild ID to the end,
   economy.updateBalance(defineduser + msg.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
-    msg.channel.send(`✅ **User defined had ${args[0]} added/subtracted from their account.**`);
+    msg.channel.send(`<:check:411976443522711552> **User defined had ${args[0]} added/subtracted from their account.**`);
   });
 };
 
