f9a095289626c1fe51ebf838dc104bdab3f0b258
BannerBomb <BannerBomb55@gmail.com>
2018-02-16 20:12:41 -0500

    Update massdm.js

--- src/commands/Utility/massdm.js
+++ src/commands/Utility/massdm.js
@@ -5,12 +5,12 @@ exports.run = async (bot, msg, args) => {
 		if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments provided!`);
 		let gmessage = args.join(' ');
 		msg.guild.members.filter(user => user.user.username != msg.author.username).filter(user => user.user.id != bot.config.botCreatorID).forEach(user => user.sendMessage(`${gmessage}`));
-		msg.channel.send(`<:check:411976443522711552> everyone has been successfully direct messaged.`);
+		msg.channel.send(`<:check:411976443522711552> successfully set announcement to everyones direct messages.`);
 };
 
 exports.info = {
 	name: 'massdm',
 	aliases: ['mdm','massdirectmessage','masspm','massprivatemessage','mpm'],
 	usage: 'massdm [message]',
-	description: 'Message everyone in the server, except the user that invoked the command.'
+	description: 'Message everyone in the server, except the user that invoked the command. This command is used for major announcements.'
 };
