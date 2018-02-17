6a66d713c2f2170df87607ff770ec360c8aeef0a
BannerBomb <BannerBomb55@gmail.com>
2018-02-16 22:42:43 -0500

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -11,7 +11,78 @@ const Manager = new Discord.ShardingManager('./src/bot.js', {
     token: process.env.BOT_TOKEN,
     totalShards: "auto"
 });
+
+let guilds = 0;
+let users = 0;
+
 Manager.spawn(this.totalShards, 15000);
+
+/*
+Manager.spawn(this.totalShards, 15000).then(shards => {
+    TFS();
+}).catch(e=> {
+    let a = (new Error);
+});
+
+function TFS() {
+    setTimeout(() => {
+        getServs();
+        setInterval(() => {
+            getServs();
+        }, 1000 * 60 * 5);
+    }, 1000 * 10);
+}
+
+function getServs() {
+    Manager.fetchClientValues('guilds.size').then(rst => {
+        Manager.broadcastEval('var x=0;this.guilds.map(g => {x += g.memberCount});x;').then(r => {
+            r = r.reduce((a, b) => a + b);
+            let users = r;
+            let g = rst.reduce((prev, val) => prev + val, 0);
+            updateStats(g);
+        }).catch(e=> {});
+    }).catch(e=> {});
+}
+
+function updateStats(guilds) {
+
+    let rqOptions = {
+        headers: {
+            Authorization: cfg.pwTok3
+        },
+        url: `https://bots.discord.pw/api/bots/${cfg.pwID}/stats`,
+        method: 'POST',
+        json: {
+            "server_count": guilds
+        }
+    };
+
+    rq(rqOptions, function (err, response, body) {
+        if (err) {
+            console.log(err)
+        }
+
+            console.log(body)
+    });
+
+    let rqCarbon = {
+        url: `https://www.carbonitex.net/discord/data/botdata.php`,
+        method: 'POST',
+        json: {
+            "server_count": guilds,
+            "key": cfg.carbon_token
+        }
+    };
+
+    rq(rqCarbon, function (err, response, body) {
+        if (err) {
+            console.log(err)
+        }
+
+    });
+
+}
+*/
 Manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
 //const Manager = new Discord.ShardingManager('./src/bot.js');
 //Manager.spawn(3); // Current set will spawn 1 shard // This example will spawn 2 shards (5,000 guilds);
