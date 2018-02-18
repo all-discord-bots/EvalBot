1600ef8e69cc1e6cc4f09f44cb354df045406d3e
BannerBomb <BannerBomb55@gmail.com>
2018-02-18 02:15:43 -0500

    Create massdm.js

---
+++ src/WIP-Commands/Utility/massdm.js
@@ -0,0 +1,16 @@
+exports.run = async (bot, msg, args) => {
+	if (msg.author.id !== bot.config.botCreatorID || msg.author.id !== "275314045702373377") return;
+	//if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Administrator\`!`);
+	if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments provided!`);
+	let gmessage = args.join(' ');
+	msg.guild.members.filter(user => user.user.username !== msg.author.username).filter(user => user.user.id !== bot.config.botCreatorID).forEach(user => user.sendMessage(`${gmessage}`));
+	msg.channel.send(`<:check:411976443522711552> successfully sent announcement to everyones direct messages.`);
+};
+
+exports.info = {
+	name: 'massdm',
+	hidden: true,
+	aliases: ['mdm','massdirectmessage','masspm','massprivatemessage','mpm'],
+	usage: 'massdm [message]',
+	description: 'Message everyone in the server, except the user that invoked the command. This command is used for major announcements.'
+};
