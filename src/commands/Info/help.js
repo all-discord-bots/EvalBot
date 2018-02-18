f035ff52dcc4d79d91c3de218584322469c44c8f
BannerBomb <BannerBomb55@gmail.com>
2018-02-18 06:09:06 -0500

    Update help.js

--- src/commands/Info/help.js
+++ src/commands/Info/help.js
@@ -6,26 +6,78 @@ exports.run = async (bot, msg, args) => {
     let title = 'Categories';
 
     if (args.length > 0) {
-        if (/^category|module|type$/i.test(args[0])) {
-            if (args.length < 2) {
-                throw 'You must specify a category!';
-            }
-
-            commands = bot.commands.all(args[1]);
-            title = `${args[1]} Commands`;
+		let categories = bot.commands.categories().sort();
+		let module = categories.map(c => `${c.toLowerCase()}`);
+		let fcmd = bot.commands.get(args[0].toLowerCase());
+		let aliases = ["NOTHING_HERE", "NOTHING_HERE_EITHER"];
+		if (fcmd && fcmd.info.name) {
+			aliases = fcmd.info.aliases;
+		}
+		let gcmd;
+		let gali;
+		if (fcmd) {
+			gcmd = fcmd.info.name;
+			if (fcmd.info.aliases) {
+				gali = fcmd.info.aliases;
+			} else {
+				gali = "THIS_IS_A_FAKE_ALIASES_TO_FORCE_A_FALSE_STATEMENT";
+			}
+		} else {
+			gcmd = "THIS_IS_A_FAKE_COMMAND_TO_FORCE_A_FALSE_STATEMENT";
+			gali = "THIS_IS_A_FAKE_ALIASES_TO_FORCE_A_FALSE_STATEMENT";
+		}
+		if (args.length === 1 && new RegExp(module.join("|")).test(args[0].toLowerCase()) || /^category:|module:|type:/i.test(args[0].toLowerCase())) {
+			if (new RegExp(module.join("|")).test(gcmd.toLowerCase()) === true || new RegExp(module.join("|")).test(gali.toLowerCase()) === true) {
+				return msg.channel.send(` a command and module with the same name was found please use \`command:${args[0].toLowerCase()}\` or \`module:${args[0].toLowerCase()}\` or \`command ${args[0]}!\`!`);
+			}
+        //if (/^category|module|type$/i.test(args[0])) {
+            //if (args.length < 1) { // 2
+            //    throw 'You must specify a module!';
+            //}
+			let getargs;
+			if (/^category:|module:|type:/i.test(args[0].toLowerCase())) {
+				getargs = args[0].replace(/^category:|module:|type:/gi, "");
+			} else {
+				getargs = args[0];
+			}
+            commands = bot.commands.all(getargs);
+            title = `${getargs.toLowerCase()} Commands`;
         } else if (/^all|full|every$/i.test(args[0])) {
             commands = bot.commands.all();
             title = 'All Commands';
-        } else {
-            let command = bot.commands.get(args[0]);
+        } else if (args.length === 1 && new RegExp(commands.join("|")).test(args[0].toLowerCase()) === true || new RegExp(aliases.join("|")).test(args[0].toLowerCase()) === true || /^command:|cmd:/i.test(args[0].toLowerCase())) {
+			//if (new RegExp(module.join("|")).test(gcmd.toLowerCase()) === true || new RegExp(module.join("|")).test(gali.toLowerCase()) === true) {
+			//	return msg.channel.send(` a command and module with the same name was found please use \`command:${args[0].toLowerCase()}\` or \`module:${args[0].toLowerCase()}\`!`);
+			//}
+			let gargs;
+			if (/^command:|cmd:/i.test(args[0].toLowerCase())) {
+				gargs = args[0].replace(/^command:|cmd:/gi, "");
+			} else {
+				gargs = args[0];
+			}
+            let command = bot.commands.get(gargs);
             if (!command) {
                 throw `The command '${args[0]}' does not exist!`;
             }
 
             commands = [command];
-            title = `Help for \`${command.info.name}\``;
-        }
-    }
+            title = `Help for \`${command.info.name.toLowerCase()}\``;
+        } else if (/^command|cmd$/i.test(args[0]) && args.length > 1) {
+			let command = bot.commands.get(args[1]);
+			if (!command) {
+				throw `The command '${args[1]}' does not exist!`;
+			}
+			commands = [command];
+			title = `Help for \`${command.info.name.toLowerCase()}\``;
+		} else {
+			let command = bot.commands.get(args[0]);
+			if (!command) {
+				throw `The command '${args[0]}' does not exist!`;
+			}
+			commands = [command];
+			title = `Help for \`${command.info.name.toLowerCase()}\``;
+		}
+	}
 
     if (commands.length > 0) {
         let fields = commands.filter(c => !c.info.hidden)
@@ -62,19 +114,17 @@ exports.run = async (bot, msg, args) => {
         });
     } else {
         let categories = bot.commands.categories().sort();
-        await(msg.channel.send("Loading...").then((msg)=>{
-         msg.edit({
+         msg.channel.send({
             embed: bot.utils.embed(title, stripIndents`
             **Available categories:**
             ${categories.map(c => `- __${c}__`).join('\n')}
 
             **Usage:**
-            Do \`${bot.config.prefix}help category <name>\` for a list of commands in a specific category.
+            Do \`${bot.config.prefix}help module <name>\` or \`${bot.config.prefix}help [module:<name>]\` for a list of commands in a specific category.
             Do \`${bot.config.prefix}help all\` for a list of every command available in this bot.
-            Do \`${bot.config.prefix}help <command>\` for **extended** command help and command options.`)
+            Do \`${bot.config.prefix}help <command>\` or \`${bot.config.prefix}help command <command>\` or \`${bot.config.prefix}help command:<command>\` for **extended** command help and command options.`)
         });
-      }));
-    }
+	}
 };
 
 const getHelp = (bot, command, single) => {
@@ -119,6 +169,6 @@ if (command.info.aliases === undefined || command.info.aliases == "") {
 
 exports.info = {
     name: 'help',
-    usage: 'help all|[command]|[category <name>]',
+    usage: 'help all|[command]|[command <command>]|[module:<command>]|[command:<command>]',
     description: 'Shows you help for all commands or just a single command'
 };
