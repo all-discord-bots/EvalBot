1deed2476fd7cf06eb04d670c8f9d1fadebb064e
BannerBomb <BannerBomb55@gmail.com>
2018-02-18 07:43:02 -0500

    Update listemojis.js

--- src/commands/Utility/listemojis.js
+++ src/commands/Utility/listemojis.js
@@ -1,10 +1,11 @@
 exports.run = async (bot, msg) => {
   if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('ADMINISTRATOR') || !msg.member.hasPermission('manageServer')) return;
+    if (!msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(`<:redx:411978781226696705> you are missing permissions \`Manage Server\`!`);
   }
   
   const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
   if (emojiList.length < 1) return msg.channel.send(`<:redx:411978781226696705> This server does not have any emojis!`);
+  if (emojiList.length > 1999) return msg.channel.send(`<:redx:411978781226696705> The message exceeds 2000 characters!`);
   msg.channel.send(emojiList);
 };
 
