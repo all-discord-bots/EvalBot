a0955d85305f5d6854b14212e6f06dad06c10b89
BannerBomb <BannerBomb55@gmail.com>
2018-02-18 08:10:29 -0500

    Create info.js

---
+++ src/commands/Info/info.js
@@ -0,0 +1,82 @@
+const formatTime = (time) => {
+    let seconds = time[0] + time[1] / 1e9;
+
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds % 60;
+
+    let hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+    return `${hours.toFixed(0)}h ${minutes.toFixed(0)}m ${seconds.toFixed(0)}s`;
+};
+
+const activityTypes = [
+    'playing',
+    'streaming',
+    'listening to',
+    'watching',
+];
+
+exports.run = async (bot, msg) => {
+    const game = bot.user.presence.game || {};
+    (await msg.channel.send("Loading stats...").then((msg)=>{
+        msg.edit({
+        embed: bot.utils.embed(`${bot.user.username} Stats`, '***Stats***', [
+//            {
+//                name: ':outbox_tray: Messages sent',
+//                value: bot.managers.stats.get('messages-sent') || 0,
+//            },
+//            {
+//                name: ':inbox_tray: Messages received',
+//                value: bot.managers.stats.get('messages-received') || 0,
+//            },
+//            {
+//                name: ':mailbox: Mentions',
+//                value: bot.managers.stats.get('mentions') || 0
+//            },
+            {
+                name: ':baby: Users',
+                value: `${bot.guilds.reduce((mem, g) => mem += g.memberCount, 0)}`,
+            },
+            {
+                name: ':desktop: Servers',
+                value: `${bot.guilds.size.toLocaleString()}`,
+            },
+            {
+                name: ':keyboard: Channels',
+                value: `${bot.channels.size.toLocaleString()}`,
+            },
+            {
+                name: ':thinking: RAM usage',
+                value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+            },
+            {
+                name: ':stopwatch: Shard Uptime',
+                value: formatTime(process.hrtime(bot.managers.stats.get('start-time')))
+            },
+            {
+                name: ':book: Library',
+                value: 'discord.js',
+            },
+//            {
+//                name: ':video_game: Game',
+//                value: (game.name) ? `*${activityTypes[game.type]}* ${game.name} ${game.streaming ? `[(Streaming)](${game.url})` : ''}` : 'none'
+//            },
+            {
+                name: ':robot: Total Shards',
+                value: `${bot.options.shardCount.toLocaleString()}`,
+            },
+            {
+                name: ':1234: Shard ID',
+                value: `${bot.options.shardId.toLocaleString()}`,
+            }
+        ], { inline: true })
+    });
+  }));
+};
+
+exports.info = {
+    name: 'info',
+    aliases: ['botinfo','bot-info'],
+    usage: 'info',
+    description: 'Shows you stats about the bot'
+};
