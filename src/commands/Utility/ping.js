06755a15952b84126df0f21a4119de345eb1d09d
BannerBomb <BannerBomb55@gmail.com>
2018-02-18 21:40:43 -0500

    Update ping.js

--- src/commands/Utility/ping.js
+++ src/commands/Utility/ping.js
@@ -1,16 +1,33 @@
 exports.run = async (bot, msg) => {
-  // const m = await msg.channel.send("Pinging...");
-  var start = new Date().getTime();
-  (await msg.channel.send("Pinging...").then((msg)=> {
-    var end = new Date().getTime();
-    //msg.edit(`Latency: \`${msg.createdTimestamp - msg.createdTimestamp}ms\`\nAPI Latency: \`${Math.round(bot.ping)}ms\``);
-    msg.edit(`Latency: \`${end-start}ms\`\nAPI Latency: \`${Math.round(bot.ping)}ms\``);
-  }));
+	let start = new Date().getTime();
+	(await msg.channel.send({ embed: ({ author: { name: 'Pinging...', icon_url: 'http://4.bp.blogspot.com/-JF6M1HaI9rQ/VD_eCkLpG7I/AAAAAAAAAXk/0f1ym7hBXYs/s1600/Loading-Circle.gif', }})}).then((msg) => {
+		let end = new Date().getTime();
+		let bpings;
+		if (bot.pings.length < 1) {
+			bpings = "N/A";
+		} else {
+			bpings = bot.pings[0];
+		}
+		msg.edit("Collecting message edit latency...").then((msg) => {
+			let edited = msg.editedTimestamp;
+			msg.edit({ embed: ({
+				color: 631398,
+				title: 'Ping Data',
+				description: `
+Latency: \`${end-start}ms\`
+API Latency: \`${Math.round(bot.ping)}ms\`
+Heartbeat: \`${Math.floor(bpings)}ms\`
+Message Edit: \`${edited-start}ms\`
+`
+			})});
+		});
+	}));
 };
 
 exports.info = {
-  name: 'ping',
-  hidden: true,
-  usage: 'ping',
-  description: 'Get the API Latency'
+	name: 'ping',
+	aliases: ['pong'],
+	hidden: true,
+	usage: 'ping',
+	description: 'Get the Latency of the bot.'
 };
