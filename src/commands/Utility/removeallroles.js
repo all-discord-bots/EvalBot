61d6434a659ce5751fef1c2e2c727282f3e35409
BannerBomb <BannerBomb55@gmail.com>
2018-02-19 20:04:31 -0500

    Update removeallroles.js

--- src/commands/Utility/removeallroles.js
+++ src/commands/Utility/removeallroles.js
@@ -1,22 +1,22 @@
 exports.run = async (bot, msg, args) => {
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send("You are missing permission `Administrator`!");
-  }
-  if (args.length < 1) {
-    return msg.channel.send("Invalid arguments!");
-  }
-    if (args[1].toLowerCase() != "name" || args[1].toLowerCase() != "id") {
-      return msg.channel.send("Invalid arguments!");
-  }
-  if (args[0].toLowerCase() == "name") {
-    msg.guild.roles.find('name', `${args[1]}`).members.map(m => m.removeRole('name', `${args[1]}`));
-  } else if (args[0].toLowerCase() == "id") {
-    msg.guild.roles.find('id', `{args[1]}`).members.map(m => m.removeRole('id', `${args[1]}`));
-  }
+	if (msg.author.id !== bot.config.botCreatorID) {
+		if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing permission \`Administrator\`!`);
+	}
+	if (args.length < 1) {
+		return msg.channel.send(`<:redx:411978781226696705> Invalid arguments!`);
+	}
+	let grole;
+	if (msg.guild.roles.find(`name`, `${args[0]}`)) {
+		grole = msg.guild.roles.find(`name`, `${args[0]}`);
+	} else if (msg.guild.roles.find(`id`, `${args[0]}`)) {
+		grole = msg.guild.roles.find(`name`, `${args[0]}`);
+	}
+	msg.guild.members.filter(m => !m.user.bot).filter(m => m.user.roles.has(grole.id)).map(m => m.user.removeRole(grole));
+	msg.channel.send(`<:check:411976443522711552> Successfully removed \`${grole.name}\` from all members.`);
 };
-  
+
 exports.info = {
-  name: 'takeallrole',
-  usage: 'takeallrole <name|id> <role name|role id>',
-  description: 'Remove a role from every member in your server if they have it'
+	name: 'takeallrole',
+	usage: 'takeallrole <role>',
+	description: 'Remove a role from every member in your server if they have it.'
 };
