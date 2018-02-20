48cf74ad1bbe45ee5215082d19d2eab445552ccd
BannerBomb <BannerBomb55@gmail.com>
2018-02-19 20:09:44 -0500

    Update removeallroles.js

--- src/commands/Utility/removeallroles.js
+++ src/commands/Utility/removeallroles.js
@@ -11,7 +11,7 @@ exports.run = async (bot, msg, args) => {
 	} else if (msg.guild.roles.find(`id`, `${args[0]}`)) {
 		grole = msg.guild.roles.find(`name`, `${args[0]}`);
 	}
-	msg.guild.members.filter(m => !m.user.bot).filter(m => m.user.roles.has(grole.id)).map(m => m.user.removeRole(grole));
+	msg.guild.members.filter(m => !m.user.bot).filter(m => m.roles.has(grole.id)).map(m => m.user.removeRole(grole));
 	msg.channel.send(`<:check:411976443522711552> Successfully removed \`${grole.name}\` from all members.`);
 };
 
