5f194aa4c2687b12aee568f474369691cd27691a
BannerBomb <BannerBomb55@gmail.com>
2018-02-19 20:10:05 -0500

    Update addallroles.js

--- src/commands/Utility/addallroles.js
+++ src/commands/Utility/addallroles.js
@@ -1,6 +1,6 @@
 exports.run = async (bot, msg, args) => {
 	if (msg.author.id !== bot.config.botCreatorID) {
-		if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing permission \`Administrator\`!`);
+		if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Administrator\`!`);
 	}
 	if (args.length < 1) {
 		return msg.channel.send(`<:redx:411978781226696705> Invalid arguments!`);
@@ -11,7 +11,7 @@ exports.run = async (bot, msg, args) => {
 	} else if (msg.guild.roles.find(`id`, `${args[0]}`)) {
 		grole = msg.guild.roles.find(`name`, `${args[0]}`);
 	}
-	msg.guild.members.filter(m => !m.user.bot).filter(m => !m.user.roles.has(grole.id)).map(m => m.user.addRole(grole));
+	msg.guild.members.filter(m => !m.user.bot).filter(m => !m.roles.has(grole.id)).map(m => m.user.addRole(grole));
 	msg.channel.send(`<:check:411976443522711552> Successfully gave all members \`${grole.name}\``);
 };
 
