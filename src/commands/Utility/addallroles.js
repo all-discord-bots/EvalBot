de1d089c0e7e20199c1c425aec668d144a76d9ed
BannerBomb <BannerBomb55@gmail.com>
2018-02-19 19:57:10 -0500

    Update addallroles.js

--- src/commands/Utility/addallroles.js
+++ src/commands/Utility/addallroles.js
@@ -1,19 +1,22 @@
 exports.run = async (bot, msg, args) => {
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send("You are missing permission `Administrator`!");
-  }
-  if (args.length < 1) {
-    return msg.channel.send("Invalid arguments!");
-  }
-  if (msg.guild.roles.find(`name`, `${args[0]}`)) {
-    msg.guild.roles.find(`name`, `${args[0]}`).members.filter(m => !m.user.bot).map(m => m.addRole(`name`, `${args[0]}`));
-  } else if (msg.guild.roles.find(`id`, `${args[0]}`)) {
-    msg.guild.roles.find(`id`, `${args[0]}`).members.filter(m => !m.user.bot).map(m => m.addRole(`id`, `${args[0]}`));
-  }
+	if (msg.author.id !== bot.config.botCreatorID) {
+		if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing permission \`Administrator\`!`);
+	}
+	if (args.length < 1) {
+		return msg.channel.send(`<:redx:411978781226696705> Invalid arguments!`);
+	}
+	let grole;
+	if (msg.guild.roles.find(`name`, `${args[0]}`)) {
+		grole = msg.guild.roles.find(`name`, `${args[0]}`);
+	} else if (msg.guild.roles.find(`id`, `${args[0]}`)) {
+		grole = msg.guild.roles.find(`name`, `${args[0]}`);
+	}
+	msg.guild.members.filter(m => !m.user.bot).filter(m => !m.user.roles.has(grole.id)).map(m => m.user.addRole(grole));
+	msg.channel.send(`<:check:411976443522711552> Successfully gave all members \`${grole.name}\``);
 };
-  
+
 exports.info = {
-  name: 'giveallrole',
-  usage: 'giveallrole <role name|role id>',
-  description: 'Add a role to every member in your server'
+	name: 'giveallrole',
+	usage: 'giveallrole <role name|role id>',
+	description: 'Add a role to every member in your server.'
 };
