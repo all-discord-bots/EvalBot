e653a800548656812e6fd8858c9ac79f9f1c9478
BannerBomb <BannerBomb55@gmail.com>
2018-02-20 20:12:47 -0500

    Update addallroles.js

--- src/commands/Utility/addallroles.js
+++ src/commands/Utility/addallroles.js
@@ -1,9 +1,11 @@
 exports.run = async (bot, msg, args) => {
+	let gbot = msg.guild.members.get(bot.user.id);
+	if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
 	if (msg.author.id !== bot.config.botCreatorID) {
-		if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Administrator\`!`);
+		if (!msg.member.hasPermission('ADMINISTRATOR')) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Administrator\`!`).catch(console.error);
 	}
 	if (args.length < 1) {
-		return msg.channel.send(`<:redx:411978781226696705> Invalid arguments!`);
+		return msg.channel.send(`<:redx:411978781226696705> Invalid arguments!`).catch(console.error);
 	}
 	let grole;
 	if (msg.guild.roles.find(`name`, `${args[0]}`)) {
@@ -11,7 +13,7 @@ exports.run = async (bot, msg, args) => {
 	} else if (msg.guild.roles.find(`id`, `${args[0]}`)) {
 		grole = msg.guild.roles.find(`name`, `${args[0]}`);
 	}
-	msg.guild.members.filter(m => !m.user.bot).filter(m => !m.roles.has(grole.id)).map(m => m.addRole(grole));
+	msg.guild.members.filter(m => !m.user.bot).filter(m => !m.roles.has(grole.id)).map(m => m.addRole(grole)).catch(console.error);
 	msg.channel.send(`<:check:411976443522711552> Successfully gave all members \`${grole.name}\``);
 };
 
