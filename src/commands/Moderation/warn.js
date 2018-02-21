6b81f42573da876b2bbd3cda2606971d7047769f
BannerBomb <BannerBomb55@gmail.com>
2018-02-20 20:09:53 -0500

    Update warn.js

--- src/commands/Moderation/warn.js
+++ src/commands/Moderation/warn.js
@@ -9,14 +9,16 @@ exports.run = async (bot, msg, args) => {
   let mutedrole = "muted";
   let modlogs = "mod_logs";
   //!warn @daeshan <reason>
+  let gbot = msg.guild.members.get(bot.user.id);
+  if (!gbot.hasPermission(0x00000002)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Kick Members\`!`).catch(console.error);
   if (msg.author.id !== bot.config.botCreatorID) {
-    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Kick Members\`!`);
+    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Kick Members\`!`).catch(console.error);
   }
   let wUser = msg.guild.member(msg.mentions.users.first()) || msg.guild.members.get(args[0])
-  if(!wUser) return msg.channel.send(`<:redx:411978781226696705> Can't find user!`);
+  if(!wUser) return msg.channel.send(`<:redx:411978781226696705> Can't find that user!`).catch(console.error);
 //  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
   let reason = args.join(" ").slice(22);
-  if (!reason) return msg.channel.send(`<:redx:411978781226696705> Please provide a reason.`);
+  if (!reason) return msg.channel.send(`<:redx:411978781226696705> Please provide a reason.`).catch(console.error);
   if(!warns[wUser.id]) warns[wUser.id] = {
     warns: 0
   };
@@ -37,29 +39,29 @@ exports.run = async (bot, msg, args) => {
   .addField("Reason", reason);
 
   let warnchannel = msg.guild.channels.find(`name`, `${modlogs}`);
-  if(!warnchannel) return msg.channel.send(`<:redx:411978781226696705> Couldn't find ${modlogs} channel!`);
+  if(!warnchannel) return msg.channel.send(`<:redx:411978781226696705> Couldn't find ${modlogs} channel!`).catch(console.error);
 
   warnchannel.send(warnEmbed);
 
   if (automute) {
     if(warns[wUser.id].warns === 2){
       let muterole = msg.guild.roles.find(`name`, `${mutedrole}`);
-      if(!muterole) return msg.channel.send(`<:redx:411978781226696705> Cannot find ${mutedrole} role!`);
+      if(!muterole) return msg.channel.send(`<:redx:411978781226696705> Cannot find ${mutedrole} role!`).catch(console.error);
 
       let mutetime = "10s";
-      await(wUser.addRole(muterole.id));
+      await(wUser.addRole(muterole.id)).catch(console.error);
       msg.channel.send(`<:check:411976443522711552> <@${wUser.id}> has been temporarily muted.`);
 
       setTimeout(function() {
-        wUser.removeRole(muterole.id)
-        msg.channel.send(`<:check:411976443522711552> <@${wUser.id}> has been unmuted.`)
-      }, ms(mutetime))
+        wUser.removeRole(muterole.id).catch(console.error);
+        msg.channel.send(`<:check:411976443522711552> <@${wUser.id}> has been unmuted.`);
+      }, ms(mutetime));
     }
   }
   if (autoban) {
-    if(warns[wUser.id].warns === 3){
-      msg.guild.member(wUser).ban(reason);
-      msg.reply(`<:check:411976443522711552> <@${wUser.id}> has been banned.`)
+    if(warns[wUser.id].warns === 3) {
+      msg.guild.member(wUser).ban(reason).catch(err => msg.channel.send(`Could not ban ${wUser.username}: ${err}`));
+      msg.reply(`<:check:411976443522711552> <@${wUser.id}> has been banned.`);
     }
   }
 
