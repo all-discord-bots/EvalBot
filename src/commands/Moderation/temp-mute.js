a39c4d9b9e9e51499d3c98ccb1b53f5d6f165ae0
BannerBomb <BannerBomb55@gmail.com>
2018-02-20 20:05:44 -0500

    Update temp-mute.js

--- src/commands/Moderation/temp-mute.js
+++ src/commands/Moderation/temp-mute.js
@@ -4,11 +4,15 @@ const ms = require("ms");
 exports.run = async (bot, msg, args) => {
 let mutedrole = "Muted"; // name of the 'muted' role
   //!tempmute @user 1s/m/h/d
+  let gbot = msg.guild.members.get(bot.user.id);
+  if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
+  if (!gbot.hasPermission(0x00000002)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Kick Members\`!`).catch(console.error);
   if (msg.author.id !== bot.config.botCreatorID) {
-    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Kick Members\`!`);
+    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Kick Members\`!`).catch(console.error);
+    if(!msg.member.hasPermission("MANAGE_ROLES")) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Manage Roles\`!`).catch(console.error);
   }
   let tomute = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
-  if(!tomute) return msg.channel.send(`<:redx:411978781226696705> Couldn't find user.`);
+  if(!tomute) return msg.channel.send(`<:redx:411978781226696705> Can't find that user.`).catch(console.error);
   //if(tomute.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(`<:redx:411978781226696705> Can't mute them!`);
   let muterole = msg.guild.roles.find(`name`, `${mutedrole}`);
   //start of create role
@@ -18,7 +22,7 @@ let mutedrole = "Muted"; // name of the 'muted' role
         name: `${mutedrole}`,
         color: "#818386",
         permissions:[]
-      })
+      }).catch(console.error);
       msg.guild.channels.forEach(async (channel, id) => {
         await channel.overwritePermissions(muterole, {
           SEND_MESSAGES: false,
@@ -32,16 +36,16 @@ let mutedrole = "Muted"; // name of the 'muted' role
   //end of create role
   let mutetime = args[1];
   if(mutetime) {
-    await(tomute.addRole(muterole.id));
+    await(tomute.addRole(muterole.id)).catch(console.error);
     msg.channel.send(`<:check:411976443522711552> <@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
 
     setTimeout(function(){
-      tomute.removeRole(muterole.id);
+      tomute.removeRole(muterole.id).catch(console.error);
       msg.channel.send(`<@${tomute.id}> has been unmuted!`);
     }, ms(mutetime));
   } else if (!mutetime) {
     msg.channel.send(`<:check:411976443522711552> <@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-    await(tomute.addRole(muterole.id));
+    await(tomute.addRole(muterole.id)).catch(console.error);
   }
 
 
