1c3a05860ec2a3a2711c58e3b7024febee097242
BannerBomb <BannerBomb55@gmail.com>
2018-02-20 19:40:15 -0500

    Update kick.js

--- src/commands/Moderation/kick.js
+++ src/commands/Moderation/kick.js
@@ -5,9 +5,18 @@ exports.run = async (bot, message, args) => {
     let kUser = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
     if(!kUser) return msg.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
+    let gbot = msg.guild.members.get(bot.user.id);
+    if (!gbot.hasPermission(0x00000002)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Kick Members\`!`);
     if (msg.author.id !== bot.config.botCreatorID) {
         if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("You are missing the permissions `Kick Members`!");
     }
+    if(!msg.guild.member(kUser).kickable) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`);
+    
+   
+    if (msg.author.id !== bot.config.botCreatorID) {
+      if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Ban Members\`!`);
+    }
+    
 //    if(kUser.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("That person can't be kicked!");
 
     let kickEmbed = new Discord.RichEmbed()
@@ -22,7 +31,7 @@ exports.run = async (bot, message, args) => {
     let kickChannel = msg.guild.channels.find(`name`, `${modlogs}`);
     if(!kickChannel) return msg.channel.send(`Can't find ${modlogs} channel.`);
 
-    msg.guild.member(kUser).kick(kReason);
+    msg.guild.member(kUser).kick(kReason).catch(err => msg.channel.send(`I could not kick this user due to the error: ${err}`));
     kickChannel.send(kickEmbed);
 }
 
