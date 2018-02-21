a73140f077b7a94c319da32c23f4f41eece60dd2
BannerBomb <BannerBomb55@gmail.com>
2018-02-20 22:01:33 -0500

    Update role-id.js

--- src/commands/Utility/role-id.js
+++ src/commands/Utility/role-id.js
@@ -3,7 +3,12 @@ exports.run = async (bot, msg, args) => {
     return msg.channel.send("Invalid arguments! Please provide a role name.");
   }
   const rolename = args.join(" ");
-  let getroleid = msg.guild.roles.find('name', `${rolename}`).id;
+  let getroleid;
+  if (msg.guild.roles.find(`name`, `${rolename}`)) {
+    getroleid = msg.guild.roles.find(`name`, `${rolename}`).id;
+  } else {
+    return msg.channel.send(`<:redx:411978781226696705> No roles with the name ${rolename} exists!`).catch(cosole.error);
+  }
   msg.channel.send(`${getroleid}`);
 };
 
@@ -11,5 +16,5 @@ exports.info = {
   name: 'role-id',
   aliases: ['roleid'],
   usage: 'role-id <role>',
-  description: 'Easy and simple way to get the ID of any role.'
+  description: 'Easy and simple way to get the ID of any role. CASE SENSITIVE role name.'
 };
