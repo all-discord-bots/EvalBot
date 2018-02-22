fb63947d66c66eb1f0ed90afa26a9c113490ef73
BannerBomb <BannerBomb55@gmail.com>
2018-02-21 20:50:14 -0500

    Update pussy.js

--- src/commands/Nsfw/pussy.js
+++ src/commands/Nsfw/pussy.js
@@ -1,14 +1,14 @@
 const Pornhub = require("pornhub-api")
-const Videos = new Pornhub.Videos()
 
 exports.run = async (bot, msg, args) => {
+if (msg.author.id !== bot.config.botCreatorID) return;
 if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Please provide a search string!`).catch(console.error);
 if (!msg.channel.nsfw) return msg.channel.send(`<:redx:411978781226696705> This channel has not been marked as NSFW!`).catch(console.error);
 let gbot = msg.guild.members.get(bot.user.id);
 if (!gbot.hasPermission(0x00008000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Attach Files\`!`).catch(console.error);
 if (!msg.member.hasPermission('ATTACH_FILES')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Attach Files\`!`).catch(console.error);
 let searchqueue = args.join(' ');
-
+const Videos = new Pornhub.Videos();
 Videos.search({
     search: `${searchqueue}`
 }).then(videos => {
@@ -18,6 +18,7 @@ Videos.search({
 
 exports.info = {
   name: 'pornhub',
+  hidden: true,
   aliases: ['phub','ph','pornh','p0rnhub','cornhub','c0rnhub','p0rnh','porn-hub','p0rn-hub'],
   usage: 'pornhub <search>',
   description: 'Sends a video straight from pornhub.'
