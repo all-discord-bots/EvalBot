f0ddf839f9fd590c2b814d2ba2c07ecb60967235
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:31:19 -0500

    Update np.js

--- src/commands/Music/np.js
+++ src/commands/Music/np.js
@@ -1,1 +1,9 @@
+exports.run = async (bot, msg) => {
+	console.log('Now Playing command executed.');
+};
 
+exports.info = {
+	name: 'np',
+	usage: 'np',
+	description: 'Shows the currently playing song.'
+};
