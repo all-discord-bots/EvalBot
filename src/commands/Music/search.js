eeb5f7b91411c65841b364ddc6808fca843e4e05
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:45:14 -0500

    Update search.js

--- src/commands/Music/search.js
+++ src/commands/Music/search.js
@@ -1,1 +1,11 @@
+exports.run = async (bot, msg, args) => {
+	let arg = args.join(' ');
+	if (!arg) return;
+	console.log(`Searching for song: '${arg}'`);
+};
 
+exports.info = {
+	name: 'search',
+	usage: 'search <string>',
+	description: 'Searches for up to 10 results.'
+};
