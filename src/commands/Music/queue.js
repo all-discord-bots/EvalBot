4833374307383a25d029b1973fd55d38a6830199
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 13:58:37 -0500

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,1 +1,9 @@
+exports.run = async (bot, msg) => {
+	console.log('Executed queue commands.');
+};
 
+exports.info = {
+	name: 'queue',
+	usage: 'queue',
+	description: 'Display the current queue.'
+};
