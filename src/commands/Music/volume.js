7a64d4cde324c35a5698e9939d7d5cc26341bedf
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:06:35 -0500

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -1,1 +1,11 @@
+exports.run = async (bot, msg, args) => {
+	if (!args[0]) return msg.channel.send(`<:redx:411978781226696705> You must specify the volume!`).catch(console.error);
+	if (args[0] < 0) return msg.channel.send(`<:redx:411978781226696705> Volume can only be between \`0-200\`!`).catch(console.error);
+	if (args[0] > 200) return msg.channel.send(`<:redx:411978781226696705> Volume can only be between \`0-200\`!`).catch(console.error);
+};
 
+exports.info = {
+	name: 'volume',
+	usage: 'volume <number>',
+	description: 'Resume music playback.'
+};
