3a61c5471137febb94f491abbdce9b89511b0c86
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 13:56:28 -0500

    Update skip.js

--- src/commands/Music/skip.js
+++ src/commands/Music/skip.js
@@ -1,1 +1,20 @@
+exports.run = async (bot, msg, args) => {
+	//let arg = args.join(' ');
+	let skipped;
+	let s;
+	if (!args[0]) {
+		skipped = '1';
+		s = '';
+	} else if (args[0]) {
+		skipped = args[0];
+		s = 's';
+	}
+	console.log(`Skipped ${skipped} song${s}.`);
+	//if (args[0] < 1) return msg.channel.send(`<:redx:411978781226696705> Invalid number to skip.`).catch(console.error);
+};
 
+exports.info = {
+	name: 'skip',
+	usage: 'skip [number]',
+	description: 'Skip a song or multi songs.'
+};
