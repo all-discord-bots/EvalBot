9e18f55637f3484e04a81e02f3df3b6a68a47bf3
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:08:04 -0500

    Update leave.js

--- src/commands/Music/leave.js
+++ src/commands/Music/leave.js
@@ -1,1 +1,9 @@
+exports.run = async (bot, msg) => {
+	console.log('Bot left voice channel.');
+};
 
+exports.info = {
+	name: 'leave',
+	usage: 'leave',
+	description: 'Clears the song queue and leaves the channel.'
+};
