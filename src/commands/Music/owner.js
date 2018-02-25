697d5a52a602fdcdf8d227afbdd4e6d08be5be56
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:12:23 -0500

    Update owner.js

--- src/commands/Music/owner.js
+++ src/commands/Music/owner.js
@@ -1,1 +1,11 @@
+exports.run = async (bot, msg) => {
+	if (msg.author.id !== bot.config.botCreatorID) return;
+	console.log ('Owner commands executed');
+};
 
+exports.info = {
+	name: 'owner',
+	usage: 'owner',
+	hidden: true,
+	description: 'Various owner commands/actions. (W.I.P)'
+};
