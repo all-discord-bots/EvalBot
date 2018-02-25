39219cb935749824fb1887615372968886725f57
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:49:33 -0500

    Update set.js

--- src/commands/Music/set.js
+++ src/commands/Music/set.js
@@ -1,1 +1,12 @@
+exports.run = async (bot, msg, args) => {
+	if (msg.author.id !== bot.config.botCreatorID) return;
+	if (args.length < 1) return;
+	console.log('Executed the \'Set\' command');
+};
 
+exports.info = {
+	name: 'set',
+	usage: 'set <option> <new value>',
+	hidden: true,
+	description: 'Changes settings for the server. Use without specifing a setting to see valid settings.'
+};
