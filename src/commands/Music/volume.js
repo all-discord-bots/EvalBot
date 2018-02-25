3253d7a29b045edad8ec88a2481bc8a739564ebf
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:40:50 -0500

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -1,10 +1,15 @@
 exports.run = async (bot, msg, args) => {
-	let arg = args[0];
-	console.log(`Changed volume to ${arg}.`);
+	let newvolume;
+	if (!args[0] || args[0] < 1) {
+		newvolume = '0';
+	} else if (args[0]) {
+		newvolume = args[0];
+	}
+	console.log(`Changed volume to ${newvolume}.`);
 };
 
 exports.info = {
 	name: 'volume',
 	usage: 'volume <number>',
-	description: 'Resume music playback.'
+	description: 'Adjusts the volume of the bot.'
 };
