f7facbf28e8edb9ce9e6fdd90ce428dae2b254b6
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:59:23 -0500

    Update leave.js

--- src/commands/Music/leave.js
+++ src/commands/Music/leave.js
@@ -5,5 +5,6 @@ exports.run = async (bot, msg) => {
 exports.info = {
 	name: 'leave',
 	usage: 'leave',
+	hidden: true, // because this command is broken atm
 	description: 'Leave and clear the queue.'
 };
