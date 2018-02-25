d1f4382cd0b2dd942e462babba2d7b0301fbf1d1
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 14:22:59 -0500

    Update play.js

--- src/commands/Music/play.js
+++ src/commands/Music/play.js
@@ -1,6 +1,6 @@
 exports.run = async (bot, msg, args) => {
 	let arg = args.join(' ');
-	if (arg.length < 1) return msg.channel.send(`<:redx:411978781226696705> Please provide a search string or url.`).catch(console.error);
+	console.log(`Playing music: ${arg}`);
 };
 
 exports.info = {
