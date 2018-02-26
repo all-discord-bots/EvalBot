70c3902c14ad9168b247ebfea0f19685c6af914f
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 22:31:34 -0500

    Update resume.js

--- src/commands/Music/resume.js
+++ src/commands/Music/resume.js
@@ -1,8 +1,10 @@
 exports.run = async (bot, msg) => {
+	if (msg.author.id !== bot.config.botCreatorID) return;
 	console.log('Resumed song.');
 };
 
 exports.info = {
+	hidden: true,
 	name: 'resume',
 	usage: 'resume',
 	description: 'Resume music playback.'
