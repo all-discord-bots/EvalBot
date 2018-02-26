40b6467b1c54f277fc9409f9e7bb9bdb02f4fff5
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 22:30:18 -0500

    Update loop.js

--- src/commands/Music/loop.js
+++ src/commands/Music/loop.js
@@ -1,8 +1,10 @@
 exports.run = async (bot, msg) => {
+	if (msg.author.id !== bot.config.botCreatorID) return;
 	console.log('Looping song');
 };
 
 exports.info = {
+	hidden: true,
 	name: 'loop',
 	usage: 'loop',
 	description: 'Changes the loop state.'
