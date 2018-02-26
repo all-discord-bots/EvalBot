a3af7e303325e7a2137b15ccc0ebc958d2035b6a
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 22:29:57 -0500

    Update clearqueue.js

--- src/commands/Music/clearqueue.js
+++ src/commands/Music/clearqueue.js
@@ -1,8 +1,10 @@
 exports.run = async (bot, msg) => {
+	if (msg.author.id !== bot.config.botCreatorID) return;
 	console.log('Cleared queue.');
 };
 
 exports.info = {
+	hidden: true,
 	name: 'clearqueue',
 	usage: 'clearqueue',
 	description: 'Clears the current queue.'
