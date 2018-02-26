830375408944b41a100c11af0c478743a2d51963
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 20:47:41 -0500

    Update invite.js

--- src/commands/Info/invite.js
+++ src/commands/Info/invite.js
@@ -1,24 +1,23 @@
 exports.run = async (bot, msg) => {
-        msg.channel.send({
-        embed: bot.utils.embed(`${bot.user.username} Invite`, '***Invite***', [
-            {
-                name: '**Bot Invite**:',
-                //value: `[Invite](https://discordapp.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot)`,
-                    value: `[Invite](https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=8)`,
-            }, {
-                name: '**Support Server**:',
-                value: `[Join](https://discord.gg/AY6z42D)`,
-            }, {
-                name: '**Upvote Bot**:',
-                value: `[Upvote](https://discordbots.org/bot/${bot.user.id}/vote)`,
-            }
-        ], { inline: true })
-    });
+	msg.channel.send({
+		embed: bot.utils.embed(`${bot.user.username} Invite`, '***Invite***', [
+			{
+				name: '**Bot Invite**:',
+				value: `[Invite](https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&permissions=271969526&scope=bot)`,
+			}, {
+				name: '**Support Server**:',
+				value: `[Join](https://discord.gg/AY6z42D)`,
+			}, {
+				name: '**Upvote Bot**:',
+				value: `[Upvote](https://discordbots.org/bot/${bot.user.id}/vote)`,
+			}
+		], { inline: true })
+	});
 };
 
 exports.info = {
-    name: 'invite',
-    aliases: 'support',
-    usage: 'invite',
-    description: 'Gives you a invite link for the bot'
+	name: 'invite',
+	aliases: 'support',
+	usage: 'invite',
+	description: 'Gives you a invite link for the bot'
 };
