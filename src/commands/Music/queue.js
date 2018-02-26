e9fbaded3d7b0bfb6f0f544a2fcebbaa315172de
BannerBomb <BannerBomb55@gmail.com>
2018-02-25 22:31:18 -0500

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,8 +1,10 @@
 exports.run = async (bot, msg) => {
+	if (msg.author.id !== bot.config.botCreatorID) return;
 	console.log('Executed queue commands.');
 };
 
 exports.info = {
+	hidden: true,
 	name: 'queue',
 	usage: 'queue',
 	description: 'Shows the current queue.'
