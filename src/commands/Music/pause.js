590b4ac5e6871a2d7be30a9a655271d74d1b512d
BannerBomb <BannerBomb55@gmail.com>
2018-03-01 13:49:41 -0500

    Update pause.js

--- src/commands/Music/pause.js
+++ src/commands/Music/pause.js
@@ -1,10 +1,14 @@
 exports.run = async (bot, msg) => {
-	if (msg.author.id !== bot.config.botCreatorID) return;
-	console.log('Paused Song.');
+	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
+	const dispatcher = voiceConnection.player.dispatcher;
+	if (dispatcher.paused) return msg.channel.send(`<:redx:411978781226696705> Music already paused!`).catch(console.error);
+	if (!dispatcher.paused) {
+		dispatcher.pause();
+		msg.channel.send(`<:check:411976443522711552> Playback paused.`);
+	}
 };
 
 exports.info = {
-	hidden: true,
 	name: 'pause',
 	usage: 'pause',
 	description: 'Pause music playback.'
