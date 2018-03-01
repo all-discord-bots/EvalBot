c25eed4fb874d2437218c7436051acaaa4f92a34
BannerBomb <BannerBomb55@gmail.com>
2018-03-01 12:11:04 -0500

    Update and rename leave.js to stop.js

---
+++ src/commands/Music/stop.js
@@ -0,0 +1,13 @@
+exports.run = async (bot, msg) => {
+	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
+	if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`).catch(console.error);
+	if (voiceConnection !== null) return voiceConnection.disconnect().then(msg => { msg.channel.send(`<:check:411976443522711552> Successfully disconnected from voice channel!`) }).catch(console.error);
+	
+};
+
+exports.info = {
+	name: 'stop',
+	aliases: ['leave'],
+	usage: 'stop',
+	description: 'Leave and clear the queue.'
+};
