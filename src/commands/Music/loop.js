e09ba4b09effbb1fc5ab7fd73f1869bb77437255
BannerBomb <BannerBomb55@gmail.com>
2018-03-01 14:12:05 -0500

    Update loop.js

--- src/commands/Music/loop.js
+++ src/commands/Music/loop.js
@@ -1,6 +1,43 @@
+const musiccfg = require('../../../conf/musicconfig');
+// TODO: Setup FS functions to write to /conf/musicconfig.json
 exports.run = async (bot, msg) => {
 	if (msg.author.id !== bot.config.botCreatorID) return;
-	console.log('Looping song');
+	/*
+	musicbot.loopState = (server) => {
+    if (musicbot.global) return false;
+    if (!musicbot.loops[server]) {
+      musicbot.loops[server] = {
+        looping: false,
+        last: null
+      };
+    };
+    if (musicbot.loops[server].looping) return true;
+    else if (!musicbot.loops[server].looping) return false;
+  };
+  
+  musicbot.setLoopState = (server, state) => {
+    if (state && typeof state !== 'boolean') return console.log(`[loopingSet] ${new Error(`state wasnt a boolean`)}`);
+    if (!musicbot.loops[server]) {
+      musicbot.loops[server] = {
+        looping: false,
+        last: null
+      };
+    };
+    if (!state) return musicbot.loops[server].looping = false;
+    if (state) return musicbot.loops[server].looping = true;
+  };
+	*/
+/*	const voiceConnection = client.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
+	if (voiceConnection === null) return msg.channel.send(`No music is being played.`).catch(console.error);
+	const looping = musiccfg.loopState[msg.guild.id];
+	if (looping) {
+		musiccfg.setLoopState(msg.guild.id, false);
+		return msg.channel.send(`Looping disabled! :arrow_forward:`);
+	} else if (!looping) {
+		musiccfg.setLoopState(msg.guild.id, true);
+		return msg.channel.send(`Looping enabled! :repeat_one:`);
+	};
+*/
 };
 
 exports.info = {
