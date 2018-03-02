b16119b532287d3fc916403ee43563f7c9f68c82
BannerBomb <BannerBomb55@gmail.com>
2018-03-02 09:11:48 -0500

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,12 +1,12 @@
 require('../../../src/commands/Music/play.js');
 exports.run = async (bot, msg) => {
+	if (musicqueue.length < 1) return msg.channel.send(`<:redx:411978781226696705> Queue is empty.`).catch(console.error);
 	let i = 0;
 	let queue = musicqueue.map(a => a.toString());
-	 msg.channel.send(`\`\`\`${i++} - ${queue}\n\`\`\``);
+	msg.channel.send(`\`\`\`${i++} - ${queue}\n\`\`\``);
 };
 
 exports.info = {
-	hidden: true,
 	name: 'queue',
 	usage: 'queue',
 	description: 'Shows the current queue.'
