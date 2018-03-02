0984c5a3f0c64a6c2a63ba2d1c75aa814c59dce3
BannerBomb <BannerBomb55@gmail.com>
2018-03-02 14:03:18 -0500

    Update stop.js

--- src/commands/Music/stop.js
+++ src/commands/Music/stop.js
@@ -2,7 +2,9 @@ require('../../conf/globals.js');
 exports.run = async (bot, msg) => {
 	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
 	if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`).catch(console.error);
-	musicqueue[msg.guild.id].splice(0, musicqueue[msg.guild.id].length);
+	if (musicqueue[msg.guild.id] && musicqueue[msg.guild.id].length > 0) {
+		musicqueue[msg.guild.id].splice(0, musicqueue[msg.guild.id].length);
+	}
 	if (voiceConnection !== null) return voiceConnection.disconnect();
 	msg.channel.send(`<:check:411976443522711552> Successfully disconnected from voice channel!`);
 	
