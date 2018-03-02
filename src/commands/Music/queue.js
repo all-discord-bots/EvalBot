a8b570ca02c8e1f062c5fec8e14fd5a46d4e3856
BannerBomb <BannerBomb55@gmail.com>
2018-03-02 11:32:06 -0500

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,6 +1,6 @@
 require('../../conf/globals.js');
 exports.run = async (bot, msg) => {
-	if (musicqueue[msg.guild.id].length < 1) return msg.channel.send(`<:redx:411978781226696705> Queue is empty.`).catch(console.error);
+	if (!musicqueue[msg.guild.id]) return msg.channel.send(`<:redx:411978781226696705> Queue is empty.`).catch(console.error);
 	let i = 0;
 	let gqueue = musicqueue[msg.guild.id];
 	gqueue.map(list => msg.channel.send(`\`\`\`${i++} - ${list.toString()}\n\`\`\``));
