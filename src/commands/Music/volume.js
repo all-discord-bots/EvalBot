52a4d6ac1316f738be4011742d5f457367f596d4
BannerBomb <BannerBomb55@gmail.com>
2018-03-02 20:21:23 -0500

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -2,7 +2,7 @@ exports.run = async (bot, msg, args) => {
 	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
 	if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`).catch(console.error);
 	const dispatcher = voiceConnection.player.dispatcher;
-	if (args[0] < 0 || args[0] > 200) return msg.channel.send(`<:redx:411978781226696705> Volume must be \`0-100\`!`).catch(console.error);
+	if (args[0] < 0 || args[0] > 200) return msg.channel.send(`<:redx:411978781226696705> Volume must be \`0-200\`!`).catch(console.error);
 	let nvol;
 	if (args.length < 1 || isNaN(args[0])) {
 		nvol = parseInt('0');
@@ -10,7 +10,7 @@ exports.run = async (bot, msg, args) => {
 		nvol = parseInt(args[0]);
 	}
 	dispatcher.setVolume((nvol / 100));
-	msg.channel.send(`<:check:411976443522711552> Volume set to ${nvol}`);
+	msg.channel.send(`<:check:411976443522711552> Volume set to \`${nvol}\``);
 };
 
 exports.info = {
