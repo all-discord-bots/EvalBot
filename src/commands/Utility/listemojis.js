321f159e1011006e544ac1c3a844592ff114546b
BannerBomb <BannerBomb55@gmail.com>
2018-03-03 17:03:18 -0500

    Update listemojis.js

--- src/commands/Utility/listemojis.js
+++ src/commands/Utility/listemojis.js
@@ -1,16 +1,33 @@
 exports.run = async (bot, msg) => {
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(`<:redx:411978781226696705> you are missing permissions \`Manage Server\`!`);
-  }
-  
-  const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
-  if (emojiList.length < 1) return msg.channel.send(`<:redx:411978781226696705> This server does not have any emojis!`);
-  if (emojiList.length > 1999) return msg.channel.send(`<:redx:411978781226696705> The message exceeds 2000 characters!`);
-  msg.channel.send(emojiList);
+	if (msg.author.id !== bot.config.botCreatorID) {
+		if (!msg.member.hasPermission('MANAGE_EMOJIS')) return msg.channel.send(`<:redx:411978781226696705> you are missing permissions \`Manage Emojis\`!`);
+	}
+	
+	const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
+	if (emojiList.length < 1) return msg.channel.send(`<:redx:411978781226696705> This server does not have any emojis!`);
+	//if (emojiList.length > 1999) return msg.channel.send(`<:redx:411978781226696705> The message exceeds 2000 characters!`);
+	const MESSAGE_CHAR_LIMIT = 2000;
+	const splitString = (string, prepend = '', append = '') => {
+		if (string.length <= MESSAGE_CHAR_LIMIT) {
+			return [string];
+		}
+		const splitIndex = string.lastIndexOf('>', MESSAGE_CHAR_LIMIT - prepend.length - append.length); // string.lastIndexOf('\n',
+		const sliceEnd = splitIndex > 0 ? splitIndex : MESSAGE_CHAR_LIMIT - prepend.length - append.length;
+		const rest = splitString(string.slice(sliceEnd), prepend, append);
+		return [`${string.slice(0, sliceEnd)}${append}`, `${prepend}${rest[0]}`, ...rest.slice(1)];
+	};
+	let listemoji = splitString(`${emojiList}`);
+	listemoji.forEach(gemojis => {
+		msg.channel.send({embed: ({
+			color: 3447003,
+			title: `Emoji List`,
+			description: `${emojiList}`
+		})});
+	});
 };
 
 exports.info = {
-  name: 'listemojis',
-  usage: 'listemojis',
-  description: 'Displays all the emojis for the current server'
+	name: 'listemojis',
+	usage: 'listemojis',
+	description: 'Displays all the emojis for the current server'
 };
