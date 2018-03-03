43f40822a3730d004bef56ccc918bea1e47a0192
BannerBomb <BannerBomb55@gmail.com>
2018-03-03 09:35:49 -0500

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -3,7 +3,16 @@ exports.run = async (bot, msg) => {
 	if (musicqueue[msg.guild.id].length < 1 || !musicqueue[msg.guild.id]) return msg.channel.send(`<:redx:411978781226696705> Queue is empty.`).catch(console.error);
 	let i = 0;
 	let gqueue = musicqueue[msg.guild.id];
-	gqueue.map(list => msg.channel.send(`\`\`\`${i++} - ${list.toString()}\n\`\`\``));
+	gqueue.map(list => {
+		let hashtag;
+		if (i == 0){
+			hashtag = '# ';
+		} else {
+			hashtag = '';
+		}
+		msg.channel.send(`\`\`\`md\n${hashtag}${i} - ${list.toString()}\n\`\`\``);
+		i++;
+	});
 };
 
 exports.info = {
