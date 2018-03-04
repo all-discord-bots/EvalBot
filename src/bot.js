3d54ad03e08c1174f2032a150e3f9d588a0217ad
BannerBomb <BannerBomb55@gmail.com>
2018-03-04 00:11:05 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -70,12 +70,12 @@ const stats = bot.managers.stats = new Managers.Stats(bot);
 bot.deleted = new Discord.Collection();
 
 bot.setInterval(() => {
-    bot.deleted.clear();
+	bot.deleted.clear();
 }, 7200000);
 
 const settings = global.settings = {
-    dataFolder: path.resolve(__dirname, '..', 'data'),
-    configsFolder: path.resolve(__dirname, '..', 'data', 'configs')
+	dataFolder: path.resolve(__dirname, '..', 'data'),
+	configsFolder: path.resolve(__dirname, '..', 'data', 'configs')
 };
 
 if (!fse.existsSync(settings.dataFolder)) fse.mkdirSync(settings.dataFolder);
@@ -92,44 +92,41 @@ bot.on('ready', () => {
 		logger.severe(`${bot.user.username} is a bot, but you entered a selfbot token. Please follow the instructions at ${chalk.green('https://discordapp.com/developers')} and re-enter your token by running ${chalk.green('yarn run config')}.`);
 		process.exit(666);
 	}
-
-    // =======================================================
-    // === Until we know how to fix this, just make people ===
-    // === use the //status command to make the bot invis. ===
-    // =======================================================
-    // bot.user.setStatus('invisible');
-    //bot.user.setPresence({ game: { name: `${bot.config.prefix}help for commands`, type: 0 } }); // last presence [playing]
-    var s;
-    if (bot.guilds.size === 1) {
-        s = "";
-    } else {
-        s = "s";
-    }
+	
+	// =======================================================
+	// === Until we know how to fix this, just make people ===
+	// === use the //status command to make the bot invis. ===
+	// =======================================================
+	// bot.user.setStatus('invisible');
+	var s;
+	if (bot.guilds.size === 1) {
+		s = "";
+	} else {
+		s = "s";
+	}
 	bot.user.setPresence({ game: { name: `${bot.guilds.size} server${s}`, type: 3 } });
-    /*
-    0 - Playing
-    1 - Streaming // Defaults to "Playing" if not streaming
-    2 - Listening to
-    3 - Watching
-    */
-    // Fix mobile notifications
-    bot.user.setAFK(true);
-
-    commands.loadCommands();
-
-    (title => {
-        process.title = title;
-        process.stdout.write(`\u001B]0;${title}\u0007`);
-    })(`${bot.user.username}`);
-	//- User: ${bot.user.username}#${bot.user.discriminator} <ID: ${bot.user.id}>
-    logger.info(stripIndents`Stats:
-        - User: ${bot.user.tag} <ID: ${bot.user.id}>
-        - Users: ${bot.users.filter(user => !user.bot).size}
-        - Bots: ${bot.users.filter(user => user.bot).size}
-        - Channels: ${bot.channels.size}
-        - Guilds: ${bot.guilds.size}
-	- Shards: ${bot.shard.count}`
-    );
+	/*
+	0 - Playing
+	1 - Streaming // Defaults to "Playing" if not streaming
+	2 - Listening to
+	3 - Watching
+	*/
+	// Fix mobile notifications
+	bot.user.setAFK(true);
+	commands.loadCommands();
+	
+	(title => {
+		process.title = title;
+		process.stdout.write(`\u001B]0;${title}\u0007`);
+	})(`${bot.user.username}`);
+	logger.info(stripIndents`Stats:
+		- User: ${bot.user.tag} <ID: ${bot.user.id}>
+		- Users: ${bot.users.filter(user => !user.bot).size}
+		- Bots: ${bot.users.filter(user => user.bot).size}
+		- Channels: ${bot.channels.size}
+		- Guilds: ${bot.guilds.size}
+		- Shards: ${bot.shard.count}`
+	);
 	
 	stats.set('start-time', process.hrtime());
 	
@@ -138,8 +135,6 @@ bot.on('ready', () => {
 	
 	console.info(`Connected user ${bot.user.username}`)
 	logger.info('Bot loaded');
-	//bot.shards.forEach(s => logger.info(`Loaded shard ${s.id}`))
-	
 	loaded = true;
 	//setInterval(() => {
 	//	dbl.postStats(bot.guilds.size, bot.shard.id, bot.shard.count);
@@ -201,7 +196,6 @@ bot.on("guildCreate", (guild) => {
 
 //removed from a server
 bot.on("guildDelete", (guild) => {
-        //>eval let prob = "10%"; let dec = parseFloat(prob); let p = dec/100;  msg.channel.send(0/p);
 	console.log("Left a guild: " + guild.name);
 	let gusers = guild.members.filter(user => !user.user.bot).size; // get only users and exclude bots
 	let gtotal = guild.members.filter(user => user.user).size; // get all users and bots
@@ -299,7 +293,6 @@ bot.on('message', (msg) => {
 				})}).catch(console.error);
 			}
 			// END DEBUGGING MESSAGES LOG FOR ERRORS
-			//if (!gbot.hasPermission(0x00000020)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Guild\`!`).catch(console.error);
 			if (!gbot.hasPermission(0x00040000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Use External Emojis\`!`).catch(console.error);
 			if (!gbot.hasPermission(0x00004000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Embed Links\``).catch(console.error);
 		}
@@ -319,13 +312,11 @@ bot.on('message', (msg) => {
 				})}).catch(console.error);
 			}
 			// END DEBUGGING MESSAGES LOG FOR ERRORS
-			//if (!gbot.hasPermission(0x00000020)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Guild\`!`).catch(console.error);
 			if (!gbot.hasPermission(0x00040000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Use External Emojis\`!`).catch(console.error);
 			if (!gbot.hasPermission(0x00004000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Embed Links\`!`).catch(console.error);
 		}
 	}
 	if (msg === "") return;
-	//if (msg.guild.owner.user.id !== msg.author.id) return;
 	if (msg.guild && bot.config.blacklistedServers && bot.config.blacklistedServers.indexOf(msg.guild.id.toString()) > -1) {
 		return;
 	}
@@ -383,18 +374,7 @@ bot.on('warn', (w) => {
 	Hook.custom(bot.user.username, `${w}`, "Warn", "#C1BD3A");
 	console.warn;
 });
-/*bot.on('disconnect', event => {
-    if (event.code === 1000) {
-        logger.info('Disconnected from Discord cleanly');
-    } else if (event.code === 4004) {
-        // Force the user to reconfigure if their token is invalid
-        logger.severe(`Failed to authenticate with Discord. Please follow the instructions at ${chalk.green('https://discordapp.com/developers')} and re-enter your token by running ${chalk.green('yarn run config')}.`);
-        process.exit(666);
-    } else {
-        logger.warn(`Disconnected from Discord with code ${event.code}`);
-    }
-});
-*/
+
 bot.on('disconnect', event => {
 	if (event.code === 0) {
 		Hook.custom(bot.user.username, "[0] Gateway Error", "Warn", "#C1BD3A");
@@ -402,9 +382,8 @@ bot.on('disconnect', event => {
 	} else if (event.code === 1000) {
 		Hook.custom(bot.user.username, "[1000] Disconnected from Discord cleanly", "Info", "#59EADA");
 	} else if (event.code === 4000) {
-		Hook.custom(bot.user.username, "[4000] Unknown Error", "Error", "#EF5350");
-		logger.severe('Unknown Error');
-		process.exit(666);
+		Hook.custom(bot.user.username, "[4000] Unknown Error", "Warn", "#C1BD3A");
+		logger.warn('Unknown Error');
 	} else if (event.code === 4001) {
 		Hook.custom(bot.user.username, "[4001] Unknown Opcode", "Warn", "#C1BD3A");
 		logger.warn('Unknown Opcode');
@@ -456,58 +435,5 @@ process.on('unhandledRejection', err => {
 	Hook.custom(bot.user.username, err.stack, "Unhandled Rejection | Uncaught Promise error:", "#EF5350");
 	logger.severe('Uncaught Promise error: \n' + err.stack);
 });
-
-/*
-if (!global.msgo) {
-	let interval = setInterval(() => {
-		if (debug) {
-			console.log('No guild id captured yet!');
-		}
-		if (global.msgo) {
-			clearInterval(interval);
-			if (debug) {
-				console.log(global.msgo);
-			}
-			let gprefix;
-			if (!bot.config[global.msgo]) {
-				global.gprefix = bot.config.prefix;
-			} else if (bot.config[global.msgo]) {
-				global.gprefix = bot.config[global.msgo].prefix;
-			}
-			if (debug) {
-				console.log(global.gprefix);
-			}
-			const music = new Music(bot, {
-				youtubeKey: process.env.YOUTUBE_API_KEY, // A YouTube Data API3 key. Required to run.
-				prefix: global.gprefix, // The prefix of the bot. Defaults to "!".
-				thumbnailType: 'high', // Type of thumbnails to use for videos on embeds. Can equal: default, medium, high.
-				global: false, // Whether to use one global queue or server specific ones.
-				maxQueueSize: 100, // Max queue size allowed. Defaults 20.
-				defVolume: 200, // The default volume of music. 1 - 200, defaults 50.
-				anyoneCanSkip: true, // Whether or not anyone can skip.
-				clearInvoker: false, // Whether to delete command messages.
-				messageHelp: false, // Whether to message the user on help command usage. If it can't, it will send it in the channel like normal.
-				//botAdmins: [], // An array of Discord user ID's to be admins as the bot. They will ignore permissions for the bot, including the set command.
-				enableQueueStat: true, // Whether to enable the queue status, old fix for an error that occurs for a few people.
-				anyoneCanAdjust: true, // Whether anyone can adjust volume.
-				ownerOverMember: false, // Whether the owner over-rides CanAdjust and CanSkip.
-				anyoneCanLeave: false, // Whether anyone can make the bot leave the currently connected channel. // false because of a bug with permissions atm
-				//botOwner: '269247101697916939', // The ID of the Discord user to be seen as the owner. Required if using ownerOverMember.
-				logging: true, // Some extra none needed logging (such as caught errors that didn't crash the bot, etc).
-				requesterName: true, // Whether or not to display the username of the song requester.
-				inlineEmbeds: false, // Whether or not to make embed fields inline (help command and some fields are excluded).
-				disableHelp: true, // Disable the help command.
-				disableSet: true, // Disable the set command.
-				disableOwnerCmd: true, // Disable the owner command.
-				//disableLeaveCmd: true // Disable the leave command. // Because this command is broken at the moment
-				// https://www.npmjs.com/package/discord.js-musicbot-addon
-			});
-			if (debug) {
-				console.log('Music Loaded');
-			}
-		}
-	}, 2000);
-}
-*/
 
 bot.config && bot.login(process.env.BOT_TOKEN).catch(console.error);
