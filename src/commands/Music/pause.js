1057beb07e48ae08120b34c1ac392c7bbf731cfb
BannerBomb <BannerBomb55@gmail.com>
2018-03-05 18:58:30 -0500

    Update pause.js

--- src/commands/Music/pause.js
+++ src/commands/Music/pause.js
@@ -1,6 +1,8 @@
+require('../../conf/globals.js');
 exports.run = async (bot, msg) => {
 	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
 	if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played`).catch(console.error);
+	if (musicqueue[msg.guild.id]['streaming']) return msg.channel.send(`<:redx:411978781226696705> Streams cannot be paused.`);
 	const dispatcher = voiceConnection.player.dispatcher;
 	if (dispatcher.paused) return msg.channel.send(`<:redx:411978781226696705> Playback already paused!`).catch(console.error);
 	if (!dispatcher.paused) {
