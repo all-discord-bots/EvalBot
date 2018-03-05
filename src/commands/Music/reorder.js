9ea126782a3df5f1e862ce4b2e90f9f7d7967b56
BannerBomb <BannerBomb55@gmail.com>
2018-03-05 18:53:46 -0500

    Update reorder.js

--- src/commands/Music/reorder.js
+++ src/commands/Music/reorder.js
@@ -1,19 +1,19 @@
 require('../../conf/globals.js');
 exports.run = async (bot, msg, args) => {
 	if (args.length !== 2) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments provided.`).catch(console.error);
-	if (musicqueue[msg.guild.id].length < 1) return msg.channel.send(`<:redx:411978781226696705> There are no songs in the queue!`).catch(console.error);
+	if (musicqueue[msg.guild.id]['music'].length < 1) return msg.channel.send(`<:redx:411978781226696705> There are no songs in the queue!`).catch(console.error);
 	if (args[0] == 0) return msg.channel.send(`<:redx:411978781226696705> You can't move the current playing video!`).catch(console.error);
-	if (args[1] < 1 || args[1] > musicqueue[msg.guild.id].length) return msg.channel.send(`<:redx:411978781226696705> Could not move to position \`${args[1]}\` valid position are \`1-${musicqueue[msg.guild.id].length}\`!`).catch(console.error);
+	if (args[1] < 1 || args[1] > musicqueue[msg.guild.id]['music'].length) return msg.channel.send(`<:redx:411978781226696705> Could not move to position \`${args[1]}\` valid position are \`1-${musicqueue[msg.guild.id].length}\`!`).catch(console.error);
 	let old_pos = parseInt(args[0]);
 	let new_pos = parseInt(args[1]);
-	if (new_pos >= musicqueue[msg.guild.id].length) {
-		let pos = new_pos - musicqueue[msg.guild.id].length + 1;
+	if (new_pos >= musicqueue[msg.guild.id]['music'].length) {
+		let pos = new_pos - musicqueue[msg.guild.id]['music'].length + 1;
 		while (pos--) {
-			musicqueue[msg.guild.id].push(undefined);
+			musicqueue[msg.guild.id]['music'].push(undefined);
 		}
 	}
 	msg.channel.send(`<:check:411976443522711552> Position \`${old_pos}\` has been moved to position \`${new_pos}\` in the queue.`);
-	musicqueue[msg.guild.id].splice(new_pos, 0, musicqueue[msg.guild.id].splice(old_pos, 1)[0]);
+	musicqueue[msg.guild.id]['music'].splice(new_pos, 0, musicqueue[msg.guild.id]['music'].splice(old_pos, 1)[0]);
 };
 
 exports.info = {
