0724e39cdff4649845ee4dcbfd96cb42bf084287
BannerBomb <BannerBomb55@gmail.com>
2018-03-05 20:53:01 -0500

    Update resume.js

--- src/commands/Music/resume.js
+++ src/commands/Music/resume.js
@@ -2,6 +2,7 @@ exports.run = async (bot, msg) => {
 	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
 	if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played`).catch(console.error);
 	const dispatcher = voiceConnection.player.dispatcher;
+	if (!dispatcher) return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`).catch(console.error);
 	if (!dispatcher.paused) return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`).catch(console.error);
 	if (dispatcher.paused) {
 		dispatcher.resume();
