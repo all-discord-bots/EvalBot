23c39f70d41568251881d32660c61ce1cf5e84c0
BannerBomb <BannerBomb55@gmail.com>
2018-03-07 12:22:24 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -24,30 +24,6 @@ const bot = global.bot = exports.client = new Discord.Client();
 const Music = require('discord.js-musicbot-addon');
 const snekfetch = require('snekfetch');
 
-const FTPS = require('ftps');
-const ftps = new FTPS({
-  host: 'files.000webhost.com', // required 
-  username: '', // Optional. Use empty username for anonymous access.
-  password: process.env.WEBHOST_PASSWORD, // Required if username is not empty, except when requiresPassword: false 
-  protocol: 'ftp', // Optional, values : 'ftp', 'sftp', 'ftps', ... default: 'ftp' 
-  // protocol is added on beginning of host, ex : sftp://domain.com in this case 
-  port: 22, // Optional 
-  // port is added to the end of the host, ex: sftp://domain.com:22 in this case 
-  escape: true, // optional, used for escaping shell characters (space, $, etc.), default: true 
-  retries: 2, // Optional, defaults to 1 (1 = no retries, 0 = unlimited retries) 
-  timeout: 10, // Optional, Time before failing a connection attempt. Defaults to 10 
-  retryInterval: 5, // Optional, Time in seconds between attempts. Defaults to 5 
-  retryMultiplier: 1, // Optional, Multiplier by which retryInterval is multiplied each time new attempt fails. Defaults to 1 
-  requiresPassword: true, // Optional, defaults to true 
-  autoConfirm: true, // Optional, is used to auto confirm ssl questions on sftp or fish protocols, defaults to false 
-  cwd: '', // Optional, defaults to the directory from where the script is executed 
-  additionalLftpCommands: '', // Additional commands to pass to lftp, splitted by ';' 
-  requireSSHKey:  false, //  Optional, defaults to false, This option for SFTP Protocol with ssh key authentication 
-  sshKeyPath: '' // Required if requireSSHKey: true , defaults to empty string, This option for SFTP Protocol with ssh key authentication 
-});
-// Do some amazing things 
-//ftps.addFile(__dirname).exec(console.log);
-
 // begin database
 //const { Client } = require('pg');
 
