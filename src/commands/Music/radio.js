d88428ae21ebfecf053e4a6bcf1430c232e899df
BannerBomb <BannerBomb55@gmail.com>
2018-03-06 23:19:13 -0500

    Update radio.js

--- src/commands/Music/radio.js
+++ src/commands/Music/radio.js
@@ -52,8 +52,8 @@ exports.run = async (bot, msg, args) => {
 	if (!musicqueue[msg.guild.id]['looped']) musicqueue[msg.guild.id]['looped'] = false;
 	if (!musicqueue[msg.guild.id]['music']) musicqueue[msg.guild.id]['music'] = [];
 	let getarg = arg.toLowerCase().toString();
-	let filteredbuiltinradio = radiostationsqueue.map(list => list.toString()).filter(list => list.startsWith(getarg.toString()));
-	if (getarg.length <= 4 && filteredbuiltinradio.length < 1) return msg.channel.send(`<:redx:411978781226696705> You must provide a valid stream`).catch(console.error);
+	if (getarg.length < 4) return msg.channel.send(`You must provide a valid stream`);
+	let filteredbuiltinradio = radiostationsqueue.map(list => list.toLowerCase().toString()).filter(list => list.toLowerCase().startsWith(getarg.toString()));
 	let queuethis;
 	let playingbuiltinstations = false;
 	if (filteredbuiltinradio.length > 0 && filteredbuiltinradio.length < 2) {
