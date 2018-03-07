1f4e69b72b813edb04483ae224ec6c709c4717ba
BannerBomb <BannerBomb55@gmail.com>
2018-03-07 09:06:09 -0500

    Update globals.js

--- src/conf/globals.js
+++ src/conf/globals.js
@@ -1,5 +1,9 @@
 const musicqueue = global.musicqueue = {};
 const builtinradio = global.builtinradio = {
-'fun radio': 'http://streaming.radio.funradio.fr/fun-1-44-128',
-'1.fm absolute top 40': 'http://strm112.1.fm/top40_mobile_mp3'
+	'fun radio': 'http://streaming.radio.funradio.fr/fun-1-44-128',
+	'1.fm absolute top 40': 'http://strm112.1.fm/top40_mobile_mp3',
+	'977 hits': 'http://19353.live.streamtheworld.com/977_HITS_SC',
+	'absolute radio': 'http://icy-e-bab-09-boh.sharp-stream.com/absoluteradio.mp3',
+	'heart': 'http://media-ice.musicradio.com/HeartSouthWalesMP3',
+	'monstercat': 'https://www.youtube.com/watch?v=ueupsBPNkSc'
 };
