188e11597511aa61da7565d0f95a45a3a0afb64a
BannerBomb <BannerBomb55@gmail.com>
2018-03-08 21:13:55 -0500

    Update globals.js

--- src/conf/globals.js
+++ src/conf/globals.js
@@ -1,4 +1,5 @@
 const musicqueue = global.musicqueue = {};
+const songqueue = global.songqueue = {};
 const builtinradio = global.builtinradio = {
 	'fun radio': 'http://streaming.radio.funradio.fr/fun-1-44-128',
 	'1.fm absolute top 40': 'http://strm112.1.fm/top40_mobile_mp3',
