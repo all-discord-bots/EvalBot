d0e39cd04f48be7d577225525910e7b7a169d3b4
BannerBomb <BannerBomb55@gmail.com>
2018-03-11 18:18:28 -0400

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -3,6 +3,8 @@
     instead of your main bot file.
 */
 
+const Webhook = require('webhook-discord');
+const wbhook = new Webhook(process.env.WEBHOOK_SHARD_LOGGER);
 const Discord = require('discord.js');
 const SpawnShards = require('../data/configs/config.json');
 const Manager = new Discord.ShardingManager('./src/bot.js', {
@@ -16,75 +18,7 @@ let guilds = 0;
 let users = 0;
 
 Manager.spawn(this.totalShards, 15000);
-/*
-Manager.spawn(this.totalShards, 15000).then(shards => {
-    TFS();
-}).catch(e=> {
-    let a = (new Error);
-});
-
-function TFS() {
-    setTimeout(() => {
-        getServs();
-        setInterval(() => {
-            getServs();
-        }, 1000 * 60 * 5);
-    }, 1000 * 10);
-}
-
-function getServs() {
-    Manager.fetchClientValues('guilds.size').then(rst => {
-        Manager.broadcastEval('var x=0;this.guilds.map(g => {x += g.memberCount});x;').then(r => {
-            r = r.reduce((a, b) => a + b);
-            let users = r;
-            let g = rst.reduce((prev, val) => prev + val, 0);
-            updateStats(g);
-        }).catch(e=> {});
-    }).catch(e=> {});
-}
-
-function updateStats(guilds) {
-
-    let rqOptions = {
-        headers: {
-            Authorization: cfg.pwTok3
-        },
-        url: `https://bots.discord.pw/api/bots/${cfg.pwID}/stats`,
-        method: 'POST',
-        json: {
-            "server_count": guilds
-        }
-    };
-
-    rq(rqOptions, function (err, response, body) {
-        if (err) {
-            console.log(err)
-        }
-
-            console.log(body)
-    });
-
-    let rqCarbon = {
-        url: `https://www.carbonitex.net/discord/data/botdata.php`,
-        method: 'POST',
-        json: {
-            "server_count": guilds,
-            "key": cfg.carbon_token
-        }
-    };
-
-    rq(rqCarbon, function (err, response, body) {
-        if (err) {
-            console.log(err)
-        }
-
-    });
-
-}
-*/
 Manager.on('launch', (shard) => {
-	const Webhook = require("webhook-discord");
-	const Hook = new Webhook(process.env.WEBHOOK_SHARD_LOGGER);
-	Hook.success('CripsBot', `Successfully launched shard \`${shard.id}\``);
+	wbhook.success('CripsBot', `Successfully launched shard \`${shard.id}\`.`);
 	console.log(`Successfully launched shard ${shard.id}`);
 });
