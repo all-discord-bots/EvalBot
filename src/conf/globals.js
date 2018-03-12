6a27889e78fa4a1276cf92e3fd2a35c4b4cd5fa3
BannerBomb <BannerBomb55@gmail.com>
2018-03-11 21:53:26 -0400

    Update globals.js

--- src/conf/globals.js
+++ src/conf/globals.js
@@ -5,6 +5,8 @@ const builtinradio = global.builtinradio = {
 	'1.fm absolute top 40': 'http://strm112.1.fm/top40_mobile_mp3',
 	'977 hits': 'http://19353.live.streamtheworld.com/977_HITS_SC',
 	'absolute radio': 'http://icy-e-bab-09-boh.sharp-stream.com/absoluteradio.mp3',
-	'heart': 'http://media-ice.musicradio.com/HeartSouthWalesMP3'
+	'heart': 'http://media-ice.musicradio.com/HeartSouthWalesMP3',
+	'christian radio': 'http://17683.live.streamtheworld.com/WFSHFM_SC',
+	'christian teaching and talk': 'http://17553.live.streamtheworld.com/WAVAFM_SC'
 };
 // 'monstercat': 'https://www.youtube.com/watch?v=ueupsBPNkSc'
