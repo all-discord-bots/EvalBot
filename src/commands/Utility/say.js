4dd1acd84488640d7fefb74facaed16436bf1887
BannerBomb <BannerBomb55@gmail.com>
2018-03-15 10:13:03 -0400

    Update say.js

--- src/commands/Utility/say.js
+++ src/commands/Utility/say.js
@@ -1,1 +1,24 @@
+exports.run = async (bot, msg, args) => {
+	let desc;
+	//let argzero = args[0].join(' ');
+	let arg = args[0].join(' ');
+	if (arg.length > 0) {
+		desc = `${arg.toString()}`;
+	} else {
+		desc = ` `;
+	}
+	//let gchannel;
+	//if (!msg.guild.channel.find(`id`, `${args[0].toString()}`)) {
+	//	gchannel = msg.channel.send(
+	//}
+	msg.channel.send({embed: ({
+		desc,
+		timestamp: new Date()
+	})});
+};
 
+exports.info = {
+	name: 'say',
+	description: 'Tell the bot to say something',
+	usage: 'say [channel] <message>'
+};
