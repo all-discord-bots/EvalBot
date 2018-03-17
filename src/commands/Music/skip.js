cbd4bf4eb03a7de99f578cfcb52b4a127863e9cc
BannerBomb <BannerBomb55@gmail.com>
2018-03-17 18:40:45 -0400

    Update skip.js

--- src/commands/Music/skip.js
+++ src/commands/Music/skip.js
@@ -1,19 +1,30 @@
+require('../../conf/globals.js');
 exports.run = async (bot, msg, args) => {
-	if (msg.author.id !== bot.config.botCreatorID) return;
-	let skipped;
-	let s;
+	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
+	if (voiceConnection === null || !musicqueue[msg.guild.id] || musicqueue[msg.guild.id] && musicqueue[msg.guild.id]['music'].length < 1) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`).catch(console.error);
+	let skipped, s;
 	if (!args[0]) {
-		skipped = '1';
+		skipped = `1`;
 		s = '';
 	} else if (args[0]) {
 		skipped = args[0];
 		s = 's';
 	}
-	console.log(`Skipped ${skipped} song${s}.`);
+	let toSkip = Math.min(parseInt(args[0]), musicqueue[msg.guild.id]['music'].length);
+	musicqueue[msg.guild.id]['music'].splice(0, parseInt(toSkip) - 1);
+	try {
+		const dispatcher = voiceConnection.player.dispatcher;
+		if (voiceConnection.paused) dispatcher.resume();
+		dispatcher.end();
+	} catch (e) {
+		const nerr = e.toString().split(':');
+		return msg.channel.send(`Error occoured!\n\`\`\`\n${nerr[0]}: ${nerr[1]}\n\`\`\``);
+	};
+	//console.log(`Skipped ${skipped} song${s}.`);
+	msg.channel.send(`Skipped ${skipped} song${s}.`);
 };
 
 exports.info = {
-	hidden: true,
 	name: 'skip',
 	usage: 'skip [number]',
 	description: 'Skip a song or multi songs.'
