881cd9d15ee7af28d322d6e01302bf6bdd71864c
BannerBomb <BannerBomb55@gmail.com>
2018-04-12 14:24:29 -0400

    Update whitelist.js

--- src/commands/Utility/whitelist.js
+++ src/commands/Utility/whitelist.js
@@ -2,53 +2,64 @@ const DBL = require("dblapi.js");
 const paste = require("better-pastebin");
 
 exports.run = async (bot, msg, args) => {
+	let pastebinID = "xd2bt7z9";
 	if (msg.author.id !== bot.config.botCreatorID) return;
 	const dbl = new DBL(process.env.DB_TOKEN, bot);
-	if (msg.guild.id !== bot.config.botMainServerID) return msg.channel.send(`<:redx:411978781226696705> This command may only be used on the support server!`);
+	if (msg.guild.id !== bot.config.botMainServerID) return;
+	if (msg.channel.type !== "dm") return msg.channel.send(`<:redx:411978781226696705> This command may only be used in ${bot.user.username}'s DM's`).catch(console.error);
 	let gbot = msg.guild.members.get(bot.user.id);
 	if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
 	paste.setDevKey(process.env.PASTEBIN_KEY);
-	//paste.get("CZiT7m3E", function(success, data) {
-	//	//data contains the contents of the paste
-	//	if (success) {
-	//		let append = data;
-	//	}
-	//});
-	paste.login(process.env.username, process.env.pw, function(success, data) {
-		if (!success) {
-			console.log("Failed (" + data + ")");
-			return false;
-		}
-		paste.edit("CZiT7m3E", {
-			contents: `${args[0]}\n`,
-		}, function(success, data) {
-			if (!success) {
-				console.log("Failed (" + data + ")");
-				return false;
-			}
-		});
-	});
+	//
+	let requestuser = msg.author.tag;
 	let guser = msg.guild.members.find(`id`, `${msg.author.id}`);
 	let whitelistedRole = msg.guild.roles.find('id', '433745475930292235');
 	(await msg.channel.send({ embed: ({ author: { name: 'Validating request...', icon_url: 'http://4.bp.blogspot.com/-JF6M1HaI9rQ/VD_eCkLpG7I/AAAAAAAAAXk/0f1ym7hBXYs/s1600/Loading-Circle.gif', } }) }).then((msg) => {
 		if (dbl.hasVoted(msg.author.id) && !msg.member.roles.has(whitelistedRole.id)) { // user has already upvoted
 			// ({original: true})
 			// send pastebin here
+			paste.login(process.env.username, process.env.pw, function(success, data) {
+				if (success) {
+					console.log("Successfully logged in to pastebin!");
+				} else {
+					console.log("Failed (" + data + ")");
+					return false;
+				}
+				paste.get(pastebinID, function(success, data) {
+					//data contains the contents of the paste
+					if (success) {
+						// paste.edit("xd2bt7z9", contents = `${data}\n${args[0].toString()} - [${msg.author.tag} <${msg.author.id}>]`, function(success, data) {
+						paste.edit(pastebinID, contents = `${data}\n${args[0].toString()}`, function(success, data) {
+							if (success) {
+								// TODO: Create 'Whitelist Logs' channel to log messages in
+								console.log(data);
+							} else {
+								console.log("Failed (" + data + ")");
+								return false;
+							}
+							//data contains the new contents of the paste
+						});
+					} else {
+						console.log("Failed (" + data + ")");
+					}
+				});
+			});
+
 			guser.addRole(whitelistedRole).then(() => {
 				msg.edit({
 					embed: ({
 						color: 6732394,
-						title: `Successfully whitelisted user ${msg.author.tag}!`,
+						title: `Successfully whitelisted user ${requestuser}!`,
 						timestamp: new Date(),
-						description: `<:check:411976443522711552> ${msg.author.tag} has been successfully whitelisted. You now have full access to CripsSploit <:dancingblob:413502883900686348>.\nHead to <#430696088077598722> if you need to download CripsSploit.\nPlease check your DM's for your whitelist information.`
+						description: `<:check:411976443522711552> ${requestuser} has been successfully whitelisted. You now have full access to CripsSploit <a:dancingblob:413502883900686348>.\nHead to <#430696088077598722> if you need to download CripsSploit.\nPlease check your DM's for your whitelist information.`
 					})
 				});
-				msg.author.send({
+				guser.send({
 					embed: ({
 						color: 6732394,
 						title: `Your Whitelist Information`,
 						timestamp: new Date(),
-						description: `HWID: \`${args[0]}\``
+						description: `HWID: \`${args[0].toString()}\``
 					})
 				});
 				/*
@@ -62,7 +73,7 @@ exports.run = async (bot, msg, args) => {
 			msg.edit({
 				embed: ({
 					color: 15684432,
-					title: `Error whitelisting user, ${msg.author.tag}!`,
+					title: `Error whitelisting user, ${requestuser}!`,
 					timestamp: new Date(),
 					description: `<:redx:411978781226696705> You must upvote my discord bot [here](https://discordbots.org/bot/${bot.user.id}) first!`
 				})
@@ -71,7 +82,7 @@ exports.run = async (bot, msg, args) => {
 			msg.edit({
 				embed: ({
 					color: 15684432,
-					title: `User ${msg.author.tag}, has already been whitelisted!`,
+					title: `User ${guser.tag}, has already been whitelisted!`,
 					timestamp: new Date(),
 					description: `<:redx:411978781226696705> You have already been whitelisted no need to do this again!`
 				})
