c083d948222a7dfafc44549bf404dc811af133ad
BannerBomb <BannerBomb55@gmail.com>
2018-04-12 19:41:10 -0400

    Update whitelist.js

--- src/commands/Utility/whitelist.js
+++ src/commands/Utility/whitelist.js
@@ -3,8 +3,8 @@ const paste = require("better-pastebin");
 
 exports.run = async (bot, msg, args) => {
 	// if (msg.author.id !== bot.config.botCreatorID) return;
-	//if (msg.guild.id !== bot.config.botMainServerID) return;
-	if (msg.channel.type !== "dm") return msg.channel.send(`<:redx:411978781226696705> This command may only be used in ${bot.user.username}'s DM's`).catch(console.error);
+	if (msg.guild.id !== bot.config.botMainServerID) return msg.channel.send(`<:redx:411978781226696705> This command may only be used on the support server!`);
+	//if (msg.channel.type !== "dm") return msg.channel.send(`<:redx:411978781226696705> This command may only be used in ${bot.user.username}'s DM's`).catch(console.error);
 	let gbot = msg.guild.members.get(bot.user.id);
 	if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
 	if (!args[0] || args[0].length < 6) return msg.channel.send(`<:redx:411978781226696705> Invalid HWID provided please provide a valid ID to whitelist.`).catch(console.error);
