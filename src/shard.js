ba06bbace9442c482efc9972de5fae239bb08426
BannerBomb <BannerBomb55@gmail.com>
2018-07-01 07:00:01 +0000

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -6,7 +6,6 @@
 const Webhook = require('webhook-discord');
 const wbhook = new Webhook(process.env.WEBHOOK_SHARD_LOGGER);
 const Discord = require('discord.js');
-const SpawnShards = require('../data/configs/config.json');
 const Manager = new Discord.ShardingManager('./src/bot.js', {
     respawn: true,
     //token: SpawnShards.botToken,
@@ -17,7 +16,7 @@ const Manager = new Discord.ShardingManager('./src/bot.js', {
 let guilds = 0;
 let users = 0;
 
-Manager.spawn(this.totalShards, 15000);
+// Manager.spawn(this.totalShards, 15000);
 Manager.on('launch', (shard) => {
 	wbhook.success('CripsBot', `Successfully launched shard \`${shard.id}\`.`);
 	console.log(`Successfully launched shard ${shard.id}`);
