bc228ef8dc27ca9e7597cd0b1d923093a22e7303
BannerBomb <BannerBomb55@gmail.com>
2018-08-13 00:34:55 +0000

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -16,8 +16,9 @@ const Manager = new ShardingManager('./src/bot.js', {
 let guilds = 0;
 let users = 0;
 
-console.log(`Value: ${this.totalShards} || Type: ${typeof(this.totalShards)}`)
-Manager.spawn(this.totalShards, 15000);
+//console.log(`Value: ${this.totalShards} || Type: ${typeof(this.totalShards)}`)
+// Manager.spawn(this.totalShards, 15000);
+Manager.spawn();
 Manager.on('launch', (shard) => {
 	wbhook.success('CripsBot', `Successfully launched shard \`${shard.id}\`.`);
 	console.log(`Successfully launched shard ${shard.id}`);
