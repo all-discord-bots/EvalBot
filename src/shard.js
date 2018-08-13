63e789307090bb1d99450cfd91e65765d5fa3b88
BannerBomb <BannerBomb55@gmail.com>
2018-08-13 00:55:21 +0000

    Update shard.js

--- src/shard.js
+++ src/shard.js
@@ -10,17 +10,15 @@ const Manager = new ShardingManager('./src/bot.js', {
     respawn: true,
     //token: SpawnShards.botToken,
     token: process.env.BOT_TOKEN,
-    totalShards: 1
+    totalShards: "auto"
 });
-
-console.log(`BOT_TOKEN: ${process.env.BOT_TOKEN}`);
 
 let guilds = 0;
 let users = 0;
 
 console.log(`Value: ${this.totalShards} || Type: ${typeof(this.totalShards)} || ManagerConstValue: ${Manager.totalShards}`);
-Manager.spawn(this.totalShards, 15000);
-//Manager.spawn();
+//Manager.spawn(this.totalShards, 15000);
+Manager.spawn(Manager.totalShards, 15000);
 Manager.on('launch', (shard) => {
 	wbhook.success('CripsBot', `Successfully launched shard \`${shard.id}\`.`);
 	console.log(`Successfully launched shard ${shard.id}`);
