02a1084afcab1bf42c329a5c2b69afd5e3860738
BannerBomb <BannerBomb55@gmail.com>
2018-08-13 08:11:00 +0000

    Update reboot.js

--- src/commands/Utility/reboot.js
+++ src/commands/Utility/reboot.js
@@ -3,7 +3,7 @@ exports.run = async (bot, msg) => {
     (await msg.channel.send(`<:check:411976443522711552> Rebooting bot...`).then((msg)=>{
 	    msg.edit(`<:check:411976443522711552> Bot rebooted successfully.`);
 	}));
-	process.exit(1);
+	bot.shutdown(true);
 };
 
 exports.info = {
