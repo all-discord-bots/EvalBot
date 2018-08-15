d670faca92449dd749bd8e584f4a472e53be998e
BannerBomb <BannerBomb55@gmail.com>
2018-08-15 03:45:49 +0000

    Update pussy.js

--- src/commands/Nsfw/pussy.js
+++ src/commands/Nsfw/pussy.js
@@ -1,29 +1,29 @@
-const { MessageAttachment } = require('discord.js');
+//const { MessageAttachment } = require('discord.js');
 const Pornsearch = require('pornsearch');
 
 exports.run = async (bot, msg, args) => {
 	if (!msg.channel.nsfw) return msg.channel.send(`<:redx:411978781226696705> This channel has not been marked as NSFW!`).catch(console.error);
 	if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Please provide a search string!`).catch(console.error);
-	let gbot = msg.guild.members.get(bot.user.id);
-	if (!gbot.hasPermission(0x00008000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Attach Files\`!`).catch(console.error);
-	if (!msg.member.hasPermission('ATTACH_FILES')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Attach Files\`!`).catch(console.error);
+	//let gbot = msg.guild.members.get(bot.user.id);
+	//if (!gbot.hasPermission(0x00008000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Attach Files\`!`).catch(console.error);
+	//if (!msg.member.hasPermission('ATTACH_FILES')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Attach Files\`!`).catch(console.error);
 	let searchqueue = args.join(' ');
 	const Searcher = new Pornsearch(searchqueue.toString());
 	let randomnum = Math.floor(Math.random() * 2);
 	if (randomnum <= 0)
 	{
 		Searcher.videos().then(result => {
-			const attachment = new MessageAttachment(`${result[0].url}`);
-			msg.channel.send(`Title: __**${result[0].title}**__\r\nDuration: **${result[0].duration}**`,attachment);
+			//const attachment = new MessageAttachment(`${result[0].url}`);
+			msg.channel.send(`Title: __**${result[0].title}**__\r\nDuration: **${result[0].duration}**\r\n${result[0].url}`);
 		}).catch(err => {
-			return msg.channel.send(`<:redx:411978781226696705> ${err}`);
+			return msg.channel.send(`<:redx:411978781226696705> ${err.toString()}`);
 		});
 	}
 	else if (randomnum >= 1)
 	{
 		Searcher.gifs().then(result => {
-			const attachment = new MessageAttachment(`${result[0].url}`);
-			msg.channel.send(`Title: __**${result[0].title}**__\r\nDuration: **${result[0].duration}**`,attachment);
+			//const attachment = new MessageAttachment(`${result[0].url}`);
+			msg.channel.send(`Title: __**${result[0].title}**__\r\nDuration: **${result[0].duration}**\r\n${result[0].url}`);
 		}).catch(err => {
 			return msg.channel.send(`<:redx:411978781226696705> ${err.toString()}`);
 		});
