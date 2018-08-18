ff43362f61e3f247e8c14044a169522b001c0fee
BannerBomb <BannerBomb55@gmail.com>
2018-08-18 08:33:10 +0000

    Update finduser.js

--- src/commands/Info/finduser.js
+++ src/commands/Info/finduser.js
@@ -1,6 +1,5 @@
 exports.run = async (bot, msg, args) => {
-  let user = bot.utils.getMembers(msg,args.join(' '));
-  msg.channel.send(`${user}`);
+    msg.channel.send(bot.utils.getMembers(msg,args.join(' ')));
 };
 
 exports.info = {
