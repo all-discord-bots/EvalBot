506e6d2cd5e48ebdf57fa376c83a385f4a54fe64
BannerBomb <BannerBomb55@gmail.com>
2018-08-19 20:27:56 +0000

    Update prefix.js

--- src/commands/Utility/prefix.js
+++ src/commands/Utility/prefix.js
@@ -2,15 +2,15 @@ const fetch = require('node-fetch');
 
 exports.run = async (bot, msg, args) => {
 	if (args.length <= 0) {
-		if (!bot.config[msg.guild.id]) return msg.channel.send(`This guilds current prefix is \`${bot.config.prefix}\`.`).catch(err => console.error);
-		if (bot.config[msg.guild.id]) return msg.channel.send(`This guilds current prefix is \`${bot.config[msg.guild.id].prefix}\`.`).catch(err => console.error);
+		if (!bot.config[msg.guild.id.toString()]) return msg.channel.send(`This guilds current prefix is \`${bot.config.prefix}\`.`).catch(err => console.error);
+		if (bot.config[msg.guild.id.toString()]) return msg.channel.send(`This guilds current prefix is \`${bot.config[msg.guild.id.toString()].prefix}\`.`).catch(err => console.error);
 	}
 	
 	if (msg.author.id !== bot.config.botCreatorID) {
 		if (!msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Manage Server\`!`).catch(err => console.error);
 	}
 	
-	bot.managers.config.set(msg.guild.id, {prefix: args.join(' ')});
+	bot.managers.config.set(msg.guild.id.toString(), {prefix: args.join(' ')});
 	fetch(`http://cripsbot.000webhostapp.com/database/database_update.php?guild_id=${msg.guild.id}&prefix=${args.join(' ')}&database_token=QISqwssXd93riidEqjjRFom19SDuSTEU`).catch(err => console.error);
 	msg.channel.send(`<:check:411976443522711552> Prefix changed to \`${args.join(' ')}\`.`);
 };
