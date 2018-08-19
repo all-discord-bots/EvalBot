34ca454eaf0c425d417c2a1cc24adf72ecb1f136
BannerBomb <BannerBomb55@gmail.com>
2018-08-19 12:00:25 +0000

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -117,13 +117,13 @@ class CripsBot extends Client {
 			
 			try {
 				// let array = new Array();
-				fse.readJson('../../data/config.json', function(err, data) {
+				fse.readJson('../data/configs/config.json', function(err, data) {
 					if (err) throw new Error(err);
 					fetch('http://cripsbot.000webhostapp.com/database/read_json.php')
 						.then(res => res.json())
-						.then(json => fs.writeJson("../../data/config.json", JSON.stringify(merge(JSON.parse(data),json))))
+						.then(json => fs.writeJson("../data/configs/config.json", JSON.stringify(merge(JSON.parse(data),json))))
 						.catch(err => console.error(err.toString()));
-					console.log("Successfully set custom configuration data.");
+					if (!err) console.log("Successfully set custom configuration data.");
 				});
 			} catch (err) {
 				return console.error(err.toString());
