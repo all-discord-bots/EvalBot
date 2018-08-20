d92be19d678d02f91a6d2d172772cea39e766688
BannerBomb <BannerBomb55@gmail.com>
2018-08-20 02:06:44 +0000

    Update prefix.js

--- src/commands/Utility/prefix.js
+++ src/commands/Utility/prefix.js
@@ -9,21 +9,20 @@ exports.run = async (bot, msg, args) => {
 	if (msg.author.id !== bot.config.botCreatorID) {
 		if (!msg.member.hasPermission('MANAGE_SERVER')) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Manage Server\`!`).catch(err => console.error);
 	}
-	
-	if (!bot.config[msg.guild.id.toString()])
-	{
+	try {
 		bot.managers.config.set(msg.guild.id.toString(), {prefix: args.join(' ')});
+		fetch(`http://cripsbot.000webhostapp.com/database/database_update.php?guild_id=${msg.guild.id}&prefix=${args.join(' ')}&database_token=QISqwssXd93riidEqjjRFom19SDuSTEU`).catch(err => console.error);
+		msg.channel.send(`<:check:411976443522711552> Prefix changed to \`${args.join(' ')}\`.`);
+	} catch (err) {
+		console.error(err.toString());
 	}
-	else
-	{
-		bot.managers.config.set(msg.guild.id.toString().prefix, args.join(' '));
-	}
-	fetch(`http://cripsbot.000webhostapp.com/database/database_update.php?guild_id=${msg.guild.id}&prefix=${args.join(' ')}&database_token=QISqwssXd93riidEqjjRFom19SDuSTEU`).catch(err => console.error);
-	msg.channel.send(`<:check:411976443522711552> Prefix changed to \`${args.join(' ')}\`.`);
 };
 
 exports.info = {
 	name: 'prefix',
 	usage: 'prefix <prefix>',
+	examples: [
+		'prefix !'
+	],
 	description: 'Change the prefix of the bot'
 };
