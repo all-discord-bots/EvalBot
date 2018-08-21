1c1edbb8e2bd33427296c7db23db2985a9eb19a4
BannerBomb <BannerBomb55@gmail.com>
2018-08-21 10:30:16 +0000

    Update sendemoji.js

--- src/commands/Fun/sendemoji.js
+++ src/commands/Fun/sendemoji.js
@@ -1,7 +1,7 @@
 exports.run = async (bot, msg, args) => {
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('EXTERNAL_EMOJIS')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permission \`External Emojis\`!`).catch(console.error);
-  }
+  //if (msg.author.id !== bot.config.botCreatorID) {
+    //if (!msg.member.hasPermission('EXTERNAL_EMOJIS')) return msg.channel.send(`<:redx:411978781226696705> You are missing the permission \`External Emojis\`!`).catch(console.error);
+  //}
   if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Please provide a emoji ID!`).catch(console.error);
   
   const emojis = bot.emojis.get(`${args[0]}`);
@@ -10,7 +10,8 @@ exports.run = async (bot, msg, args) => {
 };
 
 exports.info = {
-  name: 'sendemoji',
-  usage: 'sendemoji <emoji id>',
-  description: 'Make the bot message an emoji from an emoji ID only if the bot is in the server.'
+    name: 'sendemoji',
+    usage: 'sendemoji <emoji id>',
+    clientPermissions: ["EXTERNAL_EMOJIS"],
+    description: 'Make the bot message an emoji from an emoji ID only if the bot is in the server.'
 };
