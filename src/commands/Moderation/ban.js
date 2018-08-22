63df005029dcf3b13053fddcbb8364ba847e928f
BannerBomb <BannerBomb55@gmail.com>
2018-08-22 13:00:15 +0000

    Update ban.js

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -1,32 +1,33 @@
 const Discord = require("discord.js");
 
 exports.run = async (bot, msg, args) => {
-	if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must provide a user to ban.`).catch(err => console.error);
-	let modlogs = "mod_logs";
-	let bUser = bot.utils.getMembers(msg,args[0],false,true);
-	if (!user) return msg.channel.send(`<:redx:411978781226696705> I could not find that user.`).catch(err => console.error);
-	if (user.toString().includes("I could not find that user.")) return;
-	if(!bUser) return msg.channel.send(`Can't find that user!`).catch(console.error);
-	let bReason = args.join(" ").slice(22);
-	if(!msg.guild.member(bUser).bannable) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`).catch(err => console.error);
-//    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-	if (bUser.user.id === msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`).catch(err => console.error);
-	let incidentchannel = msg.guild.channels.find(`name`, `${modlogs}`);
-//    if(!incidentchannel) return msg.channel.send(`Can't find ${modlogs} channel.`).catch(console.error);
-
-	msg.guild.member(bUser).ban(bReason).catch(err => msg.channel.send(`<:redx:411978781226696705> I could not ban this user due to the error: ${err}`));
-	if (incidentchannel) {
-		let banEmbed = new Discord.RichEmbed()
-		.setDescription("~Ban~")
-		.setColor("#bc0000")
-		.addField("Banned User", `${bUser} with ID ${bUser.id}`)
-		.addField("Banned By", `<@${msg.author.id}> with ID ${msg.author.id}`)
-		.addField("Banned In", msg.channel)
-		.addField("Time", msg.createdAt)
-		.addField("Reason", bReason);
-		incidentchannel.send(banEmbed).catch(console.error);
+	try {
+		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must provide a user to ban.`).catch(err => console.error);
+		let modlogs = "mod_logs";
+		let bUser = bot.utils.getMembers(msg,args[0],false,true);
+		if (!bUser) return msg.channel.send(`<:redx:411978781226696705> I could not find that user.`).catch(err => console.error);
+		if (bUser.toString().includes("I could not find that user.")) return;
+		let bReason = args.join(" ").slice(22);
+		if(!msg.guild.member(bUser).bannable) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`).catch(err => console.error);
+		if (bUser.user.id === msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`).catch(err => console.error);
+		let incidentchannel = msg.guild.channels.find(`name`, `${modlogs}`);
+		// if(!incidentchannel) return msg.channel.send(`Can't find ${modlogs} channel.`).catch(console.error);
+		msg.guild.member(bUser).ban(bReason).catch(err => msg.channel.send(`<:redx:411978781226696705> I could not ban this user due to the error: ${err}`));
+		if (incidentchannel) {
+			let banEmbed = new Discord.RichEmbed()
+				.setDescription("~Ban~")
+				.setColor("#bc0000")
+				.addField("Banned User", `${bUser} with ID ${bUser.id}`)
+				.addField("Banned By", `<@${msg.author.id}> with ID ${msg.author.id}`)
+				.addField("Banned In", msg.channel)
+				.addField("Time", msg.createdAt)
+				.addField("Reason", bReason);
+			incidentchannel.send(banEmbed).catch(err => console.error);
+		}
+		msg.channel.send(`<:check:411976443522711552> Successfully banned <@${bUser.id}>`).catch(err => console.error);
+	} catch (err) {
+		console.error(err.toString);
 	}
-	msg.channel.send(`<:check:411976443522711552> Successfully banned <@${bUser.id}>`).catch(err => console.error);
 };
 
 exports.info = {
