678d1347339e8e346c798e25f6928be07f981c1f
BannerBomb <BannerBomb55@gmail.com>
2018-08-23 20:47:22 +0000

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -1,23 +1,33 @@
 exports.run = async (bot, msg, args) => {
-	const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-	if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`).catch(console.error);
-	const dispatcher = voiceConnection.player.dispatcher;
-	if (parseInt(args[0]) < 0 || parseInt(args[0]) > 200) return msg.channel.send(`<:redx:411978781226696705> Volume must be \`0-200\`!`).catch(console.error);
-	//let nvol;
-	//>eval const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id); voiceConnection.player.dispatcher.volume
-	if (args.length < 1 || isNaN(args[0])) return msg.channel.send(`Current volume is \`${parseInt(dispatcher.volume) * 100}%\`.`).catch(console.error);
-	// if (args.length < 1 || isNaN(args[0])) {
-	//	nvol = parseInt('0');
-	//} else {
-		let nvol = parseInt(args[0]);
-	//}
-	dispatcher.setVolume((nvol / 100));
-	msg.channel.send(`<:check:411976443522711552> Volume set to \`${nvol}%\`.`);
+	try {
+		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch(err => console.error);
+		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
+		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`).catch(err => console.error);
+		const dispatcher = voiceConnection.player.dispatcher;
+		if (parseInt(args[0]) < 0 || parseInt(args[0]) > 200) return msg.channel.send(`<:redx:411978781226696705> Volume must be \`0-200\`!`).catch(err => console.error);
+		//>eval const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id); voiceConnection.player.dispatcher.volume
+		if (args.length < 1 || isNaN(args[0])) return msg.channel.send(`Current volume is \`${parseInt(dispatcher.volume) * 100}%\`.`).catch(err => console.error);
+		// if (args.length < 1 || isNaN(args[0])) {
+		//	nvol = parseInt('0');
+		//} else {
+			let nvol = parseInt(args[0]);
+		//}
+		dispatcher.setVolume((nvol / 100));
+		msg.channel.send(`<:check:411976443522711552> Volume set to \`${nvol}%\`.`);
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
 	name: 'volume',
+	userPermissions: ['CONNECT'],
+	clientPermissions: ['CONNECT'],
 	aliases: ['vol'],
 	usage: 'volume <number>',
-	description: 'Adjusts the volume of the bot.'
+	examples: [
+		'volume 100',
+		'volume 200'
+	],
+	description: 'Adjusts the playback volume.'
 };
