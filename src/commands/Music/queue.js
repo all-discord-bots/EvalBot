509733acc6a1cd1e550a104ff6801e9eb45da592
BannerBomb <BannerBomb55@gmail.com>
2018-08-23 06:08:34 +0000

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -1,3 +1,38 @@
+const { YTSearcher } = require('ytsearcher');
+require('../../conf/globals.js');
+
+exports.run = async (bot, msg, args) => {
+	try {
+		let i = 0;
+		if (!musicqueue[msg.guild.id] || musicqueue[msg.guild.id]['music'].length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no items in the queue.`).catch(err => console.error);
+		let gqueue = musicqueue[msg.guild.id]['music'];
+		gqueue.map(list => {
+			let hashtag;
+			if (i == 0){
+				hashtag = '# ';
+			} else {
+				hashtag = '  ';
+			}
+			msg.channel.send(`\`\`\`md\n${hashtag}${i} - ${list.toString()}\n\`\`\``);
+			i++;
+		});
+	} catch (err) {
+		console.error(err.toString());
+	}
+};
+
+exports.info = {
+	name: 'queue',
+	aliases: ['getqueue','musicqueue','songqueue','queuelist','listqueue','queued'],
+	usage: 'queue',
+	examples: [
+		'queue'
+	],
+	description: 'Shows the current audio queue.'
+};
+
+
+
 /*const { YTSearcher } = require('ytsearcher');
 require('../../conf/globals.js');
 exports.run = async (bot, msg) => {
@@ -49,30 +84,3 @@ exports.info = {
 	description: 'Shows the current queue.'
 };
 */
-
-
-
-
-const { YTSearcher } = require('ytsearcher');
-require('../../conf/globals.js');
-exports.run = async (bot, msg) => {
-	let i = 0;
-	if (!musicqueue[msg.guild.id] || musicqueue[msg.guild.id]['music'].length < 1) return msg.channel.send(`<:redx:411978781226696705> There are no items in the queue.`).catch(console.error);
-	let gqueue = musicqueue[msg.guild.id]['music'];
-	gqueue.map(list => {
-		let hashtag;
-		if (i == 0){
-			hashtag = '# ';
-		} else {
-			hashtag = '  ';
-		}
-		msg.channel.send(`\`\`\`md\n${hashtag}${i} - ${list.toString()}\n\`\`\``);
-		i++;
-	});
-};
-exports.info = {
-	name: 'queue',
-	aliases: ['getqueue','musicqueue','songqueue','queuelist','listqueue','queued'],
-	usage: 'queue',
-	description: 'Shows the current queue.'
-};
