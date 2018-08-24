623a9758cedf5f5f790b912edeb46e2ca8175e9e
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 20:07:22 +0000

    Update listemojis.js

--- src/commands/Utility/listemojis.js
+++ src/commands/Utility/listemojis.js
@@ -1,33 +1,36 @@
-exports.run = async (bot, msg) => {
-	if (msg.author.id !== bot.config.botCreatorID) {
-		if (!msg.member.hasPermission('MANAGE_EMOJIS')) return msg.channel.send(`<:redx:411978781226696705> you are missing permissions \`Manage Emojis\`!`);
-	}
-	
-	const emojiList = msg.guild.emojis.map(e=>e.toString()).join(" ");
-	if (emojiList.length < 1) return msg.channel.send(`<:redx:411978781226696705> This server does not have any emojis!`);
-	//if (emojiList.length > 1999) return msg.channel.send(`<:redx:411978781226696705> The message exceeds 2000 characters!`);
-	const MESSAGE_CHAR_LIMIT = 2000;
-	const splitString = (string, prepend = '', append = '') => {
-		if (string.length <= MESSAGE_CHAR_LIMIT) {
-			return [string];
+exports.run = async (bot, msg, args) => {
+	try {
+		const emojiList = msg.guild.emojis.map(e => e.toString()).join(" ");
+		if (emojiList.length <= 0) return msg.channel.send(`<:redx:411978781226696705> This server does not have any emojis to list.`).catch((err) => console.error);
+		const MESSAGE_CHAR_LIMIT = 2000;
+		const splitString = (string, prepend = '', append = '') => {
+			if (string.length <= MESSAGE_CHAR_LIMIT) {
+				return [string];
+			}
+			const splitIndex = string.lastIndexOf('>', MESSAGE_CHAR_LIMIT - prepend.length - append.length);
+			const sliceEnd = splitIndex > 0 ? splitIndex : MESSAGE_CHAR_LIMIT - prepend.length - append.length;
+			const rest = splitString(string.slice(sliceEnd), prepend, append);
+			return [`${string.slice(0, sliceEnd)}${append}`, `${prepend}${rest[0]}`, ...rest.slice(1)];
 		}
-		const splitIndex = string.lastIndexOf('>', MESSAGE_CHAR_LIMIT - prepend.length - append.length); // string.lastIndexOf('\n',
-		const sliceEnd = splitIndex > 0 ? splitIndex : MESSAGE_CHAR_LIMIT - prepend.length - append.length;
-		const rest = splitString(string.slice(sliceEnd), prepend, append);
-		return [`${string.slice(0, sliceEnd)}${append}`, `${prepend}${rest[0]}`, ...rest.slice(1)];
-	};
-	let listemoji = splitString(`${emojiList}`);
-	listemoji.forEach(gemojis => {
-		msg.channel.send({embed: ({
-			color: 3447003,
-			title: `Emoji List`,
-			description: `${emojiList}`
-		})});
-	});
+		let listemoji = splitString(`${emojiList}`);
+		listemoji.forEach((gemojis) => {
+			msg.channel.send({embed: ({
+				color: 3447003,
+				title: `Emoji List`,
+				description: `${emojiList}`
+			})});
+		});
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
 	name: 'listemojis',
+	userPermissions: ['MANAGE_EMOJIS'],
 	usage: 'listemojis',
+	examples: [
+		'listemojis'
+	],
 	description: 'Displays all the emojis for the current server'
 };
