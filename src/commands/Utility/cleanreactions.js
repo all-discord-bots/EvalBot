2a9b781ff8962da146c57759fecd1db3b2cc4567
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 19:54:13 +0000

    Update cleanreactions.js

--- src/commands/Utility/cleanreactions.js
+++ src/commands/Utility/cleanreactions.js
@@ -1,24 +1,38 @@
 exports.run = (bot, msg, args) => {
-  if(!parseInt(args[0], 10)) {
-    return msg.channel.send(`Please provide a number of messages to clean reactions from!`);
-  }
-  let gbot = msg.guild.members.get(bot.user.id);
-  if (!gbot.hasPermission(0x00002000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Messages\`!`).catch(console.error);
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Manage Messages\``).catch(console.error);
-  }
-  msg.channel.fetchMessages({limit: parseInt(args[0], 10)}).then(msglog => {
-    msg.channel.send(`Clearing Reactions from this channel for ${args[0]} messages...`);
-    msglog.forEach(message => {
-      message.clearReactions();
-    }).catch(console.error);
-  }).catch(console.error);
+	try {
+		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must specify the number of messages to clean reactions for.`).catch((err) => console.error);
+		if (!parseInt(args[0], 10)) return msg.channel.send(`<:redx:411978781226696705> Please provide a number of messages to clean reactions for.`).catch((err) => console.error);
+		msg.channel.fetchMessages({limit: parseInt(args[0], 10)}).then((msglog) => {
+			(await msg.channel.send(`<:check:411976443522711552> Clearing Reactions from this channel for \`${args[0]}\` messages...`).then((msg) => {
+				let count = 0;
+				msglog.forEach((message) => {
+					message.clearReactions();
+					count++;
+				}).then(() => {
+					msg.edit(`<:check:411976443522711552> Successfully cleared \`${count}\` messages of reactions.`);
+				}).catch((err) => {
+					console.error(err.toString());
+					msg.edit(`<:redx:411978781226696705> I am sorry but I seem to have encountered an error. I was only able to successfully clean \`${count}\` messages of their reactions.`);
+				});
+			}));
+		}).catch((err) => {
+			console.error(err.toString());
+			msg.channel.send(`<:redx:411978781226696705> I am sorry but I seem to have encountered an error. I wasn't able to clean any of the messages of their reactions.`);
+		});
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
-  name: 'clearreactions',
-  hidden: true,
-  aliases: ['cr'],
-  description: 'Clears all reactions from given number of messages.',
-  usage: 'clearreactions <message count>'
+	name: 'clearreactions',
+	userPermissions: ['MANAGE_MESSAGES'],
+	clientPermissions: ['MANAGE_MESSAGES'],
+	hidden: true,
+	aliases: ['cr'],
+	usage: 'clearreactions <message count>',
+	examples: [
+		'clearreactions 5'
+	],
+	description: 'Clears all reactions from given number of messages.'
 };
