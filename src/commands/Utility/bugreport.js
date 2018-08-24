fa9cf926f738e6eb43b1e21725655337cabaca84
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 19:19:49 +0000

    Update bugreport.js

--- src/commands/Utility/bugreport.js
+++ src/commands/Utility/bugreport.js
@@ -1,37 +1,41 @@
 exports.run = async (bot, msg, args) => {
-	if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Please explain the bug you encountered!`);
-	if (msg.author.id !== bot.config.botCreatorID) {
-		if(!msg.member.hasPermission("MANAGE_SERVER")) return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Manage Server\`!`).catch(console.error);
-    }
-	let report = args.join(' ');
-	bot.channels.get('415897875315425291').send({embed: ({
-		color: 15684432,
-		title: `__Bug Report__`,
-		timestamp: new Date(),
-		author: {
-			name: `${msg.author.tag} <${msg.author.id}>`,
-			icon_url: `${msg.author.displayAvatarURL}`
-		}, fields: [
-		{
-			name: '__User Reporting Issue__',
-			value: `<@${msg.author.id}> \`[${msg.author.tag}]\``,
-			inline: true
-		}, {
-			name: '__Server Sent From__',
-			value: `${msg.guild.name} \`[${msg.guild.id}]\``,
-			inline: true
-		}, {
-			name: '__Described Issue__',
-			value: `${report}`
-		}
-	],
-})}).catch(console.error);
-	msg.channel.send(`<:check:411976443522711552> Bug report has been successfully sent.`);
+	try {
+		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> Please explain the bug you encountered!`);
+		bot.channels.get('415897875315425291').send({embed: ({
+			color: 15684432,
+			title: `__Bug Report__`,
+			timestamp: new Date(),
+			author: {
+				name: `${msg.author.tag} <${msg.author.id}>`,
+				icon_url: `${msg.author.displayAvatarURL}`
+			}, fields: [
+			{
+				name: '__User Reporting Issue__',
+				value: `<@${msg.author.id}> \`[${msg.author.tag}]\``,
+				inline: true
+			}, {
+				name: '__Server Sent From__',
+				value: `${msg.guild.name} \`[${msg.guild.id}]\``,
+				inline: true
+			}, {
+				name: '__Described Issue__',
+				value: `${args.join(' ')}`
+			}
+		],
+	})}).catch((err) => console.error);
+		msg.channel.send(`<:check:411976443522711552> Bug report has been successfully sent.`);
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
 	name: 'bug-report',
-	aliases: ['bugreport','reportbug','sendbugreport','send-bug-report'],
+	userPermissions: ['MANAGE_SERVER'],
+	aliases: ['bugreport','reportbug','sendbugreport','send-bug-report','feedback'],
 	usage: 'bug-report <explain your bug here>',
+	examples: [
+		'bug-report This bot is nice.'
+	],
 	description: 'Report bugs using this command, which sends a bug report to me so I may fix them. You may use markdown.'
 };
