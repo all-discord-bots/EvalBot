fdbe546339410c94c7c950f946f17fd22df1fdb4
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 20:09:34 +0000

    Update shutdown.js

--- src/commands/Utility/shutdown.js
+++ src/commands/Utility/shutdown.js
@@ -1,14 +1,21 @@
-exports.run = async (bot, msg) => {
-	if (msg.author.id !== bot.config.botCreatorID) return;
-	(await msg.channel.send("Shutting down...").then((msg) => {
-		msg.edit(`<:check:411976443522711552> Shutting down. Bye!`);
-	}));
-	bot.shutdown(false);
+exports.run = async (bot, msg, args) => {
+	try {
+		(await msg.channel.send("Shutting down...").then((msg) => {
+			msg.edit(`<:check:411976443522711552> Shutting down. Bye!`);
+		}));
+		bot.shutdown(false);
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
 	name: 'shutdown',
+	ownerOnly: true,
 	hidden: true,
 	usage: 'shutdown',
+	examples: [
+		'shutdown'
+	],
 	description: 'Fully shuts the bot down'
 };
