ac3f721b3d6f85f6be35d242922a54970ec848b4
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 20:21:30 +0000

    Update verifyupvote.js

--- src/commands/Utility/verifyupvote.js
+++ src/commands/Utility/verifyupvote.js
@@ -1,40 +1,48 @@
 const DBL = require("dblapi.js");
+
 exports.run = async (bot, msg, args) => {
-	const dbl = new DBL(process.env.DB_TOKEN, bot);
-	if (msg.guild.id !== bot.config.botMainServerID) return msg.channel.send(`<:redx:411978781226696705> This command may only be used on the support server!`);
-	let gbot = msg.guild.members.get(bot.user.id);
-	if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
-	let guser = msg.guild.members.find(`id`, `${msg.author.id}`);
-	let upvoteRole = msg.guild.roles.find(`id`, `414897780553941002`);
-	if (dbl.hasVoted(msg.author.id) && !msg.member.roles.has(upvoteRole.id)) { // user has already upvoted
-		guser.addRole(upvoteRole).then(() => {
+	try {
+		const dbl = new DBL(process.env.DB_TOKEN, bot);
+		let guser = msg.guild.members.get(`${msg.author.id}`);
+		let upvoteRole = msg.guild.roles.get(`414897780553941002`);
+		if (dbl.hasVoted(msg.author.id) && !msg.member.roles.has(upvoteRole.id)) { // user has already upvoted
+			guser.addRole(upvoteRole).then(() => {
+				msg.channel.send({ embed: ({
+					color: 6732394,
+					title: `Successfully Verified ${msg.author.tag}!`,
+					timestamp: new Date(),
+					description: `<:check:411976443522711552> You have been successfully verified! <:pepoThumbUp:414910627786784768>`
+				})});
+			});
+		} else if (!dbl.hasVoted(msg.author.id) && !msg.member.roles.has(upvoteRole.id)) { // user has not upvoted yet
 			msg.channel.send({ embed: ({
-				color: 6732394,
-				title: `Successfully Verified ${msg.author.tag}!`,
+				color: 15684432,
+				title: `Error Verifying ${msg.author.tag}!`,
 				timestamp: new Date(),
-				description: `<:check:411976443522711552> You have been successfully verified! <:pepoThumbUp:414910627786784768>`
+				description: `<:redx:411978781226696705> You must upvote the bot [here](https://discordbots.org/bot/408741303837392926) first!`
 			})});
-		});
-	} else if (!dbl.hasVoted(msg.author.id) && !msg.member.roles.has(upvoteRole.id)) { // user has not upvoted yet
-		msg.channel.send({ embed: ({
-			color: 15684432,
-			title: `Error Verifying ${msg.author.tag}!`,
-			timestamp: new Date(),
-			description: `<:redx:411978781226696705> You must upvote the bot [here](https://discordbots.org/bot/408741303837392926) first!`
-		})});
-	} else if (dbl.hasVoted(msg.author.id) && msg.member.roles.has(upvoteRole.id)) {
-		msg.channel.send({ embed: ({
-			color: 15684432,
-			title: `Already Verified ${msg.author.tag}!`,
-			timestamp: new Date(),
-			description: `<:redx:411978781226696705> You have already been verified no need to do this again!`
-		})});
+		} else if (dbl.hasVoted(msg.author.id) && msg.member.roles.has(upvoteRole.id)) {
+			msg.channel.send({ embed: ({
+				color: 15684432,
+				title: `Already Verified ${msg.author.tag}!`,
+				timestamp: new Date(),
+				description: `<:redx:411978781226696705> You have already been verified no need to do this again!`
+			})});
+		}
+	} catch (err) {
+		console.error(err.toString());
 	}
 };
 
 exports.info = {
 	name: 'verifyvote',
+	clientPermissions: ['MANAGE_ROLES'],
+	hidden: true,
+	guildOnly: true,
 	usage: 'verifyvote',
 	aliases: ['verify','verifyupvote','upvotecheck','checkupvote'],
+	examples: [
+		'verifyvote'
+	],
 	description: 'Verify that you have upvoted this bot and if successfully verified you are given the Upvoted role. This command can only be used within the support server.'
 };
