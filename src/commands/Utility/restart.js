2859da38f91cb230a59bb2d88bc6b7c86bcf1bf7
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 19:13:59 +0000

    Update restart.js

--- src/commands/Utility/restart.js
+++ src/commands/Utility/restart.js
@@ -1,14 +1,17 @@
-exports.run = async (bot, msg) => {
-    if (msg.author.id !== bot.config.botCreatorID) return;
-    (await msg.channel.send(`<:check:411976443522711552> Restarting bot...`).then((msg)=>{
-      msg.edit(`<:check:411976443522711552> Successfully restarted bot.`);
-    }));
-    bot.shutdown(true);
+exports.run = async (bot, msg, args) => {
+	(await msg.channel.send(`<:check:411976443522711552> Restarting bot...`).then((msg) => {
+		msg.edit(`<:check:411976443522711552> Successfully restarted bot.`);
+	}));
+	bot.shutdown(true);
 };
 
 exports.info = {
-    name: 'restart',
-    hidden: true,
-    usage: 'restart',
-    description: 'Resprings the bot.'
+	name: 'restart',
+	hidden: true,
+	ownerOnly: true,
+	usage: 'restart',
+	examples: [
+		'restart'
+	],
+	description: 'Restarts the bot.'
 };
