adcadbf164449e06e972e9887932e4179ae7b72d
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 22:10:30 +0000

    Update cleanreactions.js

--- src/commands/Utility/cleanreactions.js
+++ src/commands/Utility/cleanreactions.js
@@ -3,7 +3,7 @@ exports.run = (bot, msg, args) => {
 		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must specify the number of messages to clean reactions for.`).catch((err) => console.error);
 		if (!parseInt(parseInt(args[0].toString().replace(/^[-]/g,'')), 10)) return msg.channel.send(`<:redx:411978781226696705> Please provide a number of messages to clean reactions for.`).catch((err) => console.error);
 		(await msg.channel.send({ embed: ({ title: `<a:loading:414954381176340480> Clearing reactions for \`${parseInt(args[0].toString().replace(/^[-]/g,''))}\` messages in this channel...` })}).then((msg) => {
-			msg.channel.fetchMessages({limit: parseInt(args[0], 10)}).then((msglog) => {
+			msg.channel.fetchMessages({limit: parseInt(parseInt(args[0].toString().replace(/^[-]/g,''), 10)}).then((msglog) => {
 				let count = 0;
 				msglog.forEach((message) => {
 					message.clearReactions();
