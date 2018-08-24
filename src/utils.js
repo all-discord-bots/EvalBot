4c76c1f53f7adc9dfe92106cb804a84c4f5348ae
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 07:57:24 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const { RichEmbed, Collection } = require('discord.js');
 const got = require('got');
 
-const shards = new Collection();
+let shards = new Collection();
 
 const memberSearch = (search,guildOnly = false) => {
 	if (guildOnly)
