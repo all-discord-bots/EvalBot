f9b99e4832a1af80d371c18d56889667a31e8df9
BannerBomb <BannerBomb55@gmail.com>
2018-08-24 20:01:04 +0000

    Update debuginfo.js

--- src/commands/Utility/debuginfo.js
+++ src/commands/Utility/debuginfo.js
@@ -1,20 +1,28 @@
-exports.run = async (bot, msg) => {
-	if (msg.author.id !== bot.config.botCreatorID) return;
-	msg.channel.send({embed: ({
-		title: `Debug Information:`,
-		"fields": [
-			{
-				name: `**__Listeners:__**`,
-				value: `${bot.actions.client._eventsCount}/${bot.actions.client._maxListeners}`
-			}
-		],
-		timestamp: new Date()
-	})});
+exports.run = async (bot, msg, args) => {
+	try {
+		msg.channel.send({embed: ({
+			title: `Debug Information:`,
+			fields: [
+				{
+					name: `**__Listeners:__**`,
+					value: `\`${bot.actions.client._eventsCount}\`/\`${bot.actions.client._maxListeners}\``
+				}
+			],
+			timestamp: new Date()
+		})});
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
 	name: 'debuginfo',
+	ownerOnly: true,
+	hidden: true,
 	aliases: ['debuginformation'],
 	usage: 'debuginfo',
+	examples: [
+		'debuginfo'
+	],
 	description: 'Shows debug information of the bot.'
 };
