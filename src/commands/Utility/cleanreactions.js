0025d8036624109d5ea4754cd2ddf4e4c4a643ac
BannerBomb <BannerBomb55@gmail.com>
2018-08-25 22:46:13 +0000

    Update cleanreactions.js

--- src/commands/Utility/cleanreactions.js
+++ src/commands/Utility/cleanreactions.js
@@ -2,8 +2,8 @@ exports.run = async (bot, msg, args) => {
 	try {
 		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must specify the number of messages to clean reactions for.`).catch((err) => console.error);
 		if (!parseInt(args[0]) || !parseInt(parseInt(args[0].toString().replace(/^[-]/g,'')), 10)) return msg.channel.send(`<:redx:411978781226696705> Please provide a number of messages to clean reactions for.`).catch((err) => console.error);
-		(await msg.channel.send({ embed: ({ title: `<a:loading:414954381176340480> Clearing reactions for \`${parseInt(args[0].toString().replace(/^[-]/g,''))}\` messages in this channel...` })}).then((msg) => {
-			msg.channel.fetchMessages({limit: `${parseInt(parseInt(args[0].toString().replace(/^[-]/g,'')), 10)}`}).then((msglog) => {
+		msg.channel.fetchMessages({limit: `${parseInt(parseInt(args[0].toString().replace(/^[-]/g,'')), 10)}`}).then((msglog) => {
+			(await msg.channel.send({ embed: ({ title: `<a:loading:414954381176340480> Clearing reactions for \`${parseInt(args[0].toString().replace(/^[-]/g,''))}\` messages in this channel...` })}).then((msg) => {
 				let count = 0;
 				msglog.forEach((message) => {
 					message.clearReactions();
@@ -18,19 +18,26 @@ exports.run = async (bot, msg, args) => {
 					console.error(err.toString());
 					msg.edit({
 						embed: ({
-							title: `<:redx:411978781226696705> I am sorry but I seem to have encountered an error. I was only able to successfully clean \`${count}\` messages of their reactions.`
+							title: `<:redx:411978781226696705> I am sorry, but I seem to have encountered an error. I was only able to successfully clean \`${count}\` messages of their reactions.`
 						})
 					});
 				});
-			});
+			}).catch((err) => {
+				console.error(err.toString());
+				msg.channel.send({
+					embed: ({
+						title: `<:redx:411978781226696705> I am sorry, but I seem to have encountered an error. I wasn't able to clean any of the messages of their reactions. Feel free to retry the command.`
+					})
+				});
+			}));
 		}).catch((err) => {
 			console.error(err.toString());
 			msg.channel.send({
 				embed: ({
-					title: `<:redx:411978781226696705> I am sorry but I seem to have encountered an error. I wasn't able to clean any of the messages of their reactions. Feel free to retry the command.`
+					title: `<:redx:411978781226696705> I am sorry, but I seem to have encountered an error. I wasn't able to clean any of the messages of their reactions. Feel free to retry the command.`
 				})
 			});
-		}));
+		});
 	} catch (err) {
 		console.error(err.toString());
 	}
