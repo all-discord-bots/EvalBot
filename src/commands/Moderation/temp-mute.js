352b6fe962d0289c19052a02c767e12471853fc4
BannerBomb <BannerBomb55@gmail.com>
2018-08-26 11:25:40 +0000

    Update temp-mute.js

--- src/commands/Moderation/temp-mute.js
+++ src/commands/Moderation/temp-mute.js
@@ -1,63 +1,65 @@
-const Discord = require("discord.js");
-const ms = require("ms");
-
 exports.run = async (bot, msg, args) => {
-let mutedrole = "Muted"; // name of the 'muted' role
-  //!tempmute @user 1s/m/h/d
-  let gbot = msg.guild.members.get(bot.user.id);
-  if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
-  if (!gbot.hasPermission(0x00000002)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Kick Members\`!`).catch(console.error);
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Kick Members\`!`).catch(console.error);
-    if(!msg.member.hasPermission("MANAGE_ROLES")) return msg.channel.send(`<:redx:411978781226696705> You are missing permissions \`Manage Roles\`!`).catch(console.error);
-  }
-  let tomute = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
-  if(!tomute) return msg.channel.send(`<:redx:411978781226696705> Can't find that user.`).catch(console.error);
-  //if(tomute.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(`<:redx:411978781226696705> Can't mute them!`);
-  if (tomute.user.id === msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`).catch(console.error);
-  let muterole = msg.guild.roles.find(`name`, `${mutedrole}`);
-  //start of create role
-  if(!muterole){
-    try{
-      muterole = await msg.guild.createRole({
-        name: `${mutedrole}`,
-        color: "#818386",
-        permissions:[]
-      }).catch(console.error);
-      msg.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  }
-  //end of create role
-  let mutetime = args[1];
-  if(mutetime) {
-    await(tomute.addRole(muterole.id)).catch(console.error);
-    msg.channel.send(`<:check:411976443522711552> <@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
-    setTimeout(function(){
-      tomute.removeRole(muterole.id).catch(console.error);
-      msg.channel.send(`<@${tomute.id}> has been unmuted!`);
-    }, ms(mutetime));
-  } else if (!mutetime) {
-    msg.channel.send(`<:check:411976443522711552> <@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-    await(tomute.addRole(muterole.id)).catch(console.error);
-  }
-
-
-//end of module
-}
+	try {
+		let modlogs = 'mod_logs';
+		switch (args.length) {
+			case 0:
+				return msg.channel.send(`<:redx:411978781226696705> Too few arguments given.`);
+		}
+		let user = bot.utils.getMembers(msg,args[0]);
+		if (!user) return msg.channel.send(`<:redx:411978781226696705> I could not find that user.`);
+		if (user.toString().includes("I could not find that user.")) return;
+		let reason = args.slice(1).join(' ');
+		await msg.guild.fetchBans().then((bans) => {
+			let users = bans.filter((user_object) => user_object === user);
+			if (!users.first()) return msg.channel.send(`<:redx:411978781226696705> That user is not currently banned.`);
+			await msg.guild.unban(user,{
+				reason: `${reason}`
+			}).then(() => {
+				msg.channel.send(`<:check:411976443522711552> \`Case #N/A\` <@${user.id}> has been unbanned.`);
+			}).catch((err) => {
+				console.error(err.toString());
+				return msg.channel.send(`<:redx:411978781226696705> I was unable to un-ban <@${user.id}> because ${err.message}`);
+			});
+		}).catch((err) => {
+			console.error(err.toString());
+			return msg.channel.send(`<:redx:411978781226696705> I was unable to un-ban <@${user.id}>.`);
+		});
+		let modlogs_channel = msg.guild.channels.find(`name`, `${modlogs}`);
+		if (modlogs_channel) {
+			let unban_reason = ``;
+			if (reason !== "") {
+				unban_reason = `\n**Reason:** ${reason}`
+			}
+			modlogs_channel.send({
+				embed: ({
+					description: `**Member:** ${user.tag} (${user.id})\n**Action:** Un-ban${unban_reason}`,
+					color: 6732650,
+					timestamp: new Date(),
+					author: {
+						name: `${msg.author.tag}`,
+						icon_url: `${msg.author.displayAvatarURL}`
+					},
+					footer: {
+						text: `Case #N/A`
+						//text: `Case #${caseNum}`
+					}
+				})
+			});
+		}
+	} catch (err) {
+		console.error(err.toString());
+	}
+};
 
 exports.info = {
-  name: 'tempmute',
-  hidden: true,
-  ownerOnly: true,
-  aliases: ['temp-mute','c-mute','chat-mute','t-mute','text-mute','m'],
-  usage: 'tempmute <user> [1s/m/h/d]',
-  description: 'Mutes a member from the server. This prevents them from sending messages. If no length is given they will be muted until un-muted. Length format examples: `1s`, `1m`, `1h`, `1d`, `1w`, `1mth`, `1y`.',
-}
+	name: 'unban',
+	aliases: ['un-ban','rm-ban','remove-ban'],
+	userPermissions: ['BAN_MEMBERS'],
+	clientPermissions: ['BAN_MEMBERS'],
+	usage: 'unban <member> [reason]'
+	examples: [
+		'unban BannerBomb because he learned his lesson',
+		'unban BannerBomb'
+	],
+	description: 'Unbans a user from the server'
+};
