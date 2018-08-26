c1e6f165c939b7db6d2f022aff0a7adaea1951ca
BannerBomb <BannerBomb55@gmail.com>
2018-08-26 10:26:28 +0000

    Update mute.js

--- src/commands/Moderation/mute.js
+++ src/commands/Moderation/mute.js
@@ -2,7 +2,6 @@ const ms = require('ms');
 
 exports.run = (bot, msg, args) => {
 	try {
-		let modlogs = 'mod_logs';
 		let muted_role_name = 'Muted';
 		switch (args.length) {
 			case 0:
@@ -16,17 +15,33 @@ exports.run = (bot, msg, args) => {
 		let mute_length = ``;
 		if (ms(`${parseFloat(args[1])}`)) {
 			reason = args.slice(2).join(' ');
-			mute_length = 'infinite';
 		} else {
 			reason = args.slice(1).join(' ');
+			mute_length = 'infinite';
 		}
+		if (!bot[msg.guild.id]) {
+			bot[msg.guild.id] = {
+				muted_users: {}
+			};
+		}
+		if (!bot['muted_users'][msg.guild.id]) bot['muted_users'][msg.guild.id] = [];
 		if (user.id == msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`);
 		if (user.manageable == false) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`);
 		let mute_role = msg.guild.roles.find('name', muted_role_name);
-		if (!mute_role) return msg.channel.send(`<:redx:411978781226696705> I was unable to find the role \`Muted\` have you made it yet?`);
+		if (!mute_role) return msg.channel.send(`<:redx:411978781226696705> I was unable to find the role `${muted_role_name}` you can set one up using the \`muted-role\` command.`);
 		if (!user.roles.has(mute_role.id)) {
 			user.addRole(mute_role.id).then(() => {
 				msg.channel.send(`<:check:411976443522711552> \`Case #N/A\` <@${user.id}> has been muted for ${ms(ms(`${parseFloat(args[1])}`),{ long: true })}.`);
+				if (mute_length !== 'infinite') {
+					bot[msg.guild.id]['muted_users'][user.id] = setTimeout(() => {
+						delete bot[msg.guild.id]['muted_users'][user.id];
+						user.removeRole(mute_role.id).then(() => {
+							log_moderation(msg,user,args,mute_length,6732650,'Un-mute','Automatic Unmute');
+						}).catch((err) => {
+							console.error(err.toString());
+						});
+					}, ms(`${parseFloat(args[1])}`);
+				}
 			}).catch((err) => {
 				console.error(err.toString());
 				return msg.channel.send(`<:redx:411978781226696705> I was unable to mute <@${user.id}> because ${err.message}.`);
@@ -34,36 +49,40 @@ exports.run = (bot, msg, args) => {
 		} else {
 			return msg.channel.send(`<:redx:411978781226696705> That user has already been muted.`);
 		}
-		let modlogs_channel = msg.guild.channels.find('name', `${modlogs}`);
-		if (modlogs_channel) {
-			let mute_duration = ``;
-			if (mute_length !== 'infinite') {
-				mute_duration = `\n**Length** ${ms(ms(`${parseFloat(args[1])}`), { long: true })}`;
-			}
-			let mute_reason = ``;
-			if (reason !== "") {
-				mute_reason = `\n**Reason:** ${reason}`
-			}
-			modlogs_channel.send({
-				embed: ({
-					description: `**Member:** ${user.tag} (${user.id})\n**Action:** Mute${mute_duration}${mute_reason}`,
-					color: 16747777,
-					timestamp: new Date(),
-					author: {
-						name: `${msg.author.tag}`,
-						icon_url: `${msg.author.displayAvatarURL}`
-					},
-					footer: {
-						text: `Case #N/A`
-						//text: `Case #${caseNum}`
-					}
-				})
-			});
-		}
+		log_moderation(msg,user,args,mute_length,16747777,'Mute',reason);
 	} catch (err) {
 		console.error(err.toString());
 	}
 };
+
+const log_moderation = async (msg,user,args,mute_length,color,action,reason,modlogs = 'mod_logs') => {
+	let modlogs_channel = msg.guild.channels.find('name', `${modlogs}`);
+	if (modlogs_channel) {
+		let mute_duration = ``;
+		if (mute_length !== 'infinite') {
+			mute_duration = `\n**Length** ${ms(ms(`${parseFloat(args[1])}`), { long: true })}`;
+		}
+		let mute_reason = ``;
+		if (reason !== "") {
+			mute_reason = `\n**Reason:** ${reason}`
+		}
+		modlogs_channel.send({
+			embed: ({
+				description: `**Member:** ${user.tag} (${user.id})\n**Action:** ${action}${mute_duration}${mute_reason}`,
+				color: color,
+				timestamp: new Date(),
+				author: {
+					name: `${msg.author.tag}`,
+					icon_url: `${msg.author.displayAvatarURL}`
+				},
+				footer: {
+					text: `Case #N/A`
+					//text: `Case #${caseNum}`
+				}
+			})
+		});
+	}
+}
 
 exports.info = {
 	name: 'mute',
@@ -76,5 +95,5 @@ exports.info = {
 		'mute BannerBomb 10d for not listening',
 		'mute BannerBomb'
 	],
-	description: 'mutes or unmutes a mentioned user. If you would like to let the bot keep logs of moderations create a text channel named `mod_logs`'
+	description: `Mutes a member from the server.\nThis prevents them from sending messages.\nIf no length is given they will be muted until un-muted.\nIf you would like to let the bot keep logs of moderations create a text channel named \`${modlogs}\``
 };
