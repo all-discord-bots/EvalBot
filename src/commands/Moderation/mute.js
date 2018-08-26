fbb963613e9fb4e478c5604d4559cb1174480d1d
BannerBomb <BannerBomb55@gmail.com>
2018-08-26 08:18:46 +0000

    Update mute.js

--- src/commands/Moderation/mute.js
+++ src/commands/Moderation/mute.js
@@ -1,52 +1,80 @@
-const Discord = require('discord.js');
+const ms = require('ms');
 
 exports.run = (bot, msg, args) => {
-  let reason = args.slice(1).join(' ');
-  let member = msg.mentions.members.first();
-  let modlog = msg.guild.channels.find('name', 'mod_logs');
-  let muteRole = msg.guild.roles.find('name', 'Muted');
-  let gbot = msg.guild.members.get(bot.user.id);
-  if (!gbot.hasPermission(0x00000002)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Kick Members\`!`).catch(console.error);
-  if (!gbot.hasPermission(0x10000000)) return msg.channel.send(`<:redx:411978781226696705> I am missing \`Manage Roles\`!`).catch(console.error);
-  if (msg.author.id !== bot.config.botCreatorID) {
-    if (!msg.member.hasPermission('KICK_MEMBERS')) {
-      return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Kick Members\`!`).catch(console.error);
-    } else if (msg.member.hasPermission('MANAGE_ROLES')) {
-      return msg.channel.send(`<:redx:411978781226696705> You are missing the permissions \`Manage Roles\`!`).catch(console.error);
-    }
-     if(!msg.guild.member(member).kickable) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`).catch(console.error);
-  }
-  //if (!modlog) return msg.channel.send('Please create a channel named `mod_logs` first!').catch(console.error);
-  if (!muteRole) return msg.channel.send('Please create a role named `Muted` first!').catch(console.error);
-  if (reason.length < 1) return msg.channel.send('You must supply a reason for the mute.').catch(console.error);
-  if (msg.mentions.users.size < 1) return msg.channel.send('You must mention someone to mute them.').catch(console.error);
-  if (member.user.id === msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`).catch(console.error);
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setDescription(`**Action:** Un/mute\n**Target:** ${member.user.tag}\n**Moderator:** ${msg.author.tag}\n**Reason:** ${reason}`);
-
-  if (member.roles.has(muteRole.id)) {
-    member.removeRole(muteRole).then(() => {
-      if (bot.channels.get(modlog.id)) {
-        bot.channels.get(modlog.id).send({embed}).catch(console.error);
-      }
-    })
-    .catch(e=>console.error("Cannot remove muted role: " + e));
-  } else {
-    member.addRole(muteRole).then(() => {
-      if (bot.channels.get(modlog.id)) {
-        bot.channels.get(modlog.id).send({embed}).catch(console.error);
-      }
-    })
-    .catch(e=>console.error("Cannot add muted role: " + e));
-  }
-
+	try {
+		let modlogs = 'mod_logs';
+		let muted_role_name = 'Muted';
+		switch (args.length) {
+			case 0:
+				return msg.channel.send(`<:redx:411978781226696705> Too few arguments given.`);
+		}
+		let user = bot.utils.getMembers(msg,args[0]);
+		if (!user) return msg.channel.send(`<:redx:411978781226696705> I could not find that user.`);
+		if (user.toString().includes("I could not find that user.")) return;
+		let reason;
+		//if (ms(`${parseFloat(args[1])}`) && args[1] <= 0) return msg.channel.send(`Invalid mute length provided.`);
+		let mute_length = ``;
+		if (ms(`${parseFloat(args[1])}`)) {
+			reason = args.slice(2).join(' ');
+			mute_length = 'infinite';
+		} else {
+			reason = args.slice(1).join(' ');
+		}
+		if (user.id == msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`);
+		if (user.manageable == false) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`);
+		let mute_role = msg.guild.roles.find('name', muted_role_name);
+		if (!mute_role) return msg.channel.send(`<:redx:411978781226696705> I was unable to find the role \`Muted\` have you made it yet?`);
+		if (!user.roles.has(mute_role.id)) {
+			user.addRole(mute_role.id).then(() => {
+				msg.channel.send(`<:check:411976443522711552> \`Case #N/A\` <@${user.id}> has been muted for ${ms(ms(`${parseFloat(args[1])}`),{ long: true })}.`);
+			}).catch((err) => {
+				console.error(err.toString());
+				return msg.channel.send(`<:redx:411978781226696705> I was unable to mute <@${user.id}> because ${err.message}.`);
+			});
+		} else {
+			return msg.channel.send(`<:redx:411978781226696705> That user has already been muted.`);
+		}
+		let modlogs_channel = msg.guild.channels.find('name', `${modlogs}`);
+		if (modlogs_channel) {
+			let mute_duration = ``;
+			if (mute_length !== 'infinite') {
+				mute_duration = `\n**Length** ${ms(ms(`${parseFloat(args[1])}`), { long: true })}`;
+			}
+			let mute_reason = ``;
+			if (reason !== "") {
+				mute_reason = `\n**Reason:** ${reason}`
+			}
+			modlogs_channel.send({
+				embed: ({
+					description: `**Member:** ${user.tag} (${user.id})\n**Action:** Mute${mute_duration}${mute_reason}`,
+					color: 16747777,
+					timestamp: new Date(),
+					author: {
+						name: `${msg.author.tag}`,
+						icon_url: `${msg.author.displayAvatarURL}`
+					},
+					footer: {
+						text: `Case #N/A`
+						//text: `Case #${caseNum}`
+					}
+				})
+			});
+		}
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
-  name: 'mute',
-  aliases: ['unmute'],
-  description: 'mutes or unmutes a mentioned user. If you would like to let the bot keep logs of moderations create a text channel named `mod_logs`',
-  usage: 'mute <mention> [reason]'
+	name: 'mute',
+	aliases: ['unmute'],
+	userPermissions: ['KICK_MEMBERS'],
+	clientPermissions: ['MANAGE_ROLES'],
+	usage: 'mute <member> [length] [reason]',
+	examples: [
+		'mute BannerBomb being too loud',
+		'mute BannerBomb 10d for not listening',
+		'mute BannerBomb'
+	],
+	description: 'mutes or unmutes a mentioned user. If you would like to let the bot keep logs of moderations create a text channel named `mod_logs`'
 };
