bc92c27cd9313a1d7adffbe974bc69303809ecfd
BannerBomb <BannerBomb55@gmail.com>
2018-08-26 06:53:22 +0000

    Update ban.js

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -1,30 +1,53 @@
-const Discord = require("discord.js");
-
 exports.run = async (bot, msg, args) => {
 	try {
-		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must provide a user to ban.`).catch(err => console.error);
 		let modlogs = "mod_logs";
-		let bUser = bot.utils.getMembers(msg,args[0]);
-		if (!msg.guild.member(bUser)) return msg.channel.send(`<:redx:411978781226696705> I could not find that user.`).catch(err => console.error);
-		if (bUser.toString().includes("I could not find that user.")) return;
-		let bReason = args.join(" ").slice(22);
-		if(!msg.guild.member(bUser).bannable) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`).catch(err => console.error);
-		if (bUser.user.id === msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`).catch(err => console.error);
-		let incidentchannel = msg.guild.channels.find(`name`, `${modlogs}`);
-		// if(!incidentchannel) return msg.channel.send(`Can't find ${modlogs} channel.`).catch(console.error);
-		msg.guild.member(bUser).ban(bReason).catch(err => msg.channel.send(`<:redx:411978781226696705> I could not ban this user due to the error: ${err}`));
-		if (incidentchannel) {
-			let banEmbed = new Discord.RichEmbed()
-				.setDescription("~Ban~")
-				.setColor("#bc0000")
-				.addField("Banned User", `${bUser} with ID ${bUser.id}`)
-				.addField("Banned By", `<@${msg.author.id}> with ID ${msg.author.id}`)
-				.addField("Banned In", msg.channel)
-				.addField("Time", msg.createdAt)
-				.addField("Reason", bReason);
-			incidentchannel.send(banEmbed).catch(err => console.error);
+		switch (args.length) {
+			case 0:
+				return msg.channel.send(`<:redx:411978781226696705> Too few arguments given.`);
 		}
-		msg.channel.send(`<:check:411976443522711552> Successfully banned <@${bUser.id}>`).catch(err => console.error);
+		let user = bot.utils.getMembers(msg,args[0]);
+		if (!user) return msg.channel.send(`<:redx:411978781226696705> I could not find that user.`);
+		if (user.toString().includes("I could not find that user.")) return;
+		let reason;
+		if (parseFloat(args[1])) {
+			reason = args.slice(2).join(' ');
+		} else {
+			reason = args.slice(1).join(' ');
+		}
+		if (user.id == msg.author.id) return msg.channel.send(`<:redx:411978781226696705> I cannot allow self-harm!`);
+		if (user.manageable == false) return msg.channel.send(`<:redx:411978781226696705> I may need my role moved higher!`);
+		if (user.bannable == false) return msg.channel.send(`<:redx:411978781226696705> I cannot ban that user.`);
+		await user.ban({
+			days: parseFloat(args[1]) || 0,
+			reason: `${reason}`
+		}).then(() => {
+			msg.channel.send(`<:check:411976443522711552> Case #N/A <@${user.id}> has been banned.`);
+		}).catch((err) => {
+			console.error(err.toString());
+			msg.channel.send(`<:redx:411978781226696705> I was unable to ban <@${user.id}> because ${err}`);
+		});
+		let modlogs_channel = msg.guild.channels.find(`name`, `${modlogs}`);
+		if (modlogs_channel) {
+			let desc = ``;
+			if (reason !== "") {
+				desc = `\n**Reason:** ${reason}`
+			}
+			modlogs_channel.send({
+				embed: ({
+					description: `**Member:** ${user.tag} (${user.id})\n**Action:** Ban${ban_reason}`,
+					color: 15684432,
+					timestamp: new Date(),
+					author: {
+						name: `${msg.author.tag}`,
+						icon_url: `${msg.author.displayAvatarURL}`
+					},
+					footer: {
+						text: `Case #N/A`
+						//text: `Case #${caseNum}`
+					}
+				})
+			});
+		}
 	} catch (err) {
 		console.error(err.toString());
 	}
@@ -35,6 +58,10 @@ exports.info = {
 	userPermissions: ["BAN_MEMBERS"],
 	clientPermissions: ["BAN_MEMBERS"],
 	aliases: ['bean','deport'],
-	usage: 'ban <member> <reason>',
+	usage: 'ban <member> [days delete] [reason]',
+	examples: [
+		'ban BannerBomb being amazing',
+		'ban BannerBomb 3 for no reason'
+	],
 	description: 'Bans users from your guild. If you would like to let the bot keep logs of moderations create a text channel named `mod_logs`'
 };
