9b629ce9e2430a67ee4f76cf8f0af6fc203935f2
BannerBomb <BannerBomb55@gmail.com>
2018-08-26 09:01:53 +0000

    Update lockdown.js

--- src/commands/Moderation/lockdown.js
+++ src/commands/Moderation/lockdown.js
@@ -3,42 +3,61 @@ const ms = require('ms');
 exports.run = async (bot, msg, args) => {
 	try {
 		let gchannel;
-		if (msg.guild.channels.find(`name`, `${args[0]}`)) {
-			gchannel = msg.guild.channels.find(`name`, `${args[0]}`);
-		} else if (msg.guild.channels.get(`${args[0]}`)) {
-			gchannel = msg.guild.channels.get(`${args[0]}`);
-		} else if (msg.content.includes("<#") && msg.content.includes(">")) {
-			gchannel = msg.guild.channels.get(`${args[0].replace(/<#|>/g, '')}`); // gets the channels id
+		if (args.length <= 0) {
+			gchannel = msg.guild.channels.get(`${msg.channel.id}`)
 		} else {
-			gchannel = msg.guild.channels.get(`${msg.channel.id}`);
+			if (msg.guild.channels.find(`name`, `${args[0]}`)) {
+				gchannel = msg.guild.channels.find(`name`, `${args[0]}`);
+			} else if (msg.guild.channels.get(`${args[0]}`)) {
+				gchannel = msg.guild.channels.get(`${args[0]}`);
+			} else if (msg.content.includes("<#") && msg.content.includes(">")) {
+				gchannel = msg.guild.channels.get(`${args[0].replace(/<#|>/g, '')}`); // gets the channels id
+			} else {
+				gchannel = msg.guild.channels.get(`${msg.channel.id}`);
+			}
+			if (!gchannel) return msg.channel.send(`<:redx:411978781226696705> Invalid \`[channel]\` argument given.`);
 		}
 		if (!bot.lockit) bot.lockit = [];
 		let validUnlocks = ['release', 'unlock'];
-		if (!args[1]) return msg.channel.send(`<:redx:411978781226696705> You must set a duration for the lockdown in either hours, minutes or seconds`).catch(console.error);
-		if (validUnlocks.includes(args[1])) {
+		//if (args[1] && !ms(`${parseFloat(args[1])}`)) return msg.channel.send(`<:redx:411978781226696705> Invalid \`[length]\` argument given.`);
+		if (!parseFloat(args[1]) && validUnlocks.includes(parseFloat(args[1]))) {
+			if (!bot.lockit[gchannel.id] || bot.lockit[gchannel.id] <= 0) return msg.channel.send(`<:redx:411978781226696705> <#${gchannel}> is not currently locked-down.`);
 			gchannel.overwritePermissions(msg.guild.id, {
 				SEND_MESSAGES: null
 			}).then(() => {
 				msg.channel.send(`<:check:411976443522711552> <#${gchannel.id}> is no longer locked-down.`);
 				clearTimeout(bot.lockit[gchannel.id]);
 				delete bot.lockit[gchannel.id];
-			}).catch(error => {
-				console.log(error);
+			}).catch((err) => {
+				return console.log(err.toString());
+			});
+		} else if (parseFloat(args[1]) && ms(`${parseFloat(args[1])}`)) {
+			if (bot.lockit[gchannel.id]) return msg.channel.send(`<:redx:411978781226696705> <#${gchannel}> is already locked-down.`);
+			gchannel.overwritePermissions(msg.guild.id, {
+				SEND_MESSAGES: false
+			}).then(() => {
+				msg.channel.send(`<:check:411976443522711552> <#${gchannel.id}> has been locked-down for ${ms(ms(`${parseFloat(args[1])}`), { long:true })}`).then(() => {
+					bot.lockit[gchannel.id] = setTimeout(() => {
+						gchannel.overwritePermissions(msg.guild.id, {
+							SEND_MESSAGES: null
+						}).then(() => {
+							msg.channel.send(`<:check:411976443522711552> <#${gchannel.id}> is no longer locked-down.`);
+							delete bot.lockit[gchannel.id];
+						}).catch((err) => {
+							return console.error(err.toString());
+						});
+					}, ms(`${parseFloat(args[1])}`));
+				}).catch((err) => {
+					return console.error(err.toString());
+				});
 			});
 		} else {
 			gchannel.overwritePermissions(msg.guild.id, {
 				SEND_MESSAGES: false
 			}).then(() => {
-				msg.channel.send(`<:check:411976443522711552> <#${gchannel.id}> locked down for ${ms(ms(args[1]), { long:true })}`).then(() => {
-					bot.lockit[gchannel.id] = setTimeout(() => {
-						gchannel.overwritePermissions(msg.guild.id, {
-							SEND_MESSAGES: null
-						}).then(msg.channel.send(`<:check:411976443522711552> <#${gchannel.id}> is no longer locked-down.`)).catch(console.error);
-						delete bot.lockit[gchannel.id];
-					}, ms(args[1]));
-				}).catch(error => {
-					console.log(error);
-				});
+				msg.channel.send(`<:check:411976443522711552> <#${gchannel.id}> has been locked-down until you run the \`lock-down <#${gchannel.id}> unlock\` command`);
+			}).catch((err) => {
+				return console.error(err.toString());
 			});
 		}
 	} catch (err) {
@@ -51,6 +70,13 @@ exports.info = {
 	userPermissions: ["MANAGE_CHANNELS"],
 	clientPermissions: ["MANAGE_CHANNELS","MANAGE_ROLES"],
 	aliases: ['channel-lock-down','lock-down-channel','lockdown'],
+	usage: 'lock-down [channel] [length]'
+	examples: [
+		'lock-down #general 3m',
+		'lock-down #general',
+		'lock-down',
+		'lock-down #general unlock',
+		'lock-down unlock'
+	],
 	description: 'Lock down a channel, to prevent anyone sending messages. (Unless they have Administrator permission, or Send Messages is set to Allow in channel permissions for a role they have.). Example 1s, 1m, 1h .etc you can use `unlock` or `release` to un-lockdown a channel.',
-	usage: 'lock-down [channel] [duration]'
 };
