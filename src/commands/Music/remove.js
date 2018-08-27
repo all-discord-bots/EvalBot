a114912f0c7496f16f7960a161f92fe322caf01f
BannerBomb <BannerBomb55@gmail.com>
2018-08-27 23:41:20 +0000

    Update remove.js

--- src/commands/Music/remove.js
+++ src/commands/Music/remove.js
@@ -2,14 +2,14 @@ require('../../conf/globals.js');
 
 exports.run = async (bot, msg, args) => {
 	try {
-		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must provide a song id to remove. To get the song id run the \`queue\` command!`).catch(err => console.error);
+		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must provide a song id to remove. To get the song id run the \`queue\` command!`);
 		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch(err => console.error);
-		if (!musicqueue[msg.guild.id] || !musicqueue[msg.guild.id]['music']) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue!`).catch(err => console.error);
+		if (!music_items[msg.guild.id] || !music_items[msg.guild.id].queue) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue!`);
 		if (parseInt(args[0]) <= 0) return msg.channel.send(`<:redx:411978781226696705> You can't remove the current playing item.`).catch(err => console.error);
-		if (parseInt(args[0]) > musicqueue[msg.guild.id]['music'].length) return msg.channel.send(`<:redx:411978781226696705> You do not have that many songs in the queue!`).catch(err => console.error);
-		const song = musicqueue[msg.guild.id]['music'].indexOf(musicqueue[msg.guild.id]['music'][parseInt(args[0])]);
-		msg.channel.send(`<:check:411976443522711552> Removed \`${musicqueue[msg.guild.id]['music'][parseInt(args[0])]}\` from the queue.`);
-		musicqueue[msg.guild.id]['music'].splice(song, 1);
+		if (parseInt(args[0]) > music_items[msg.guild.id].queue.length) return msg.channel.send(`<:redx:411978781226696705> You do not have that many songs in the queue!`);
+		const song = music_items[msg.guild.id].queue.indexOf(music_items[msg.guild.id].queue[parseInt(args[0])]);
+		msg.channel.send(`<:check:411976443522711552> Removed \`${music_items[msg.guild.id].queue[parseInt(args[0])]}\` from the queue.`);
+		music_items[msg.guild.id].queue.splice(song, 1);
 	} catch (err) {
 		console.error(err.toString());
 	}
@@ -19,7 +19,7 @@ exports.info = {
 	name: 'remove',
 	userPermissions: ['CONNECT'],
 	clientPermissions: ['CONNECT'],
-	aliases: ['removesong','removemusic'],
+	aliases: ['removesong','removemusic','removeaudio','removetrack'],
 	examples: [
 		'remove 1'
 	],
