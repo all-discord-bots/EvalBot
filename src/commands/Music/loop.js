8c70a378af28614a7c5583669f49c1f7e0d4b9ee
BannerBomb <BannerBomb55@gmail.com>
2018-08-27 23:26:03 +0000

    Update loop.js

--- src/commands/Music/loop.js
+++ src/commands/Music/loop.js
@@ -2,29 +2,29 @@ require('../../conf/globals.js');
 
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch(err => console.error);
+		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
 		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`).catch(err => console.error);
-		if (!musicqueue[msg.guild.id] || musicqueue[msg.guild.id]['music'].length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`).catch(err => console.error);
+		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
+		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`).catch(err => console.error);
 		let arg = args.join(' ').toLowerCase();
 		if (arg === 'song' || arg === 'current' || arg === 'this' || arg === 'one' || arg === 'repeat') {
-			if (!musicqueue[msg.guild.id]['loopsong']) {
-				musicqueue[msg.guild.id]['loopsong'] = true;
-				musicqueue[msg.guild.id]['loopqueue'] = false;
+			if (!music_items[msg.guild.id].loop_song) {
+				music_items[msg.guild.id].loop_song = true;
+				music_items[msg.guild.id].loop_queue = false;
 				return msg.channel.send(`Song Looping enabled! :repeat_one:`);
-			} else if (musicqueue[msg.guild.id]['loopsong']) {
-				musicqueue[msg.guild.id]['loopsong'] = false;
-				musicqueue[msg.guild.id]['loopqueue'] = false;
+			} else if (music_items[msg.guild.id].loop_song) {
+				music_items[msg.guild.id].loop_song = false;
+				music_items[msg.guild.id].loop_queue = false;
 				return msg.channel.send(`Song Looping disabled! :arrow_forward:`);
 			}
 		} else if (arg === 'queue' || arg === 'loopqueue' || arg === 'fullqueue' || arg === 'all' || arg.length <= 0) {
-			if (!musicqueue[msg.guild.id]['loopqueue']) {
-				musicqueue[msg.guild.id]['loopqueue'] = true;
-				musicqueue[msg.guild.id]['loopsong'] = false;
+			if (!music_items[msg.guild.id].loop_queue) {
+				music_items[msg.guild.id].loop_queue = true;
+				music_items[msg.guild.id].loop_song = false;
 				return msg.channel.send(`Queue Looping enabled! :repeat:`);
-			} else if (musicqueue[msg.guild.id]['loopqueue']) {
-				musicqueue[msg.guild.id]['loopqueue'] = false;
-				musicqueue[msg.guild.id]['loopsong'] = false;
+			} else if (music_items[msg.guild.id].loop_queue) {
+				music_items[msg.guild.id].loop_queue = false;
+				music_items[msg.guild.id].loop_song = false;
 				return msg.channel.send(`Queue Looping disabled! :arrow_forward:`);
 			}
 		}
@@ -38,7 +38,7 @@ exports.info = {
 	userPermissions: ['CONNECT'],
 	clientPermissions: ['CONNECT'],
 	aliases: ['loopqueue', 'loopsong', 'loopone', 'loopcurrent', 'loopall'],
-	usage: 'loop [queue|song]',
+	usage: 'loop [queue | song]',
 	examples: [
 		'loop',
 		'loop queue',
