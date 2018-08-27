00d0b88a838b25f8fb5908611f4ed281d37d9a0e
BannerBomb <BannerBomb55@gmail.com>
2018-08-27 23:49:16 +0000

    Update stop.js

--- src/commands/Music/stop.js
+++ src/commands/Music/stop.js
@@ -2,14 +2,14 @@ require('../../conf/globals.js');
 
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch((err) => console.error);
+		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
 		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`).catch((err) => console.error);
-		//if (!musicqueue[msg.guild.id] || musicqueue[msg.guild.id]['music'].length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to stop!`).catch(err => console.error);
-		musicqueue[msg.guild.id]['loopqueue'] = false;
-		musicqueue[msg.guild.id]['loopsong'] = false;
-		if (musicqueue[msg.guild.id]['music'] && musicqueue[msg.guild.id]['music'].length > 0) {
-			musicqueue[msg.guild.id]['music'].splice(0, musicqueue[msg.guild.id]['music'].length);
+		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
+		//if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to stop!`).catch(err => console.error);
+		music_items[msg.guild.id].loop_queue = false;
+		music_items[msg.guild.id].loop_song = false;
+		if (music_items[msg.guild.id].queue && music_items[msg.guild.id].queue.length > 0) {
+			music_items[msg.guild.id].queue.splice(0, music_items[msg.guild.id].queue.length);
 		}
 		msg.channel.send(`<:check:411976443522711552> Successfully disconnected from voice channel!`);
 		if (voiceConnection !== null) return voiceConnection.disconnect();
