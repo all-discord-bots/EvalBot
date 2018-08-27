fd17d400d436bd771ca63b7a760b5774dff43df8
BannerBomb <BannerBomb55@gmail.com>
2018-08-27 23:47:45 +0000

    Update skip.js

--- src/commands/Music/skip.js
+++ src/commands/Music/skip.js
@@ -3,8 +3,8 @@ exports.run = async (bot, msg, args) => {
 	try {
 		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch(err => console.error);
 		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-		if (voiceConnection === null || !musicqueue[msg.guild.id] || musicqueue[msg.guild.id] && musicqueue[msg.guild.id]['music'].length < 1) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`).catch(err => console.error);
-		if (!musicqueue[msg.guild.id] || musicqueue[msg.guild.id]['music'].length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`).catch(err => console.error);
+		if (voiceConnection === null || !music_items[msg.guild.id] || music_items[msg.guild.id] && music_items[msg.guild.id].queue.length < 1) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
+		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`);
 		let skipped, s;
 		if (!args[0]) {
 			skipped = `1`;
@@ -13,8 +13,8 @@ exports.run = async (bot, msg, args) => {
 			skipped = args[0];
 			s = 's';
 		}
-		let toSkip = Math.min(parseInt(args[0]), musicqueue[msg.guild.id]['music'].length);
-		musicqueue[msg.guild.id]['music'].splice(0, parseInt(toSkip) - 1);
+		let toSkip = Math.min(parseInt(args[0]), music_items[msg.guild.id].queue.length);
+		music_items[msg.guild.id].queue.splice(0, parseInt(toSkip) - 1);
 		try {
 			const dispatcher = voiceConnection.player.dispatcher;
 			if (voiceConnection.paused) dispatcher.resume();
