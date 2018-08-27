0a4fbad5d9f18d480cee6b71bd09f677bf631a36
BannerBomb <BannerBomb55@gmail.com>
2018-08-27 00:15:28 +0000

    Update cleanreactions.js

--- src/commands/Utility/cleanreactions.js
+++ src/commands/Utility/cleanreactions.js
@@ -2,27 +2,17 @@ exports.run = async (bot, msg, args) => {
 	try {
 		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> You must specify the number of messages to clean reactions for.`);
 		if (!parseFloat(args[0]) || !parseFloat(parseFloat(args[0].toString().replace(/^[-]/g,'')), 10)) return msg.channel.send(`<:redx:411978781226696705> Please provide a number of messages to clean reactions for.`);
-		msg.channel.fetchMessages({
-			limit: parseFloat(parseFloat(args[0].toString().replace(/^[-]/g,'')), 10)
-		}).then(async(msglog) => {
+		msg.channel.fetchMessages({ limit: parseFloat(parseFloat(args[0].toString().replace(/^[-]/g,'')), 10) }).then(async(msglog) => {
 			(await msg.channel.send({ embed: ({ title: `<a:loading:414954381176340480> Clearing reactions for \`${parseFloat(args[0].toString().replace(/^[-]/g,''))}\` messages in this channel...` })}).then((msg) => {
 				let count = 0;
 				msglog.forEach((message) => {
 					message.clearReactions();
 					++count;
-				}).then(() => {
-					msg.edit({
-						embed: ({
-							title: `<:check:411976443522711552> Successfully cleared \`${count}\` messages of reactions.`
-						})
-					});
-				}).catch((err) => {
-					console.error(err.toString());
-					msg.edit({
-						embed: ({
-							title: `<:redx:411978781226696705> I am sorry, but I seem to have encountered an error. I was only able to successfully clean \`${count}\` messages of their reactions.`
-						})
-					});
+				});
+				msg.edit({
+					embed: ({
+						title: `<:check:411976443522711552> Successfully cleared \`${count}\` messages of reactions.`
+					})
 				});
 			}).catch((err) => {
 				console.error(err.toString());
