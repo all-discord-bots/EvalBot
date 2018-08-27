c1eec3810db60334a2fc1cc8a3bb25eddf07d1fc
BannerBomb <BannerBomb55@gmail.com>
2018-08-27 23:24:02 +0000

    Update clearqueue.js

--- src/commands/Music/clearqueue.js
+++ src/commands/Music/clearqueue.js
@@ -3,11 +3,11 @@ require('../../conf/globals.js');
 exports.run = async (bot, msg, args) => {
 	try {
 		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch(err => console.error);
-		if (!musicqueue[msg.guild.id]) return msg.channel.send(`<:redx:411978781226696705> Queue is already empty!`).catch(err => console.error);
-		musicqueue[msg.guild.id]['loopqueue'] = false;
-		musicqueue[msg.guild.id]['loopsong'] = false;
-		if (musicqueue[msg.guild.id]['music'].length > 0) {
-			musicqueue[msg.guild.id]['music'].splice(0, musicqueue[msg.guild.id]['music'].length);
+		if (!music_items[msg.guild.id]) return msg.channel.send(`<:redx:411978781226696705> Queue is already empty!`).catch(err => console.error);
+		music_items[msg.guild.id].loop_queue = false;
+		music_items[msg.guild.id].loop_song = false;
+		if (music_items[msg.guild.id].queue.length > 0) {
+			music_items[msg.guild.id].queue.splice(0, music_items[msg.guild.id].queue.length);
 			msg.channel.send(`<:check:411976443522711552> Queue has been cleared!`);
 		} else {
 			msg.channel.send(`<:redx:411978781226696705> Queue is already empty!`);
@@ -25,5 +25,5 @@ exports.info = {
 	examples: [
 		'clearqueue'
 	],
-	description: 'Clears the current queue.'
+	description: 'Clears the current audio queue.'
 };
