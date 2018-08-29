d68ea4bd37c5c2c059f02677683244ce093f7f79
BannerBomb <BannerBomb55@gmail.com>
2018-08-29 00:23:04 +0000

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -2,16 +2,12 @@ exports.run = async (bot, msg, args) => {
 	try {
 		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch(err => console.error);
 		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`).catch(err => console.error);
+		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No audio is being played.`).catch(err => console.error);
 		const dispatcher = voiceConnection.player.dispatcher;
+if (dispatcher == null || dispatcher == undefined) return msg.channel.send(`<:redx:411978781226696705> Failed to set the volume.`);
 		if (parseInt(args[0]) < 0 || parseInt(args[0]) > 200) return msg.channel.send(`<:redx:411978781226696705> Volume must be \`0-200\`!`).catch(err => console.error);
 		//>eval const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id); voiceConnection.player.dispatcher.volume
-		if (args.length < 1 || isNaN(args[0])) return msg.channel.send(`Current volume is \`${parseInt(dispatcher.volume) * 100}%\`.`).catch(err => console.error);
-		// if (args.length < 1 || isNaN(args[0])) {
-		//	nvol = parseInt('0');
-		//} else {
-			let nvol = parseInt(args[0]);
-		//}
+		if (args.length <= 0/* || isNaN(args[0])*/) return msg.channel.send(`Current volume is \`${parseInt(dispatcher.volume) * 100}%\`.`).catch((err) => console.error);
 		dispatcher.setVolume((nvol / 100));
 		msg.channel.send(`<:check:411976443522711552> Volume set to \`${nvol}%\`.`);
 	} catch (err) {
