f50ed0ffa8162dbb8f0c07a003cfc912c984e774
BannerBomb <BannerBomb55@gmail.com>
2018-08-29 02:19:07 +0000

    Update resume.js

--- src/commands/Music/resume.js
+++ src/commands/Music/resume.js
@@ -1,18 +1,14 @@
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
-		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played`);
-		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to resume!`);
-		const dispatcher = voiceConnection.player.dispatcher;
-		if (!dispatcher) return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`);
-		switch (dispatcher.paused) {
-			case true:
-				dispatcher.resume();
-				return msg.channel.send(`<:check:411976443522711552> Playback resumed.`);
-			default:
-				return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`);
-		}
+		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel.`);
+		const voiceConnection = bot.voiceConnections.get(msg.guild.id);
+		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played.`);
+		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to resume.`);
+		const dispatcher = voiceConnection.dispatcher;
+		if (!dispatcher) return msg.channel.send(`<:redx:411978781226696705> Failed to resume the audio.`);
+		if (!dispatcher.paused) return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`);
+		dispatcher.resume();
+		msg.channel.send(`<:check:411976443522711552> Playback resumed.`);
 	} catch (err) {
 		console.error(err.toString());
 	}
