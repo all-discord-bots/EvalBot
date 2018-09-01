6560c03610234426549d47d89b21ae2b3b431368
BannerBomb <BannerBomb55@gmail.com>
2018-09-01 21:29:45 +0000

    Update source.js

--- src/commands/Utility/source.js
+++ src/commands/Utility/source.js
@@ -1,25 +1,36 @@
 exports.run = (bot, msg, args) => {
-  const replyTo = args[0];
-  msg.channel.messages.fetch({limit: 1, around: replyTo})
-  .then(messages=> {
-    const replyToMsg = messages.first();
-    msg.channel.send(`Source Code for MSG ID ${replyTo}: \`\`\`md\n${clean(replyToMsg.content)}\n\`\`\``)
-    .then(() => msg.delete());
-  }).catch(console.error);
+	try {
+		msg.channel.fetchMessage(`${args[0]}`).then((message) => {
+			msg.channel.send(`Source Code for message id \`${args[0]}\`: \`\`\`md\n${clean(message.content)}\n\`\`\``).then(() => {
+				msg.delete();
+			});
+		}).catch((err) => {
+			console.error(err.toString());
+		});
+	} catch (err) {
+		console.error(err.toString());
+	}
+};
+
+const clean = (text) => {
+	try {
+		if (typeof(text) === 'string') {
+			return text.replace(/[`]/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+		} else {
+			return text;
+		}
+	} catch (err) {
+		console.error(err.toString());
+	}
 };
 
 exports.info = {
-  name: 'source',
-  hidden: true,
-  description: 'Gets the markdown source of the specified message ID in the same channel.',
-  usage: 'source [message ID]'
+	name: 'source',
+	ownerOnly: true,
+	hidden: true,
+	usage: 'source <message id>',
+	examples: [
+		'source 937204837294750'
+	],
+	description: 'Gets the markdown source of the specified message id in the same channel.'
 };
-
-function clean(text) {
-  if (typeof(text) === "string") {
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  }
-  else {
-      return text;
-  }
-}
