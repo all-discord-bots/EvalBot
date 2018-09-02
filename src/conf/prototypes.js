831f57c63f5ac5acb35bc5ee9e5238ce47f80339
BannerBomb <BannerBomb55@gmail.com>
2018-09-02 01:12:13 +0000

    Update prototypes.js

--- src/conf/prototypes.js
+++ src/conf/prototypes.js
@@ -1,1 +1,10 @@
-
+Array.prototype.remove = function() {
+    let value, args = arguments, L = args.length, ax;
+    while (L && this.length) {
+        value = args[--L];
+        while ((ax = this.indexOf(value)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
