d7386224c46d0ddf17d1a747f93c13b0f0ffa0cd
BannerBomb <BannerBomb55@gmail.com>
2018-09-02 02:37:58 +0000

    Update help.js

--- src/commands/Info/help.js
+++ src/commands/Info/help.js
@@ -44,12 +44,12 @@ exports.run = async (bot, msg, args) => {
 		}
 	
 		if (commands.length > 0) {
-			let fields = commands.filter((cmd) => !cmd.info.hidden && !commands.info.ownerOnly)
+			let fields = commands.filter((cmd) => !cmd.info.hidden && !cmd.info.ownerOnly)
 				.sort((a,b) => a.info.name.localeCompare(b.info.name))
 				.map((cmd) => getHelp(bot,msg,cmd,commands.length === 1));
 			/*let fields;
 			if (msg.author.id != process.env.bot_owner) {
-				fields = commands.filter((cmd) => !cmd.info.hidden && !commands.info.ownerOnly)
+				fields = commands.filter((cmd) => !cmd.info.hidden && !cmd.info.ownerOnly)
 					.sort((a,b) => a.info.name.localeCompare(b.info.name))
 					.map((cmd) => getHelp(bot,msg,cmd,commands.length === 1));
 			} else {
