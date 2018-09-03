af33c773fa332a748ed1544abc654e9c75a46aa3
BannerBomb <BannerBomb55@gmail.com>
2018-09-03 03:59:35 +0000

    Update loop.js

--- src/commands/Music/loop.js
+++ src/commands/Music/loop.js
@@ -10,21 +10,21 @@ exports.run = async (bot, msg, args) => {
 			if (!music_items[msg.guild.id].loop_song) {
 				music_items[msg.guild.id].loop_song = true;
 				music_items[msg.guild.id].loop_queue = false;
-				return msg.channel.send(`Song Looping enabled! :repeat_one:`);
+				return msg.channel.send(`<:check:411976443522711552> Song Looping enabled! :repeat_one:`);
 			} else if (music_items[msg.guild.id].loop_song) {
 				music_items[msg.guild.id].loop_song = false;
 				music_items[msg.guild.id].loop_queue = false;
-				return msg.channel.send(`Song Looping disabled! :arrow_forward:`);
+				return msg.channel.send(`<:check:411976443522711552> Song Looping disabled! :arrow_forward:`);
 			}
 		} else if (args[0].toLowerCase() === 'queue' || args[0].toLowerCase() === 'loopqueue' || args[0].toLowerCase() === 'fullqueue' || args[0].toLowerCase() === 'all' || args[0].toLowerCase().length <= 0) {
 			if (!music_items[msg.guild.id].loop_queue) {
 				music_items[msg.guild.id].loop_queue = true;
 				music_items[msg.guild.id].loop_song = false;
-				return msg.channel.send(`Queue Looping enabled! :repeat:`);
+				return msg.channel.send(`<:check:411976443522711552> Queue Looping enabled! :repeat:`);
 			} else if (music_items[msg.guild.id].loop_queue) {
 				music_items[msg.guild.id].loop_queue = false;
 				music_items[msg.guild.id].loop_song = false;
-				return msg.channel.send(`Queue Looping disabled! :arrow_forward:`);
+				return msg.channel.send(`<:check:411976443522711552> Queue Looping disabled! :arrow_forward:`);
 			}
 		}
 	} catch (err) {
