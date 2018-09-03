0722cbdda1a450812e3b0bb424dc2b582b4daeec
BannerBomb <BannerBomb55@gmail.com>
2018-09-03 03:53:32 +0000

    Update reorder.js

--- src/commands/Music/reorder.js
+++ src/commands/Music/reorder.js
@@ -6,15 +6,15 @@ exports.run = async (bot, msg, args) => {
 		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
 		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
 		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length < 1) return msg.channel.send(`<:redx:411978781226696705> There are no items in the queue.`);
-		if (args.length !== 2) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments provided.`);
-		if (args[0] <= 0) return msg.channel.send(`<:redx:411978781226696705> You can't move the current playing item!`);
+		if (args.length != 2) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments given!`);
+		if (args[0] == music_items[msg.guild.id].queue_position) return msg.channel.send(`<:redx:411978781226696705> You can't move the current playing item!`);
 		let queuepositions;
-		if (music_items[msg.guild.id].length <= 0) {
+		if (music_items[msg.guild.id].queue.length <= 0) {
 			queuepositions = `!`;
 		} else {
 			queuepositions = ` valid position are \`1 - ${music_items[msg.guild.id].length}\`!`;
 		}
-		if (args[1] <= 0 || args[1] > music_items[msg.guild.id].queue.length) return msg.channel.send(`<:redx:411978781226696705> Could not move to position \`${args[1]}\`${queuepositions}`);
+		if (args[1] == music_items[msg.guild.id].queue_position || args[1] > music_items[msg.guild.id].queue.length) return msg.channel.send(`<:redx:411978781226696705> Could not move to position \`${args[1]}\`${queuepositions}`);
 		let old_pos = parseInt(args[0]);
 		let new_pos = parseInt(args[1]);
 		if (new_pos >= music_items[msg.guild.id].queue.length) {
