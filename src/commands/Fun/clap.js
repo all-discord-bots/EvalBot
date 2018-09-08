ca542f597187a336002e96086b5b79ea99b47f39
BannerBomb <BannerBomb55@gmail.com>
2018-09-08 17:03:28 +0000

    Update clap.js

--- src/commands/Fun/clap.js
+++ src/commands/Fun/clap.js
@@ -1,16 +1,19 @@
-const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+const randomizeCase = (word) => {
+	word.split('').map((c) => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+}
 
 exports.run = async (bot, msg, args) => {
-    if (args.length < 1) {
-        throw 'Please provide some text to clapify';
-    }
-    (await msg.channel.send("Loading message...").then((msg)=>{
-        msg.edit(args.map(randomizeCase).join(':clap:'));
-    }));
+	if (args.length <= 0) return msg.channel.send('<:redx:411978781226696705> You must provide some text to clapify.');
+	(await msg.channel.send("Loading message...").then((msg) => {
+		msg.edit(args.map(randomizeCase).join(':clap:'));
+	}));
 };
 
 exports.info = {
-    name: 'clap',
-    usage: 'clap <text>',
-    description: 'Clapifies your text'
+	name: 'clap',
+	usage: 'clap <text>',
+	examples: [
+		'clap this is a message to clapify'
+	],
+	description: 'Clapifies your text'
 };
