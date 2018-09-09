814d522a67fda96f473d7860dcdbdc8ad6280b2e
BannerBomb <BannerBomb55@gmail.com>
2018-09-09 07:00:03 +0000

    Update queue.js

--- src/commands/Music/queue.js
+++ src/commands/Music/queue.js
@@ -17,58 +17,62 @@ exports.run = async (bot, msg, args) => {
 			let hashtag = '  ';
 			if (i == music_items[msg.guild.id].queue_position) {
 				hashtag = '# ';
-				fetchVideoInfo(music_items[msg.guild.id].queue[music_items[msg.guild.id].queue_position].id, function(err, videoInfo) {
-					if (err) console.error(`${err.toString()}`);
-					if (videoInfo) {
-						let videoDuration = duration(`${videoInfo.duration}s`);
-						let d, h, m, s; // days, hours, minutes, seconds
-						s = Math.floor(parseInt(videoDuration - 1) / 1000); // - 1 is used to round the video to get the proper second count
-						m = Math.floor(s / 60);
-						s = s % 60;
-						h = Math.floor(m / 60);
-						m = m % 60;
-						d = Math.floor(h / 24);
-						h = h % 24;
-						const voiceConnection = bot.voiceConnections.find((val) => val.channel.guild.id == msg.guild.id);
-						let currenttime = 0;
-						if (voiceConnection) {
-							currenttime = voiceConnection.player.dispatcher.time;
+				if (!music_items[msg.guild.id].is_streamin) {
+					fetchVideoInfo(music_items[msg.guild.id].queue[music_items[msg.guild.id].queue_position].id, function(err, videoInfo) {
+						if (err) console.error(`${err.toString()}`);
+						if (videoInfo) {
+							let videoDuration = duration(`${videoInfo.duration}s`);
+							let d, h, m, s; // days, hours, minutes, seconds
+							s = Math.floor(parseInt(videoDuration - 1) / 1000); // - 1 is used to round the video to get the proper second count
+							m = Math.floor(s / 60);
+							s = s % 60;
+							h = Math.floor(m / 60);
+							m = m % 60;
+							d = Math.floor(h / 24);
+							h = h % 24;
+							const voiceConnection = bot.voiceConnections.find((val) => val.channel.guild.id == msg.guild.id);
+							let currenttime = 0;
+							if (voiceConnection) {
+								currenttime = voiceConnection.player.dispatcher.time;
+							}
+							let currenttimepos = milliseconds.to(hours,minutes,seconds)(parseInt(currenttime));
+							let seczero, minzero, hourzero, seconezero, minonezero, houronezero;
+							if (s < 10) {
+								seczero = '0';
+							} else if (s > 9) {
+								seczero = '';
+							}
+							if (m < 10) {
+								minzero = '0';
+							} else if (m > 9) {
+								minzero = '';
+							}
+							if (h < 10) {
+								hourzero = '0';
+							} else if (h > 9) {
+								hourzero = '';
+							}
+							if (currenttimepos[2] < 10) {
+								seconezero = '0';
+							} else if (currenttimepos[2] > 9) {
+								seconezero = '';
+							}
+							if (currenttimepos[1] < 10) {
+								minonezero = '0';
+							} else if (currenttimepos[1] > 9) {
+								minonezero = '';
+							}
+							if (currenttimepos[0] < 10) {
+								houronezero = '0';
+							} else if (currenttimepos[0] > 9) {
+								houronezero = '';
+							}
+							music_items[msg.guild.id].playback_duration = ` [${houronezero}${currenttimepos[0]}:${minonezero}${currenttimepos[1]}:${seconezero}${currenttimepos[2]}/${hourzero}${h}:${minzero}${m}:${seczero}${s}]`;
 						}
-						let currenttimepos = milliseconds.to(hours,minutes,seconds)(parseInt(currenttime));
-						let seczero, minzero, hourzero, seconezero, minonezero, houronezero;
-						if (s < 10) {
-							seczero = '0';
-						} else if (s > 9) {
-							seczero = '';
-						}
-						if (m < 10) {
-							minzero = '0';
-						} else if (m > 9) {
-							minzero = '';
-						}
-						if (h < 10) {
-							hourzero = '0';
-						} else if (h > 9) {
-							hourzero = '';
-						}
-						if (currenttimepos[2] < 10) {
-							seconezero = '0';
-						} else if (currenttimepos[2] > 9) {
-							seconezero = '';
-						}
-						if (currenttimepos[1] < 10) {
-							minonezero = '0';
-						} else if (currenttimepos[1] > 9) {
-							minonezero = '';
-						}
-						if (currenttimepos[0] < 10) {
-							houronezero = '0';
-						} else if (currenttimepos[0] > 9) {
-							houronezero = '';
-						}
-						music_items[msg.guild.id].playback_duration = ` [${houronezero}${currenttimepos[0]}:${minonezero}${currenttimepos[1]}:${seconezero}${currenttimepos[2]}/${hourzero}${h}:${minzero}${m}:${seczero}${s}]`;
-					}
-				});
+					});
+				} else {
+					music_items[msg.guild.id].playback_duration = '';
+				}
 			}
 			if (i == music_items[msg.guild.id].queue_position) {
 				queue += `${hashtag}${i + 1}. ${music_items[msg.guild.id].queue[i].title || 'Failed to get title for this item!'}${music_items[msg.guild.id].playback_duration !== '' ? music_items[msg.guild.id].playback_duration : ' [00:00:00]'}\n`;
