a041236dd479490dc3444bea4216305303e96c67
BannerBomb <BannerBomb55@gmail.com>
2018-09-09 22:05:59 +0000

    Update globals.js

--- src/conf/globals.js
+++ src/conf/globals.js
@@ -1,23 +1,77 @@
 const music_items = global.music_items = {};
 const songqueue = global.songqueue = {};
-const builtinradio = global.builtinradio = {
-	'fun radio': 'http://streaming.radio.funradio.fr/fun-1-44-128',
-	'1.fm absolute top 40': 'http://strm112.1.fm/top40_mobile_mp3',
-	'977 hits': 'http://19353.live.streamtheworld.com/977_HITS_SC',
-	'absolute radio': 'http://icy-e-bab-09-boh.sharp-stream.com/absoluteradio.mp3',
-	'heart': 'http://media-ice.musicradio.com/HeartSouthWalesMP3',
-	'christian music': 'http://17683.live.streamtheworld.com/WFSHFM_SC',
-	'christian teaching and talk': 'http://17553.live.streamtheworld.com/WAVAFM_SC',
-	'todays christian music': 'http://14223.live.streamtheworld.com/TCMIR_SC',
-	'openfm 100% metallica': 'http://gr-relay-13.gaduradio.pl/62',
-	'north pole radio': 'http://85.25.217.22:8050/christmas',
-	'metal rock fm': 'http://192.99.62.212:9408/stream',
-	'icecast metal': 'http://sentinel.scenesat.com:8000/stream2',
-	'metal nation': 'http://198.100.145.185:8900/;',
-	'streamer radio metal': 'https://streamer.radio.co/s6a349b3a2/listen',
-	'hard radio': 'http://3634.cloudrad.io/stream/1/;',
-	'stream the world': 'https://15363.live.streamtheworld.com/KOMPFM_SC',
-	'nc weather': 'http://wxradio.dyndns.org:8000/WNG538',
-	'nc police dept': 'http://ca.radioboss.fm:8137/stream%26t%3D%26r%3D4RBS4'
+const built_in_radio = global.built_in_radio = {
+	'Fun Radio': {
+		'stream': 'http://streaming.radio.funradio.fr/fun-1-44-128',
+		'playlist_api': ''
+	},
+	'1.FM Absolute Top 40': {
+		'stream': 'http://strm112.1.fm/top40_mobile_mp3',
+		'playlist_api': ''
+	},
+	'977 Hits': {
+		'stream': 'http://19353.live.streamtheworld.com/977_HITS_SC',
+		'playlist_api': ''
+	},
+	'Absolute Radio': {
+		'stream': 'http://icy-e-bab-09-boh.sharp-stream.com/absoluteradio.mp3',
+		'playlist_api': ''
+	},
+	'Heart': {
+		'stream': 'http://media-ice.musicradio.com/HeartSouthWalesMP3',
+		'playlist_api': 'https://onlineradiobox.com/json/uk/heart970/playlist/?cs=uk.heart970'
+	},
+	'Christian Music': {
+		'stream': 'http://17683.live.streamtheworld.com/WFSHFM_SC',
+		'playlist_api': ''
+	},
+	'Christian Teaching and Talk': {
+		'stream': 'http://17553.live.streamtheworld.com/WAVAFM_SC',
+		'playlist_api': ''
+	},
+	'Todays Christian Music': {
+		'stream': 'http://14223.live.streamtheworld.com/TCMIR_SC',
+		'playlist_api': ''
+	},
+	'OpenFM 100% Metallica': {
+		'stream': 'http://gr-relay-13.gaduradio.pl/62',
+		'playlist_api': ''
+	},
+	'North Pole Radio': {
+		'stream': 'http://85.25.217.22:8050/christmas',
+		'playlist_api': ''
+	},
+	'Metal Rock FM': {
+		'stream': 'http://192.99.62.212:9408/stream',
+		'playlist_api': ''
+	},
+	'Icecast Metal': {
+		'stream': 'http://sentinel.scenesat.com:8000/stream2',
+		'playlist_api': ''
+	},
+	'Metal Nation': {
+		'stream': 'http://198.100.145.185:8900/;',
+		'playlist_api': ''
+	},
+	'Streamer Radio Metal': {
+		'stream': 'https://streamer.radio.co/s6a349b3a2/listen',
+		'playlist_api': ''
+	},
+	'Hard Radio': {
+		'stream': 'http://3634.cloudrad.io/stream/1/;',
+		'playlist_api': ''
+	},
+	'Stream the World': {
+		'stream': 'https://15363.live.streamtheworld.com/KOMPFM_SC',
+		'playlist_api': ''
+	},
+	'NC Weather': {
+		'stream': 'http://wxradio.dyndns.org:8000/WNG538',
+		'playlist_api': ''
+	},
+	'NC Police Dept': {
+		'stream': 'http://ca.radioboss.fm:8137/stream%26t%3D%26r%3D4RBS4',
+		'playlist_api': ''
+	}
 };
 // 'monstercat': 'https://www.youtube.com/watch?v=ueupsBPNkSc'
