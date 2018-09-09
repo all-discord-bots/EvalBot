939df70d62746af46deaf280d6c8d6e445b16a17
BannerBomb <BannerBomb55@gmail.com>
2018-09-09 06:54:27 +0000

    Update stop.js

--- src/commands/Music/stop.js
+++ src/commands/Music/stop.js
@@ -8,6 +8,7 @@ exports.run = async (bot, msg, args) => {
 		//if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to stop!`).catch(err => console.error);
 		music_items[msg.guild.id].loop_queue = false;
 		music_items[msg.guild.id].loop_song = false;
+		music_items[msg.guild.id].playback_duration = '';
 		if (music_items[msg.guild.id].queue && music_items[msg.guild.id].queue.length > 0) {
 			music_items[msg.guild.id].queue.splice(0, music_items[msg.guild.id].queue.length);
 		}
