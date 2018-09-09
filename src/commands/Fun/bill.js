822735bd1b7431151841048a57fa82cae2f96db3
BannerBomb <BannerBomb55@gmail.com>
2018-09-09 05:47:55 +0000

    Update bill.js

--- src/commands/Fun/bill.js
+++ src/commands/Fun/bill.js
@@ -1,13 +1,12 @@
 const got = require('got');
 
-exports.run = async (bot, msg) => {
+exports.run = async (bot, msg, args) => {
 	const { body } = await got('http://belikebill.azurewebsites.net/billgen-API.php?default=1', { encoding: null });
 	await msg.channel.send({
-		embed: ({
-			image: {
-				url: body
-			}
-		})
+		file: {
+			attachment: body,
+			name: 'bill.jpg'
+		}
 	});
 };
 
