bf7a7419396ad18212cc085363602aeda9dfd49a
BannerBomb <BannerBomb55@gmail.com>
2018-09-11 02:53:28 +0000

    Update weather.js

--- src/commands/Utility/weather.js
+++ src/commands/Utility/weather.js
@@ -3,71 +3,71 @@
 const got = require('got');
 const countries = require('country-data').countries.all;
 
-const makeURL = (city) => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(city)}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
-const celsius = (fahrenheit) => Math.round(((fahrenheit - 32) * 5) / 9);
-
 const spacer = {
-    name: '\u200b',
-    value: '\u200b',
+	name: '\u200b',
+	value: '\u200b',
 };
 // TODO: Collect weather emojis for use with  name: `Forecast for today is ${EMOJI HERE} *${forecast.text}*`,
 exports.run = async (bot, msg, args) => {
-    if (args.length < 1) return msg.channel.send(`<:redx:411978781226696705> Please provide a city.`).catch(console.error);
-
-    const city = args.join(' ');
-    const res = await got(makeURL(city), { json: true });
-
-    if (!res || !res.body || !res.body.query || !res.body.query.results || !res.body.query.results.channel) return msg.channel.send(`<:redx:411978781226696705> Failed to load weather info!`).catch(console.error);
-
-    const weatherInfo = res.body.query.results.channel;
-    const unit = weatherInfo.units;
-    const forecast = weatherInfo.item.forecast[0];
-
-    const countryInfo = countries.find(country => country.name === weatherInfo.location.country);
-    const countryEmoji = countryInfo ? countryInfo.emoji : ':grey_question:';
-
-    const description = `The current temperature in ${weatherInfo.location.city} is ${weatherInfo.item.condition.temp}°${unit.temperature}/${celsius(weatherInfo.item.condition.temp)}°C`;
-//item":{"title":"Conditions for Wilson, NC, US at 03:00 PM EDT","lat":"35.72171","long":"-77.916153"
-    const embed = bot.utils.embed(`${countryEmoji} ${weatherInfo.item.title}`, description, [
-        {
-            name: 'Location',
-            value: `Latitude: ${weatherInfo.item.lat}\nLongitude: ${weatherInfo.item.long}`
-        },
-        {
-            name: 'Condition',
-            value: `${weatherInfo.item.condition.text}`
-        },
-        {
-            name: 'Atmosphere',
-            value: `Humidity: ${weatherInfo.atmosphere.humidity}%\nPressure: ${weatherInfo.atmosphere.pressure}${unit.pressure}\nRising: ${weatherInfo.atmosphere.rising}\nVisibility: ${weatherInfo.atmosphere.visibility}`
-        },
-        {
-            name: ':wind_blowing_face: Wind',
-            value: `*${weatherInfo.wind.speed}${unit.speed}* ; direction: *${weatherInfo.wind.direction}°* ; chill: *${weatherInfo.wind.chill}*`
-        },
-        {
-            name: `Forecast for today is *${forecast.text}*`,
-            value: `Highest temp is ${forecast.high}°${unit.temperature}/${celsius(forecast.high)}°C, lowest temp is ${forecast.low}°${unit.temperature}/${celsius(forecast.low)}°C`,
-        },
-        spacer,
-        spacer,
-        spacer,
-        spacer,
-        {
-            name: ':sunrise: Sunrise',
-            value: weatherInfo.astronomy.sunrise
-        },
-        {
-            name: ':city_sunset: Sunset',
-            value: weatherInfo.astronomy.sunset
-        }
-    ], { inline: true });
-
-    msg.channel.send({ embed });
+	try {
+		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> Please provide a city.`);
+	
+		const city = args.join(' ');
+		const res = await got(makeURL(city), { json: true });
+	
+		if (!res || !res.body || !res.body.query || !res.body.query.results || !res.body.query.results.channel) return msg.channel.send(`<:redx:411978781226696705> Failed to load weather info!`).catch(console.error);
+	
+		const weatherInfo = res.body.query.results.channel;
+		const unit = weatherInfo.units;
+		const forecast = weatherInfo.item.forecast[0];
+	
+		const countryInfo = countries.find(country => country.name === weatherInfo.location.country);
+		const countryEmoji = countryInfo ? countryInfo.emoji : ':grey_question:';
+	
+		const description = `The current temperature in ${weatherInfo.location.city} is ${weatherInfo.item.condition.temp}°${unit.temperature}/${celsius(weatherInfo.item.condition.temp)}°C`;
+		//item":{"title":"Conditions for Wilson, NC, US at 03:00 PM EDT","lat":"35.72171","long":"-77.916153"
+		const embed = bot.utils.embed(`${countryEmoji} ${weatherInfo.item.title}`, description, [
+			{
+				name: 'Location',
+				value: `Latitude: ${weatherInfo.item.lat}\nLongitude: ${weatherInfo.item.long}`
+			},{
+				name: 'Condition',
+				value: `${weatherInfo.item.condition.text}`
+			},{
+				name: 'Atmosphere',
+				value: `Humidity: ${weatherInfo.atmosphere.humidity}%\nPressure: ${weatherInfo.atmosphere.pressure}${unit.pressure}\nRising: ${weatherInfo.atmosphere.rising}\nVisibility: ${weatherInfo.atmosphere.visibility}`
+			},{
+				name: ':wind_blowing_face: Wind',
+				value: `*${weatherInfo.wind.speed}${unit.speed}* ; direction: *${weatherInfo.wind.direction}°* ; chill: *${weatherInfo.wind.chill}*`
+			},{
+				name: `Forecast for today is *${forecast.text}*`,
+				value: `Highest temp is ${forecast.high}°${unit.temperature}/${celsius(forecast.high)}°C, lowest temp is ${forecast.low}°${unit.temperature}/${celsius(forecast.low)}°C`,
+			},
+			spacer,
+			spacer,
+			spacer,
+			spacer,
+			{
+				name: ':sunrise: Sunrise',
+				value: weatherInfo.astronomy.sunrise
+			},{
+				name: ':city_sunset: Sunset',
+				value: weatherInfo.astronomy.sunset
+			}
+		],{ inline: true });
+	msg.channel.send({ embed });
 };
 
+const makeURL = (city) => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(city)}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+const celsius = (fahrenheit) => Math.round(((fahrenheit - 32) * 5) / 9);
+
 exports.info = {
-    name: 'weather',
-    usage: 'weather <city>',
-    description: 'Shows weather info for city'
+	name: 'weather',
+	usage: 'weather <city>',
+	examples: [
+		'weather NC',
+		'weather North Caroline',
+		'weather Raleigh'
+	],
+	description: 'Shows weather info for city'
 };
