07ba3d8ab3d0e3ec05b9fd82df79974c9b2e1b85
BannerBomb <BannerBomb55@gmail.com>
2018-09-17 06:36:55 +0000

    Update radio.js

--- src/commands/Music/radio.js
+++ src/commands/Music/radio.js
@@ -63,7 +63,7 @@ exports.run = async (bot, msg, args) => {
 		if (music_items[msg.guild.id].queue.length === 1 || !msg.guild.voiceConnection) {
 			executeQueue(music_items[msg.guild.id].queue);
 		}
-		
+		if (!queue[0] || !queue[0].url) return msg.channel.send(`<:redx:411978781226696705> I was unable to play the stream. Make sure the stream is valid.`);
 		msg.channel.send(`<:check:411976443522711552> Streaming \`${music_items[msg.guild.id].queue[0].title}\`.`);
 		
 		function executeQueue(queue) {
@@ -99,7 +99,6 @@ exports.run = async (bot, msg, args) => {
 					music_items[msg.guild.id].queue_position = 0;
 					music_items[msg.guild.id].loop_queue = false;
 					music_items[msg.guild.id].loop_song = false;
-					if (!queue[0] || !queue[0].url) return msg.channel.send(`<:redx:411978781226696705> I was unable to play the stream. Make the stream is valid.`);
 					let dispatcher = connection.play(queue[0].url, { filter: 'audioonly' }, { volume: (music_items[msg.guild.id].volume / 100) }); // Radio
 					
 					connection.once('failed', (reason) => {
