ef42d643c4edeae22448f39032353c0ef933c7ff
BannerBomb <BannerBomb55@gmail.com>
2018-09-17 06:16:09 +0000

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -1,22 +1,22 @@
 exports.run = async (bot, msg, args) => {
 	try {
 		//let parsed = bot.utils.parseArgs(args, ['d']);
-		if (!msg.member.voiceChannel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch((err) => console.error);
-		const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id);
-		if (voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No audio is being played.`).catch((err) => console.error);
-		const dispatcher = voiceConnection.player.dispatcher;
-		if (dispatcher === null || dispatcher === undefined) return msg.channel.send(`<:redx:411978781226696705> Failed to set the volume.`);
+		if (!msg.member.voice.channel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`).catch((err) => console.error);
+		if (msg.guild.voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No audio is being played.`).catch((err) => console.error);
+		if (msg.guild.voiceConnection.player.dispatcher === null || msg.guild.voiceConnection.player.dispatcher === undefined) return msg.channel.send(`<:redx:411978781226696705> Failed to set the volume.`);
 		if (!args[1] || (args[1] && args[1] == false)) {
 			if (parseInt(args[0]) < 0 || parseInt(args[0]) > 200) return msg.channel.send(`<:redx:411978781226696705> Volume must be \`0-200\`!`).catch(err => console.error);
 		}
-		//>eval const voiceConnection = bot.voiceConnections.find(val => val.channel.guild.id == msg.guild.id); voiceConnection.player.dispatcher.volume
-		if (args.length <= 0/* || isNaN(args[0])*/) return msg.channel.send(`Current volume is \`${parseInt(dispatcher.volume) * 100}%\`.`).catch((err) => console.error);
+		if (args.length <= 0/* || isNaN(args[0])*/) return msg.channel.send(`Current volume is \`${parseInt(msg.guild.voiceConnection.player.dispatcher.volume) * 100}%\`.`).catch((err) => console.error);
 		if (!args[1] || (args[1] && args[1] == false)) {
-			dispatcher.setVolume(parseInt(args[0]) / 100);
+			msg.guild.voiceConnection.player.dispatcher.setVolume(parseInt(args[0]) / 100);
 		} else if (args[1] && args[1] == true) {
-			dispatcher.setVolume(parseInt(args[0]));
+			msg.guild.voiceConnection.player.dispatcher.setVolume(parseInt(args[0]));
 		}
-		msg.channel.send(`<:check:411976443522711552> Volume set to \`${parseInt(args[0])}%\`.`);
+		
+		msg.guild.voiceConnection.player.dispatcher.volumeChange.once((oldVolume,newVolume) => {
+			msg.channel.send(`<:check:411976443522711552> Volume set to \`${newVolume}%\``); //\`${parseInt(args[0])}%\`.`);
+		});
 	} catch (err) {
 		console.error(err.toString());
 	}
