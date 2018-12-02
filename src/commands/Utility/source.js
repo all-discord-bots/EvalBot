6e8d22d93ebb6c48f7d39c3dd05b545a529d8363
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2018-12-02 15:09:15 -0500

    Update source.js

--- src/commands/Utility/source.js
+++ src/commands/Utility/source.js
@@ -1,7 +1,7 @@
 exports.run = (bot, msg, args) => {
 	try {
 		if (args.length <= 0) return msg.channel.send(`<:redx:411978781226696705> you must provide a message id.`);
-		msg.channel.fetchMessage(`${args[0]}`).then((message) => {
+		msg.channel.messages.fetch(`${args[0]}`).then((message) => {
 			if (!message) return msg.channel.send(`<:redx:411978781226696705> could not find message with the id \`${args[0]}\` in the channel <#${msg.channel.id}>.`);
 			msg.channel.send(`Source Code for message id \`${args[0]}\`: \`\`\`md\n${clean(message.content)}\n\`\`\``);//.then(() => {
 				//msg.delete();
