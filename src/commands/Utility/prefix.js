7bce11e0d0f07427508295ee18dd905ee790494d
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2018-12-20 14:18:18 -0500

    Update prefix.js

--- src/commands/Utility/prefix.js
+++ src/commands/Utility/prefix.js
@@ -1,14 +1,13 @@
 const fetch = require('node-fetch');
 
 exports.run = async (bot, msg, args) => {
-	if (msg.author.id !== "269247101697916939") return;
 	if (args.length <= 0) return msg.channel.send(`This guilds current prefix is \`${bot.config[msg.guild.id] ? bot.config[msg.guild.id].prefix : bot.config.prefix}\`.`).catch((err) => console.error);
 		//if (bot.config[msg.guild.id]) return msg.channel.send(`This guilds current prefix is \`${bot.config[msg.guild.id].prefix}\`.`).catch((err) => console.error);
 	
 	try {
-		await bot.managers.config.set(msg.guild.id, {prefix: args.join(' ')});
-		await fetch(`http://cripsbot.000webhostapp.com/database/database_update.php?guild_id=${msg.guild.id}&prefix=${args.join(' ')}&database_token=QISqwssXd93riidEqjjRFom19SDuSTEU`).catch((err) => console.error);
-		msg.channel.send(`<:check:411976443522711552> Prefix changed to \`${args.join(' ')}\`.`);
+		await bot.managers.config.set(msg.guild.id, {prefix: args.join(' ').trim()});
+		await fetch(`http://cripsbot.000webhostapp.com/database/database_update.php?guild_id=${msg.guild.id}&prefix=${args.join(' ').trim()}&database_token=QISqwssXd93riidEqjjRFom19SDuSTEU`).catch((err) => console.error);
+		msg.channel.send(`<:check:411976443522711552> Prefix changed to \`${args.join(' ').trim()}\`.`);
 	} catch (err) {
 		console.error(err.toString());
 	}
