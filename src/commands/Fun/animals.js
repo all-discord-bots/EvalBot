8749ffbca442855f28c337bbd5105f7be52fe77a
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-01-18 19:17:59 -0500

    Update animals.js

--- src/commands/Fun/animals.js
+++ src/commands/Fun/animals.js
@@ -33,12 +33,30 @@ function makeCommand(type, url, transformer) {
     };
 }
 
-const vote = Math.floor(Math.random() * 2) >= 1 ? true : false;
+const endpoints = {
+    cats: ['http://aws.random.cat/meow', 'https://nekos.life/api/v2/img/meow'],
+    dogs: ['https://dog.ceo/api/breeds/image/random', 'http://random.dog/woof.json', 'https://nekos.life/api/v2/img/woof'],
+    lizards: ['https://nekos.life/api/v2/img/lizard'],
+    ducks: ['https://random-d.uk/api/v1/random']
+}
+
+const generated_number = {
+    cats: 0,
+    dogs: 0,
+    lizards: 0,
+    ducks: 0
+};
+
+function vote(category) {
+    const generator = Math.floor(Math.random() * endpoints[category].length);
+    generated_number[category] = generator;
+    return endpoints[category][generator];
+}
 
 module.exports = [
-    makeCommand('cat', vote ? 'http://aws.random.cat/meow' : 'https://nekos.life/api/v2/img/meow', body => vote ? JSON.parse(body).file : JSON.parse(body).url),
-    makeCommand('dog', vote ? 'http://random.dog/woof.json' : 'https://nekos.life/api/v2/img/woof', body => JSON.parse(body).url),
+    makeCommand('cat', vote('cats'), body => generated_number.cats >= 1 ? JSON.parse(body).file : JSON.parse(body).url),
+    makeCommand('dog', vote('dogs'), body => generated_number.dogs >= 1 ? JSON.parse(body).url : JSON.parse(body).message),
     //makeCommand('dog', vote ? 'http://random.dog/woof' : 'https://nekos.life/api/v2/img/woof', body => vote ? `http://random.dog/${body}` : JSON.parse(body).url),
-    makeCommand('lizard', 'https://nekos.life/api/v2/img/lizard', body => JSON.parse(body).url),
-    makeCommand('duck', 'https://random-d.uk/api/v1/random', body => JSON.parse(body).url)
+    makeCommand('lizard', vote('lizards'), body => JSON.parse(body).url),
+    makeCommand('duck', vote('ducks'), body => JSON.parse(body).url)
 ];
