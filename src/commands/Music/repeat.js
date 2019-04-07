c47f3b6a92867806b44ce9b5a14917e41db21f9e
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 17:47:41 -0400

    Update repeat.js

--- src/commands/Music/repeat.js
+++ src/commands/Music/repeat.js
@@ -6,7 +6,7 @@ exports.run = async (bot, msg, args) => {
 		if (msg.guild.voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
 		let fetched_queue = music_items[msg.guild.id];
 		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`).catch((err) => console.error);
-    fetched_queue.repeat = !fetched_queue.repeat;
+		fetched_queue.repeat = !fetched_queue.repeat;
 		fetched_queue.loop = false;
 		return msg.channel.send(`<:check:411976443522711552> Song repeat ${!fetched_queue.repeat ? 'enabled! :repeat_one:' : 'disabled! :arrow_forward:'}`);
 	} catch (err) {
