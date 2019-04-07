29977b6ec4a1d6c1407338750c41ac6e452d1f0b
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 19:18:33 -0400

    Update loop.js

--- src/commands/Music/loop.js
+++ src/commands/Music/loop.js
@@ -2,13 +2,13 @@ require('../../conf/globals.js');
 
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voice.channel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
-		if (msg.guild.voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
+		if (!msg.member.voice.channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
+		if (msg.guild.voiceConnection === null) return msg.channel.send('<:redx:411978781226696705> There is no audio being played.');
 		let fetched_queue = music_items[msg.guild.id];
-		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`).catch((err) => console.error);
+		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send('<:redx:411978781226696705> There are no audios in the queue to loop!').catch((err) => console.error);
 		fetched_queue.loop = !fetched_queue.loop;
 		fetched_queue.repeat = false;
-		return msg.channel.send(`<:check:411976443522711552> Queue Looping ${!fetched_queue.loop ? 'enabled! :repeat:' : 'disabled! :arrow_forward:'}`);
+		return msg.channel.send(`<:check:411976443522711552> Queue Looping ${fetched_queue.loop ? 'enabled! :repeat:' : 'disabled! :arrow_forward:'}`);
 	} catch (err) {
 		console.error(err.toString());
 	}
