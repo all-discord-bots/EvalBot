65d533174240192c01d77b0828e0aa0d538ef426
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 17:46:56 -0400

    Update reorder.js

--- src/commands/Music/reorder.js
+++ src/commands/Music/reorder.js
@@ -2,28 +2,28 @@ require('../../conf/globals.js');
 
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voice.channel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
-		if (msg.guild.voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
-		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length < 1) return msg.channel.send(`<:redx:411978781226696705> There are no items in the queue.`);
-		if (args.length != 2) return msg.channel.send(`<:redx:411978781226696705> Invalid arguments given!`);
-		if (args[0] == music_items[msg.guild.id].queue_position) return msg.channel.send(`<:redx:411978781226696705> You can't move the current playing item!`);
-		let queuepositions;
-		if (music_items[msg.guild.id].queue.length <= 0) {
-			queuepositions = `!`;
-		} else {
-			queuepositions = ` valid position are \`1 - ${music_items[msg.guild.id].length}\`!`;
-		}
-		if (args[1] == music_items[msg.guild.id].queue_position || args[1] > music_items[msg.guild.id].queue.length) return msg.channel.send(`<:redx:411978781226696705> Could not move to position \`${args[1]}\`${queuepositions}`);
+		let fetched_queue = music_items[msg.guild.id];
+		if (!msg.member.voice.channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
+		if (msg.guild.voiceConnection === null) return msg.channel.send('<:redx:411978781226696705> There is no tracks being played.');
+		if (!fetched_queue || fetched_queue.queue.length < 1) return msg.channel.send('<:redx:411978781226696705> There are no tracks in the queue.');
+		if (args.length !== 2) return msg.channel.send('<:redx:411978781226696705> Invalid arguments given!');
+		if (args[0] === fetched_queue.queue_position) return msg.channel.send('<:redx:411978781226696705> You can\'t move the current playing track!');
+		let queuepositions = (fetched_queue.queue.length <= 0) ? '!' : ` valid position are \`1 - ${fetched_queue.queue.length}\`!`;
+		if (args[1] == fetched_queue.queue_position || args[1] > fetched_queue.queue.length) return msg.channel.send(`<:redx:411978781226696705> Could not move to position \`${args[1]}\`${queuepositions}`);
 		let old_pos = parseInt(args[0]);
 		let new_pos = parseInt(args[1]);
-		if (new_pos >= music_items[msg.guild.id].queue.length) {
-			let pos = new_pos - music_items[msg.guild.id].queue.length + 1;
+		if (new_pos >= fetched_queue.queue.length) {
+			let pos = new_pos - fetched_queue.queue.length + 1;
 			while (pos--) {
-				music_items[msg.guild.id].queue.push(undefined);
+				fetched_queue.queue.push(undefined);
 			}
 		}
-		msg.channel.send(`<:check:411976443522711552> Position \`${old_pos}\` has been moved to position \`${new_pos}\` in the queue.`);
-		music_items[msg.guild.id].queue.splice(new_pos, 0, music_items[msg.guild.id].queue.splice(old_pos, 1)[0]);
+		try {
+			fetched_queue.queue.splice(new_pos, 0, fetched_queue.queue.splice(old_pos, 1)[0]);
+			msg.channel.send(`<:check:411976443522711552> track \`${old_pos}\` has been moved to position \`${new_pos}\` in the queue.`);
+		} catch (e) {
+			msg.channel.send(`<:redx:411978781226696705> failed to move track \`${old_pos}\` to position \`${new_pos}\` in the queue.`);
+		}
 	} catch (err) {
 		console.error(err.toString());
 	}
@@ -31,12 +31,13 @@ exports.run = async (bot, msg, args) => {
 
 exports.info = {
 	name: 'reorder',
+	guildOnly: true,
 	userPermissions: ['CONNECT'],
 	clientPermissions: ['CONNECT'],
-	aliases: ['reorderqueue'],
+	aliases: ['reorderqueue', 'reorder-queue'],
 	usage: 'reorder <song id> <new position>',
 	examples: [
 		'reorder 1 3'
 	],
-	description: 'Changes the position of a song in the queue. You can use the `queue` command to get the song position id.'
+	description: 'Changes the position of a track in the queue. You can use the `queue` command to get the track position id.'
 };
