bfd9a3e3351a12fff10bba0b5fa5f1a3a1cbb544
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 19:01:43 -0400

    Update repeat.js

--- src/commands/Music/repeat.js
+++ src/commands/Music/repeat.js
@@ -5,7 +5,7 @@ exports.run = async (bot, msg, args) => {
 		if (!msg.member.voice.channel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
 		if (msg.guild.voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> There is no audio being played.`);
 		let fetched_queue = music_items[msg.guild.id];
-		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to loop!`).catch((err) => console.error);
+		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send('<:redx:411978781226696705> There are no tracks in the queue to loop!').catch((err) => console.error);
 		fetched_queue.repeat = !fetched_queue.repeat;
 		fetched_queue.loop = false;
 		return msg.channel.send(`<:check:411976443522711552> Song repeat ${!fetched_queue.repeat ? 'enabled! :repeat_one:' : 'disabled! :arrow_forward:'}`);
@@ -24,5 +24,5 @@ exports.info = {
 	examples: [
 		'repeat'
 	],
-	description: 'Repeat the current song.'
+	description: 'Repeat the current track.'
 };
