6dbe8a35ddb6633b8fac57565038f09155dc903c
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 16:09:23 -0400

    Update pause.js

--- src/commands/Music/pause.js
+++ src/commands/Music/pause.js
@@ -2,14 +2,15 @@ require('../../conf/globals.js');
 
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voice.channel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
-		if (!msg.guild.voiceConnection) return msg.channel.send(`<:redx:411978781226696705> No audio is being played`);
-		if (!music_items[msg.guild.id]) return msg.channel.send(`<:redx:411978781226696705> There is no audios in the queue playing!`);
-		if (music_items[msg.guild.id].is_streaming) return msg.channel.send(`<:redx:411978781226696705> Streams cannot be paused.`);
+		if (!msg.member.voice.channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
+		if (!msg.guild.voiceConnection) return msg.channel.send('<:redx:411978781226696705> No audio is being played');
+		let fetched_queue = music_items[msg.guild.id];
+		if (!fetched_queue) return msg.channel.send('<:redx:411978781226696705> There is no audios in the queue playing!');
+		if (fetched_queue.is_streaming) return msg.channel.send('<:redx:411978781226696705> Streams cannot be paused.');
 		if (msg.guild.voiceConnection.player.dispatcher) {
-			if (!msg.guild.voiceConnection.player.dispatcher.pause) return msg.channel.send(`<:redx:411978781226696705> Playback already paused!`);
+			if (!msg.guild.voiceConnection.player.dispatcher.pause) return msg.channel.send('<:redx:411978781226696705> Playback already paused!');
 			msg.guild.voiceConnection.player.dispatcher.pause();
-			msg.channel.send(`<:check:411976443522711552> Playback paused.`);
+			msg.channel.send('<:check:411976443522711552> Playback paused.');
 		}
 	} catch (err) {
 		console.error(err.toString());
@@ -18,6 +19,7 @@ exports.run = async (bot, msg, args) => {
 
 exports.info = {
 	name: 'pause',
+	guildOnly: true,
 	userPermissions: ['CONNECT'],
 	clientPermissions: ['CONNECT'],
 	usage: 'pause',
