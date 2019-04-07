5c77aa8b7223822318c30fa761d717ba011cb913
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 17:52:41 -0400

    Update resume.js

--- src/commands/Music/resume.js
+++ src/commands/Music/resume.js
@@ -1,12 +1,16 @@
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voice.channel) return msg.channel.send(`<:redx:411978781226696705> You must be in a voice channel!`);
-		if (msg.guild.voiceConnection === null) return msg.channel.send(`<:redx:411978781226696705> No music is being played`);
-		if (!music_items[msg.guild.id] || music_items[msg.guild.id].queue.length <= 0) return msg.channel.send(`<:redx:411978781226696705> There are no audios in the queue to resume!`);
-		if (!msg.guild.voiceConnection.player.dispatcher) return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`);
-		if (!msg.guild.voiceConnection.player.dispatcher.paused) return msg.channel.send(`<:redx:411978781226696705> Playback is not paused!`);
-		msg.guild.voiceConnection.player.dispatcher.resume();
-		msg.channel.send(`<:check:411976443522711552> Playback resumed.`);
+		if (!msg.member.voice.channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
+		if (msg.guild.voiceConnection === null) return msg.channel.send('<:redx:411978781226696705> No music is being played');
+		let fetched_queue = music_items[msg.guild.id];
+		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send('<:redx:411978781226696705> There are no audios in the queue to resume!');
+		if ((!msg.guild.voiceConnection.player.dispatcher) || (msg.guild.voiceConnection.player.dispatcher && !msg.guild.voiceConnection.player.dispatcher.paused)) return msg.channel.send('<:redx:411978781226696705> Playback is not paused!');
+		try {
+			msg.guild.voiceConnection.player.dispatcher.resume();
+			msg.channel.send('<:check:411976443522711552> Playback resumed.');
+		} catch (e) {
+			msg.channel.send('<:redx:411978781226696705> Failed to resume playback.');
+		}
 	} catch (err) {
 		console.error(err.toString());
 	}
@@ -14,6 +18,7 @@ exports.run = async (bot, msg, args) => {
 
 exports.info = {
 	name: 'resume',
+	guildOnly: true,
 	userPermissions: ['CONNECT'],
 	clientPermissions: ['CONNECT'],
 	usage: 'resume',
