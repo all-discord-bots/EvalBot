9c3dfe0f66916d362bc323e9b5060e7b4c35111d
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-07 19:46:18 -0400

    Update volume.js

--- src/commands/Music/volume.js
+++ src/commands/Music/volume.js
@@ -6,15 +6,16 @@ exports.run = async (bot, msg, args) => {
 			if (!msg.guild.voiceConnection) return msg.channel.send('<:redx:411978781226696705> There\'s no audio currently being played.');
 			if (!msg.guild.voiceConnection.player.dispatcher) return msg.channel.send('<:redx:411978781226696705> Failed to set the volume.');
 			if ((parseInt(args[0]) < 0 || parseInt(args[0]) > 200) && (!args[1] || (args[1] && args[1] == false))) return msg.channel.send('<:redx:411978781226696705> Volume must be between `0-200`!');
-			if (args.length <= 0) return msg.channel.send(`Current volume is \`${parseFloat(msg.guild.voiceConnection.player.dispatcher.volume) * 100}%\`.`);
+			if (args.length <= 0) return msg.channel.send(`Current volume is \`${parseFloat(msg.guild.voiceConnection.dispatcher.volume) * 100}%\`.`);
 			resolve(msg.guild.voiceConnection);
 		}).then((connection) => {
 			if (!args[1] || (args[1] && args[1] == false)) {
-				return msg.guild.voiceConnection.player.dispatcher.setVolume(parseFloat(args[0]) / 100);
+				return msg.guild.voiceConnection.dispatcher.setVolume(parseFloat(args[0]) / 100);
 			} else if (args[1] && args[1] == true) {
-				return msg.guild.voiceConnection.player.dispatcher.setVolume(parseFloat(args[0]));
+				return msg.guild.voiceConnection.dispatcher.setVolume(parseFloat(args[0]));
 			}
-			connection.player.dispatcher.on('volumeChange', (oldVolume,newVolume) => {
+			//connection.player.dispatcher.once('volumeChange', (oldVolume,newVolume) => {
+			connection.dispatcher.once('volumeChange', (oldVolume,newVolume) => {
 				msg.channel.send(`<:check:411976443522711552> Volume set to \`${newVolume * 100}%\`.`);
 			});
 		}).catch((err) => {
