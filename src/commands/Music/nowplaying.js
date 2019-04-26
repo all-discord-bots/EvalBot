a7367389eeb46fc66a6731325ac8eb9e56c94a9f
BannerBomb <11788894+BannerBomb@users.noreply.github.com>
2019-04-25 21:48:40 -0400

    Update nowplaying.js

--- src/commands/Music/nowplaying.js
+++ src/commands/Music/nowplaying.js
@@ -280,24 +280,24 @@ exports.run = async (bot, msg, args) => {
 	}
 };
 
-const abbrNum = (number, decPlaces) => {
+const abbrNum = (number, decPlaces = 0) => {
 	// 2 decimal places => 100, 3 => 1000, etc
 	decPlaces = Math.pow(10, decPlaces);
 
 	// Enumerate number abbreviations
-	var abbrev = ["K", "M", "B", "T"];
+	let abbrev = ["K", "M", "B", "T"];
 
 	// Go through the array backwards, so we do the largest first
-	for (var i = abbrev.length - 1; i >= 0; i--) {
+	for (let i = abbrev.length - 1; i >= 0; i--) {
 
 		// Convert array index to "1000", "1000000", etc
-		var size = Math.pow(10, (i + 1) * 3);
+		let size = Math.pow(10, (i + 1) * 3);
 
 		// If the number is bigger or equal do the abbreviation
 		if (size <= number) {
 			// Here, we multiply by decPlaces, round, and then divide by decPlaces.
 			// This gives us nice rounding to a particular decimal place.
-			number = Math.round(number * decPlaces / size) / decPlaces;
+			number = Math.floor(number * decPlaces / size) / decPlaces; // Math.round rounds the number
 
 			// Add the letter for the abbreviation
 			number += abbrev[i];
