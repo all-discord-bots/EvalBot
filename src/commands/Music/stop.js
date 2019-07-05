6bf2ce72f4c35708aaa422e9361c928bd105ef7f
BannerBomb <BannerBomb55@gmail.com>
2019-07-05 02:23:00 -0400

    Update stop.js

--- src/commands/Music/stop.js
+++ src/commands/Music/stop.js
@@ -3,7 +3,7 @@ require('../../conf/globals.js');
 exports.run = async (bot, msg, args) => {
 	try {
 		//if (!msg.member.voiceChannel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
-		if (msg.guild.voiceConnection === null) return msg.channel.send('<:redx:411978781226696705> There is no audio being played.');
+		if (!bot.voice.connections.has(msg.guild.id)) return msg.channel.send('<:redx:411978781226696705> There is no audio being played.');
 		//if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send('<:redx:411978781226696705> There are no audios in the queue to stop!').catch((err) => console.error);
 		let fetched_queue = music_items[msg.guild.id];
 		fetched_queue.loop = false;
@@ -12,7 +12,7 @@ exports.run = async (bot, msg, args) => {
 		fetched_queue.queue_position = 0;
 		try {
 			if (fetched_queue.queue && fetched_queue.queue.length > 0) fetched_queue.queue.splice(0, fetched_queue.queue.length);
-			if (msg.guild.voiceConnection !== null) return msg.guild.voiceConnection.disconnect();
+			if (bot.voice.connections.has(msg.guild.id)) return bot.voice.connections.get(msg.guild.id).disconnect();
 			msg.channel.send('<:check:411976443522711552> Successfully disconnected from voice channel!');
 		} catch (e) {
 			msg.channel.send('<:redx:411978781226696705> Failed to disconnected from voice channel!');
