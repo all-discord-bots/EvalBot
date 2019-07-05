54c01e1f1632a45e41bdf7920ef302b821f757f4
BannerBomb <BannerBomb55@gmail.com>
2019-07-05 02:21:56 -0400

    Update skip.js

--- src/commands/Music/skip.js
+++ src/commands/Music/skip.js
@@ -3,14 +3,14 @@ exports.run = async (bot, msg, args) => {
 	try {
 		if (!msg.member.voice.channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!').catch((err) => console.error);
 		let fetched_queue = music_items[msg.guild.id];
-		if ((msg.guild.voiceConnection === null || !fetched_queue) || (fetched_queue && fetched_queue.queue.length < 1)) return msg.channel.send('<:redx:411978781226696705> There is no audio being played.');
+		if ((!bot.voice.connections.has(msg.guild.id) || !fetched_queue) || (fetched_queue && fetched_queue.queue.length < 1)) return msg.channel.send('<:redx:411978781226696705> There is no audio being played.');
 		//if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send('<:redx:411978781226696705> There are no audios in the queue to loop!');
 		let [skipped, s] = !args[0] ? ['1', ''] : [args[0], 's'];
 		let toSkip = Math.min(parseInt(args[0]), fetched_queue.queue.length);
 		fetched_queue.queue.splice(0, parseInt(toSkip) - 1);
 		try {
-			if (msg.guild.voiceConnection && msg.guild.voiceConnection.paused) msg.guild.voiceConnection.player.dispatcher.resume();
-			msg.guild.voiceConnection.player.dispatcher.end();
+			if (bot.voice.connections.has(msg.guild.id) && bot.voice.connections.get(msg.guild.id).paused) bot.voice.connections.get(msg.guild.id).player.dispatcher.resume();
+			bot.voice.connections.get(msg.guild.id).player.dispatcher.end();
 			msg.channel.send(`<:check:411976443522711552> Skipped ${skipped} tracks${s}.`);
 		} catch (err) {
 			msg.channel.send(`<:redx:411978781226696705> Error occurred!\n\`\`\`\n${err.toString().split(':')[0]}: ${err.toString().split(':')[1]}\n\`\`\``);
