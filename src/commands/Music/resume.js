d84903a1cb6f57ee97870fd315c6b9b3fae1e29c
BannerBomb <BannerBomb55@gmail.com>
2019-07-12 17:47:52 -0400

    Update resume.js

--- src/commands/Music/resume.js
+++ src/commands/Music/resume.js
@@ -1,12 +1,14 @@
 exports.run = async (bot, msg, args) => {
 	try {
-		if (!msg.member.voice.channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
-		if (!bot.voice.connections.has(msg.guild.id)) return msg.channel.send('<:redx:411978781226696705> No music is being played');
+		const voice_channel = msg.member.voice.channel;
+		const voice_connection = msg.guild.voice.connection;
+		if (!voice_channel) return msg.channel.send('<:redx:411978781226696705> You must be in a voice channel!');
+		if (!voice_connection) return msg.channel.send('<:redx:411978781226696705> No music is being played');
 		let fetched_queue = music_items[msg.guild.id];
 		if (!fetched_queue || fetched_queue.queue.length <= 0) return msg.channel.send('<:redx:411978781226696705> There are no audios in the queue to resume!');
-		if ((!bot.voice.connections.get(msg.guild.id).player.dispatcher) || (bot.voice.connections.get(msg.guild.id).player.dispatcher && !bot.voice.connections.get(msg.guild.id).player.dispatcher.paused)) return msg.channel.send('<:redx:411978781226696705> Playback is not paused!');
+		if ((!voice_connection.player.dispatcher) || (voice_connection.player.dispatcher && !voice_connection.player.dispatcher.paused)) return msg.channel.send('<:redx:411978781226696705> Playback is not paused!');
 		try {
-			bot.voice.connections.get(msg.guild.id).player.dispatcher.resume();
+			voice_connection.player.dispatcher.resume();
 			msg.channel.send('<:check:411976443522711552> Playback resumed.');
 		} catch (e) {
 			msg.channel.send('<:redx:411978781226696705> Failed to resume playback.');
